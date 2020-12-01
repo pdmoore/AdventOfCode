90ba3c8f8d7e7bc632635700aca86090e1fec52a
Paul Moore <pdmoore@gmail.com>
2020-11-30 21:14:42 -0800

    d1p1, switch to List<Integer> to reuse util

--- 2020/src/test/java/day01Tests.java
+++ 2020/src/test/java/day01Tests.java
@@ -1,4 +1,11 @@
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,30 +22,60 @@ public class day01Tests {
 
 
     @Test
-    public void part1() {
-        int[] input = {
+    public void part1_example() {
+        List<Integer> input = Arrays.asList(new Integer[]
+        {
                 1721,
                 979,
                 366,
                 299,
                 675,
-                1456};
+                1456});
 
         int actual = solve(input);
 
         assertEquals(514579, actual);
     }
 
-    private int solve(int[] input) {
-        for (int i = 0; i < input.length; i++) {
-            for (int j = i + 1; j < input.length; j++) {
-                int sum = input[i] + input[j];
+    @Test
+    @Disabled
+    public void part1_solution() {
+//        List<Integer> input = fileToIntegerList("../data/day01-part01.txt);
+
+//        int actual = solve(input);
+
+//        assertEquals(-99, actual);
+    }
+
+
+    private int solve(List<Integer> input) {
+        for (int i = 0; i < input.size(); i++) {
+            for (int j = i + 1; j < input.size(); j++) {
+                int sum = input.get(i) + input.get(j);
                 if (2020 == sum) {
-                    return input[i] * input[j];
+                    return input.get(i) * input.get(j);
                 }
             }
         }
 
         return 0;
+    }
+
+
+
+    static List<Integer> fileToIntegerList(String puzzleInputFile) {
+        List<Integer> masses = new ArrayList<>();
+        try{
+            File f = new File(puzzleInputFile);
+            Scanner scanner = new Scanner(f);
+            while (scanner.hasNext()){
+                int i = scanner.nextInt();
+                masses.add(i);
+            }
+        }catch(Exception err){
+            err.printStackTrace();
+            throw new RuntimeException("error while processing " + puzzleInputFile);
+        }
+        return masses;
     }
 }
