4f486fa79a70d4590bf6c3f023abc84f359f7d43
Paul Moore <pdmoore@gmail.com>
2020-11-30 18:00:30 -0800

    create 2020 directory; single project for all days; confirm simple build/test

---
+++ 2020/src/test/java/day01Tests.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class day01Tests {
+
+    @Test
+    public void verifyBuild() {
+        Assertions.assertEquals(true, true);
+    }
+}
