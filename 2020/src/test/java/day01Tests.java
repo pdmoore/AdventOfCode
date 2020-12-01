2ae59c7c186a2ac0a7967556fa07a068f58a5a92
Paul Moore <pdmoore@gmail.com>
2020-11-30 18:05:34 -0800

    static import of assertEquals; don't track .class files

--- 2020/src/test/java/day01Tests.java
+++ 2020/src/test/java/day01Tests.java
@@ -1,10 +1,11 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class day01Tests {
 
     @Test
     public void verifyBuild() {
-        Assertions.assertEquals(true, true);
+        assertEquals(true, true);
     }
 }
