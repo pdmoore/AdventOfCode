d230310862f724562fe76ff57b31bd03448adfd3
Paul Moore <pdmoore@gmail.com>
2020-11-30 21:08:23 -0800

    day 1, part 1, solved

--- 2020/src/test/java/day01Tests.java
+++ 2020/src/test/java/day01Tests.java
@@ -4,8 +4,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class day01Tests {
 
+    /*
+
+- List of numbers
+- iterate summing one to the next
+- if sum is 2020, multiply the two and halt
+- bigint?
+
+     */
+
+
     @Test
-    public void verifyBuild() {
-        assertEquals(true, true);
+    public void part1() {
+        int[] input = {
+                1721,
+                979,
+                366,
+                299,
+                675,
+                1456};
+
+        int actual = solve(input);
+
+        assertEquals(514579, actual);
+    }
+
+    private int solve(int[] input) {
+        for (int i = 0; i < input.length; i++) {
+            for (int j = i + 1; j < input.length; j++) {
+                int sum = input[i] + input[j];
+                if (2020 == sum) {
+                    return input[i] * input[j];
+                }
+            }
+        }
+
+        return 0;
     }
 }
