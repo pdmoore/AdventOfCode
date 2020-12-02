ce5d16d255d768bb80e69d8bc4a3dda20edfa80c
Paul Moore <pdmoore@gmail.com>
2020-12-01 21:15:43 -0800

    d2p1 - first pass at logic

--- 2020/src/test/java/day02Tests.java
+++ 2020/src/test/java/day02Tests.java
@@ -19,14 +19,31 @@ INVALID
      */
 
     @Test
-    public void parseLine() {
-//        1-3 a: abcde
-        //split on spaces
-        String input = "1-3 a: abcde";
+    public void validPasswords() {
+        Assertions.assertTrue(validatePassword("1-3 a: abcde"));
+        Assertions.assertTrue(validatePassword("2-9 c: ccccccccc"));
+    }
+
+    @Test
+    public void invalidPasswords() {
+        Assertions.assertFalse(validatePassword("1-3 b: cdefg"));
+    }
+
+    private boolean validatePassword(String input) {
         String[] elements = input.split(" ");
-        Assertions.assertEquals("1-3", elements[0]);
-        Assertions.assertEquals("a:", elements[1]);
-        Assertions.assertEquals("abcde", elements[2]);
+
+        String letter = elements[1].substring(0,1);
+
+        String removedLetter = elements[2].replaceAll(letter, "");
+
+        int occurencesOfLetter = elements[2].length() - removedLetter.length();
+
+        String counts = elements[0];
+        String[] bounds = counts.split("-");
+        int lowerBound = Integer.parseInt(bounds[0]);
+        int upperBound = Integer.parseInt(bounds[1]);
+
+        return lowerBound <= occurencesOfLetter && occurencesOfLetter <= upperBound;
     }
 
 
