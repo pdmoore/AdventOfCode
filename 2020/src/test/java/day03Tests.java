9024b9e42cea5c5c17cc58293a614204fe1dafb6
Paul Moore <pdmoore@gmail.com>
2020-12-02 21:37:28 -0800

    d3p2 solution

--- 2020/src/test/java/day03Tests.java
+++ 2020/src/test/java/day03Tests.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,7 +41,6 @@ public class day03Tests {
         assertEquals(7, treeCount);
     }
 
-
     @Test
     public void day3_part1() {
         List<String> slope = Utilities.fileToStringList("./data/day03-part01");
@@ -48,6 +49,46 @@ public class day03Tests {
         assertEquals(162, treeCount);
     }
 
+    @Test
+    public void day3_part2_example() {
+        List<String> slope = new ArrayList<>();
+        slope.add("..##.......");
+        slope.add("#...#...#..");
+        slope.add(".#....#..#.");
+        slope.add("..#.#...#.#");
+        slope.add(".#...##..#.");
+        slope.add("..#.##.....");
+        slope.add(".#.#.#....#");
+        slope.add(".#........#");
+        slope.add("#.##...#...");
+        slope.add("#...##....#");
+        slope.add(".#..#...#.#");
+
+
+        int actual = toboggan(slope, 1, 1) *
+                toboggan(slope, 3, 1) *
+                toboggan(slope, 5, 1) *
+                toboggan(slope, 7, 1) *
+                toboggan(slope, 1, 2);
+
+        assertEquals(336, actual);
+    }
+
+    @Test
+    public void day3_part2() {
+        List<String> slope = Utilities.fileToStringList("./data/day03-part01");
+
+        BigInteger actual1 = BigInteger.valueOf(toboggan(slope, 1, 1));
+        BigInteger actual2 = BigInteger.valueOf(toboggan(slope, 3, 1));
+        BigInteger actual3 = BigInteger.valueOf(toboggan(slope, 5, 1));
+        BigInteger actual4 = BigInteger.valueOf(toboggan(slope, 7, 1));
+        BigInteger actual5 = BigInteger.valueOf(toboggan(slope, 1, 2));
+
+        BigInteger expected = new BigInteger("3064612320");
+
+        assertEquals(expected, actual1.multiply(actual2).multiply(actual3).multiply(actual4).multiply(actual5));
+
+    }
 
 
     private int toboggan(List<String> slope, int deltaX, int deltaY) {
