1e76297a565393be604d0c87659af8856648abe9
Paul Moore <pdmoore@gmail.com>
2020-12-03 22:32:33 -0800

    d1p2 - solution

--- 2020/src/test/java/day04Tests.java
+++ 2020/src/test/java/day04Tests.java
@@ -165,7 +165,7 @@ invalid otherwise
         public boolean IsValid() {
             String[] keyPairs = passportString.split(" ");
             if (keyPairs.length == 8) {
-                return true;
+                return validateData(keyPairs);
             }
 
             if (keyPairs.length == 7) {
@@ -175,10 +175,107 @@ invalid otherwise
                         return false;
                     }
                 }
-                return true;
+                return validateData(keyPairs);
             }
 
             return false;
+        }
+
+        private boolean validateData(String[] keyPairs) {
+            /*
+byr (Birth Year) - four digits; at least 1920 and at most 2002.
+iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+hgt (Height) - a number followed by either cm or in:
+If cm, the number must be at least 150 and at most 193.
+If in, the number must be at least 59 and at most 76.
+hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+pid (Passport ID) - a nine-digit number, including leading zeroes.
+             */
+
+            for (int i = 0; i < keyPairs.length; i++) {
+                String[] field = keyPairs[i].split(":");
+                switch (field[0]) {
+                    case "byr": if (!validateBirthYear(field[1])) return false; break;
+                    case "iyr": if (!validateIssueYear(field[1])) return false; break;
+                    case "eyr": if (!validateExpirationYear(field[1])) return false; break;
+                    case "hgt": if (!validateHeight(field[1])) return false; break;
+                    case "hcl": if (!validateHairColor(field[1])) return false; break;
+                    case "ecl": if (!validateEyeColor(field[1])) return false; break;
+                    case "pid": if (!validatePassportID(field[1])) return false; break;
+                }
+            }
+
+            return true;
+
+        }
+
+        private boolean validateBirthYear(String byr) {
+            int year = Integer.parseInt(byr);
+            return year >= 1920 && year <= 2002;
+        }
+
+        private boolean validateIssueYear(String iyr) {
+            int year = Integer.parseInt(iyr);
+            return year >= 2010 && year <= 2020;
+        }
+
+        private boolean validateExpirationYear(String eyr) {
+            int year = Integer.parseInt(eyr);
+            return year >= 2020 && year <= 2030;
+        }
+
+        private boolean validateHeight(String hgt) {
+            if (hgt.endsWith("cm")) {
+                int height = Integer.parseInt((hgt.substring(0, hgt.length() - 2)));
+                return height >= 150 && height <= 193;
+            } else if (hgt.endsWith("in")) {
+                int height = Integer.parseInt((hgt.substring(0, hgt.length() - 2)));
+                return height >= 59 && height <= 76;
+            }
+            return false;
+        }
+
+        private boolean validateHairColor(String hcl) {
+            if (hcl.charAt(0) != '#' || hcl.length() != 7) return false;
+
+            System.out.println(hcl);
+
+            for (int i = 1; i < hcl.length(); i++) {
+                if (Character.digit(hcl.charAt(i), 16) == -1) {
+                    return false;
+                }
+            }
+
+
+
+            // confirm each of remaining 6 characters 0-9, a-f
+
+            //        hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+
+
+            return true;
+        }
+
+        private boolean validateEyeColor(String ecl) {
+            List<String> validEyeColors = new ArrayList<>();
+            validEyeColors.add("amb");
+            validEyeColors.add("blu");
+            validEyeColors.add("brn");
+            validEyeColors.add("gry");
+            validEyeColors.add("grn");
+            validEyeColors.add("hzl");
+            validEyeColors.add("oth");
+
+            return validEyeColors.contains(ecl);
+        }
+
+        // Didn't seem to have any input with non-digit pid????
+        private boolean validatePassportID(String pid) {
+            if (pid.length() != 9) return false;
+            String regex = "[0-9]+";
+            return pid.matches(regex);
         }
     }
 }
