0f018c4d9d9a6915fd1181dac5e412e9f1fa0e1b
Paul Moore <pdmoore@gmail.com>
2020-12-07 21:42:04 -0800

    d2p2 - solution

--- 2020/src/test/java/day08Tests.java
+++ 2020/src/test/java/day08Tests.java
@@ -9,25 +9,21 @@ public class day08Tests {
 
     /*
 
-locate infinite loop (ie, a position that has been visited already
+gonna need to cycle through entire input many times
+have a toggle index that increments each time
+- make a copy of the input
+- if toggle index command is acc then skip this input
+- if toggle is nop, change it to jmp
+- if toggle is jmp, change it to nop
 
-need to parse lines
-need to handle three commands
-acc
-jmp
-nop
-
-need a 'pointer' to current instruction
-
-need to track in accumulator
-
-
+- then try to execute the program
+- if the program cycles then abandon input and increment toggle index
 
      */
 
 
     @Test
-    public void foo() {
+    public void part1_example() {
         List<String> input = new ArrayList<>();
         input.add("nop +0");
         input.add("acc +1");
@@ -51,6 +47,107 @@ need to track in accumulator
         int actual = detectInfiniteLoop(input);
 
         assertEquals(1586, actual);
+    }
+
+    @Test
+    public void part2_example() {
+        List<String> input = new ArrayList<>();
+        input.add("nop +0");
+        input.add("acc +1");
+        input.add("jmp +4");
+        input.add("acc +3");
+        input.add("jmp -3");
+        input.add("acc -99");
+        input.add("acc +1");
+        input.add("jmp -4");
+        input.add("acc +6");
+
+        int actual = fixTheProgram(input);
+
+        assertEquals(8, actual);
+    }
+
+    @Test
+    public void part2_solution() {
+        List<String> input = Utilities.fileToStringList("./data/day08-part01");
+
+        int actual = fixTheProgram(input);
+
+        assertEquals(703, actual);
+    }
+
+
+
+    private int fixTheProgram(List<String> input) {
+        int toggleIndex = 0;
+
+        while (toggleIndex < input.size()) {
+
+            String instruction = input.get(toggleIndex).substring(0, 3);
+            if ("acc".equals(instruction)) {
+                toggleIndex++;
+            } else {
+
+                List<String> copyOfInput = new ArrayList<>();
+                copyOfInput.addAll(input);
+
+                if ("jmp".equals(instruction)) {
+
+                    // swap jmp with nop
+                    String nopNotJmp = input.get(toggleIndex).replace("jmp", "nop");
+                    copyOfInput.set(toggleIndex, nopNotJmp);
+
+
+                } else {
+
+                    // swap nop with jmp
+                    String jmpNotNop = input.get(toggleIndex).replace("nop", "jmp");
+                    copyOfInput.set(toggleIndex, jmpNotNop);
+                }
+
+                //check for infinite loop
+                if (hasInfiniteLoop(copyOfInput)) {
+                    toggleIndex++;
+                } else {
+                    return detectInfiniteLoop(copyOfInput);
+                }
+
+            }
+
+        }
+
+        return -1;
+
+    }
+
+    private boolean hasInfiniteLoop(List<String> input) {
+        int currentInstruction = 0;
+
+        List<Integer> visitedInstructions = new ArrayList<>();
+
+        while (visitedInstructions.contains(currentInstruction) == false) {
+            visitedInstructions.add(currentInstruction);
+
+            String instruction = input.get(currentInstruction).substring(0, 3);
+
+
+            if ("nop".equals(instruction)) {
+                currentInstruction += 1;
+            } else if ("acc".equals(instruction)) {
+                currentInstruction += 1;
+            } else if ("jmp".equals(instruction)) {
+                String argument = input.get(currentInstruction).substring(4);
+                int argValue = Integer.parseInt(argument);
+
+                currentInstruction += argValue;
+            } else {
+                throw new RuntimeException("unknown instruction " + instruction + currentInstruction);
+            }
+
+            if (currentInstruction >= input.size()) return false;
+        }
+
+        return visitedInstructions.size() != input.size();
     }
 
 
@@ -84,6 +181,9 @@ need to track in accumulator
             } else {
                 throw new RuntimeException("unknown instruction " + instruction + currentInstruction);
             }
+
+            if (currentInstruction >= input.size()) return accumulator;
+
         }
 
         return accumulator;
