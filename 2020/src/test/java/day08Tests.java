b0238b63bfdc467373dfdd600b632bcc93232347
Paul Moore <pdmoore@gmail.com>
2020-12-07 20:59:36 -0800

    d8 - starting files

---
+++ 2020/src/test/java/day08Tests.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class day08Tests {
+    
+    /*
+
+
+
+
+     */
+
+
+    @Test
+    public void foo() {
+        assertEquals(0, 1);
+    }
+}
