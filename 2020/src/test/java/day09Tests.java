a35ad701c9ea18a35ad5d4e0bb0344c9ce4beb49
Paul Moore <pdmoore@gmail.com>
2020-12-08 20:49:09 -0800

    d9 - starting point

---
+++ 2020/src/test/java/day09Tests.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class day09Tests {
+
+    /*
+
+
+
+
+        List<String> input = Utilities.fileToStringList("./data/day09-part01");
+
+     */
+
+
+    @Test
+    public void foo() {
+        assertEquals(0, 1);
+    }
+
+}
