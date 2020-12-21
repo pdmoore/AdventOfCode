22aee3cb08f4fb04271eab4ec61561aadac5cb98
Paul Moore <pdmoore@gmail.com>
2020-12-21 13:08:38 -0800

    d14 - starting

---
+++ 2020/src/test/java/day14Tests.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class day14Tests {
+
+    /*
+            List<String> input = Utilities.fileToStringList("./data/day14-part01");
+
+     */
+
+    @Test
+    public void foo() {
+        assertEquals(0,1);
+    }
+}
