b3403afe4b057628f9087ac3aff482a52a345818
Paul Moore <pdmoore@gmail.com>
2020-12-24 21:30:19 -0800

    d25p1 - solution

--- 2020/src/test/java/day25Tests.java
+++ 2020/src/test/java/day25Tests.java
@@ -1,11 +1,96 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class day25Tests {
 
+
+    /*
+
+20201227
+
+The handshake used by the card and the door involves an operation that transforms a subject number. To transform a subject number, start with the value 1.
+Then, a number of times called the loop size, perform the following steps:
+
+Set the value to itself multiplied by the subject number.
+Set the value to the remainder after dividing the value by 20201227.
+
+     */
+
+    public static final int SUBJECT_NUMBER = 7;
+
+    public List<String> createPart1Input() {
+        List<String> input = new ArrayList<>();
+        input.add("8252394");
+        input.add("6269621");
+        return input;
+    }
+
+    public List<String> createSampleInput() {
+        List<String> input = new ArrayList<>();
+        input.add("5764801");   //card
+        input.add("17807724");  //door
+        return input;
+    }
+
     @Test
-    public void foo() {
-        assertEquals(0, 1);
+    public void findLoopSize() {
+        assertEquals(8, discoverLoopSize(5764801));
+
+        assertEquals(11, discoverLoopSize(17807724));
+    }
+
+    @Test
+    public void part_sample() {
+        int result = findEcnryptionKey(5764801, 17807724);
+        assertEquals(14897079, result);
+    }
+
+    @Test
+    public void part1_solution() {
+        int result = findEcnryptionKey(8252394, 6269621);
+        assertEquals(-99, result);
+
+    }
+
+    private int findEcnryptionKey(int cardPublicKey, int doorPublicKey) {
+
+        int cardLoopSize = discoverLoopSize(cardPublicKey);
+        int doorLoopSize = discoverLoopSize(doorPublicKey);
+
+        int encryptionKey_1 = calculateEncryptionKey(doorPublicKey, cardLoopSize);
+//        int encryptionKey_2 = calculateEncryptionKey(cardPublicKey, doorLoopSize);
+
+        return encryptionKey_1;
+
+
+    }
+
+    private int calculateEncryptionKey(int subjectNumber, int loopCount) {
+        long target = 1;
+        while (loopCount > 0) {
+            loopCount--;
+
+            target *= subjectNumber;
+            target %= 20201227;
+        }
+
+        return (int)target;
+    }
+
+    private int discoverLoopSize(int publicKey) {
+        int target = 1;
+        int loopCount = 0;
+        while (target != publicKey) {
+            loopCount++;
+
+            target *= SUBJECT_NUMBER;
+            target %= 20201227;
+        }
+
+        return loopCount;
     }
 }
