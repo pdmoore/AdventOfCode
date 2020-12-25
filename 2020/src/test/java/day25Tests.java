cf760b74c4d2c13b1f30f2a046ec7364c41a8db4
Paul Moore <pdmoore@gmail.com>
2020-12-24 20:59:54 -0800

    d25 - prep

---
+++ 2020/src/test/java/day25Tests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class day25Tests {
+
+    @Test
+    public void foo() {
+        assertEquals(0, 1);
+    }
+}
