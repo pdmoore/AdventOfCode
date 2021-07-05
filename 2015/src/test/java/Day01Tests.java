5248f2b3db0541d3a5eda7a444648334047a0dab
Paul Moore <pdmoore@gmail.com>
2021-07-05 14:44:49 -0700

    more descriptive test names

--- 2015/src/test/java/Day01Tests.java
+++ 2015/src/test/java/Day01Tests.java
@@ -7,25 +7,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Day01Tests {
 
     @Test
-    public void EqualNumberOfOpenAndClose() {
+    public void EqualNumberOfOpenAndClose_EndsUpAtGroundFloor() {
         assertEquals(0, something("(())"));
         assertEquals(0, something("()()"));
     }
 
     @Test
-    public void MoreOpenThanClose() {
+    public void MoreOpenThanClose_EndsUpAboveGround() {
         assertEquals(3, something("))((((("));
         assertEquals(3, something("))((((("));
     }
 
     @Test
-    public void MoreCloseThanOpen() {
+    public void MoreCloseThanOpen_EndsUpInBasement() {
         assertEquals(-1, something("())"));
         assertEquals(-1, something("))("));
         assertEquals(-3, something(")))"));
         assertEquals(-3, something(")())())"));
     }
-
 
     private int something(String input) {
         int floor = 0;
