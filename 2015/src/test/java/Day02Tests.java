108cb284f4d8702e4687811a90816040c692ac57
Paul Moore <pdmoore@gmail.com>
2021-07-07 12:55:49 -0700

    calc required wrapping paper (with duplication)

--- 2015/src/test/java/Day02Tests.java
+++ 2015/src/test/java/Day02Tests.java
@@ -23,6 +23,29 @@ A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square feet of 
         Assertions.assertEquals(42, squareFootageOf("1x1x10"));
     }
 
+    @Test
+    public void CalculateRequiredWrappingPaper() {
+        Assertions.assertEquals(58, requiredWrappingPaperFor("2x3x4"));
+        Assertions.assertEquals(43, requiredWrappingPaperFor("1x1x10"));
+    }
+
+    private int requiredWrappingPaperFor(String inputLine) {
+        String[] measurements = inputLine.split("x");
+
+        int length = Integer.parseInt(measurements[0]);
+        int width = Integer.parseInt(measurements[1]);
+        int height = Integer.parseInt(measurements[2]);
+
+        int area1 = length * width;
+        int area2 = length * height;
+        int area3 = width * height;
+        int totalArea = 2 * (area1 + area2 + area3);
+
+        int smallestArea = Math.min(Math.min(area1, area2), area3);
+
+        return totalArea + smallestArea;
+    }
+
     private int squareFootageOf(String inputLine) {
         String[] measurements = inputLine.split("x");
 
@@ -30,7 +53,10 @@ A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square feet of 
         int width = Integer.parseInt(measurements[1]);
         int height = Integer.parseInt(measurements[2]);
 
-        int totalArea = 2 * ((length * width) + (length * height) + (width * height));
+        int area1 = length * width;
+        int area2 = length * height;
+        int area3 = width * height;
+        int totalArea = 2 * (area1 + area2 + area3);
         return totalArea;
     }
 }
