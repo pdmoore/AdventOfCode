aa4fd8bbc09bfc5d51a05f30cfe12a3e55c2b1b2
Paul Moore <pdmoore@gmail.com>
2021-07-23 16:16:40 -0700

    baby step to nice finder

--- 2015/src/test/java/com/pdmoore/aoc/Day05Tests.java
+++ 2015/src/test/java/com/pdmoore/aoc/Day05Tests.java
@@ -1,5 +1,12 @@
 package com.pdmoore.aoc;
 
+import com.google.common.base.CharMatcher;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Day05Tests {
 
     /*
@@ -16,4 +23,26 @@ jchzalrnumimnmhp is naughty because it has no double letter.
 haegwjzuvuyypxyu is naughty because it contains the string xy.
 dvszwmarrgswjxmb is naughty because it contains only one vowel.
      */
+
+
+    @Test
+    public void niceString_Examples() {
+        Assertions.assertTrue(isNice("aaa"));
+        Assertions.assertTrue(isNice("ugknbfddgicrmopn"));
+    }
+
+    @Test
+    public void naughtyString_LessThanThreeVowels() {
+        Assertions.assertFalse(isNice("aa"));
+    }
+
+    private boolean isNice(String input) {
+        long count = input.chars().filter(ch -> ch == 'a').count();
+        count += input.chars().filter(ch -> ch == 'e').count();
+        count += input.chars().filter(ch -> ch == 'i').count();
+        count += input.chars().filter(ch -> ch == 'o').count();
+        count += input.chars().filter(ch -> ch == 'u').count();
+
+        return count >= 3;
+    }
 }
