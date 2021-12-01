3f60118b2a4e290b58c00a5a584887c67c0e3414
Paul Moore <pdmoore@gmail.com>
2021-11-30 21:46:53 -0800

    2021d01 - part 2 solution

--- 2021/src/test/java/day01Tests.java
+++ 2021/src/test/java/day01Tests.java
@@ -8,8 +8,6 @@ import java.util.List;
 
 public class day01Tests {
 
-
-    // count if current number is greater than preceding number
     @Test
     public void part1_example() {
         List<Integer> input = new ArrayList<>(Arrays.asList(199, 200, 208, 210, 200, 207, 240, 269, 260, 263));
@@ -26,6 +24,22 @@ public class day01Tests {
         Assertions.assertEquals(1754, actual);
     }
 
+    @Test
+    public void part2_example() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(199, 200, 208, 210, 200, 207, 240, 269, 260, 263));
+
+        int actual = sumOf3MeasurementsLargerThanPrevious(input);
+
+        Assertions.assertEquals(5, actual);
+    }
+
+    @Test
+    public void part2_solution() {
+        List<Integer> input = PuzzleInput.fileToIntegerList("./data/day01");
+        int actual = sumOf3MeasurementsLargerThanPrevious(input);
+        Assertions.assertEquals(1789, actual);
+    }
+    
     private int countMeasurementsLargerThanPrevious(List<Integer> input) {
         int largerThanPrevious = 0;
         for (int i = 1; i < input.size(); i++) {
@@ -33,6 +47,18 @@ public class day01Tests {
         }
         return largerThanPrevious;
     }
+
+    private int sumOf3MeasurementsLargerThanPrevious(List<Integer> input) {
+        int previousSum = input.get(0) + input.get(1);
+        int largerThanPrevious = 0;
+        for (int i = 2; i < input.size() - 1; i++) {
+            previousSum = previousSum + input.get(i);
+            int nextSum = previousSum - input.get(i - 2) + input.get(i + 1);
+            if (nextSum > previousSum) largerThanPrevious++;
+        }
+        return largerThanPrevious;
+    }
+
 
 
 }
