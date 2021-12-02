1667593db7d7699c297738038acf826539c3eab7
Paul Moore <pdmoore@gmail.com>
2021-12-01 21:21:38 -0800

    2021d02 - refactor

--- 2021/src/test/java/com/pdmoore/aoc/day02Tests.java
+++ 2021/src/test/java/com/pdmoore/aoc/day02Tests.java
@@ -56,6 +56,33 @@ public class day02Tests {
         assertEquals(1544000595, actual);
     }
 
+    // create a struct with horizontal, depth, aim
+    // create a function that takes a struct and command and returns a new struct
+    // return the last struct calculation of horizontal * depth
+    private int part1_processInput(List<String> input) {
+        int horizontal = 0;
+        int depth = 0;
+
+        for (String command :
+                input) {
+            String[] tokens = command.split(" ");
+            int magnitude = Integer.parseInt(tokens[1]);
+            switch (tokens[0]) {
+                case "forward":
+                    horizontal += magnitude;
+                    break;
+                case "down":
+                    depth += magnitude;
+                    break;
+                case "up":
+                    depth -= magnitude;
+                    break;
+            }
+        }
+
+        return horizontal * depth;
+    }
+
     private int part2_processInput(List<String> input) {
         int horizontal = 0;
         int depth = 0;
@@ -75,31 +102,6 @@ public class day02Tests {
                     break;
                 case "up":
                     aim -= magnitude;
-                    break;
-            }
-        }
-
-        return horizontal * depth;
-    }
-
-
-    private int part1_processInput(List<String> input) {
-        int horizontal = 0;
-        int depth = 0;
-
-        for (String command :
-                input) {
-            String[] tokens = command.split(" ");
-            int magnitude = Integer.parseInt(tokens[1]);
-            switch (tokens[0]) {
-                case "forward":
-                    horizontal += magnitude;
-                    break;
-                case "down":
-                    depth += magnitude;
-                    break;
-                case "up":
-                    depth -= magnitude;
                     break;
             }
         }
