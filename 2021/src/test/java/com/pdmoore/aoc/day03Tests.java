704b06e5c18cd3916c4fc5732029f9155caa6d5d
Paul Moore <pdmoore@gmail.com>
2021-12-02 19:01:18 -0800

    2021d03 - blank slate

---
+++ 2021/src/test/java/com/pdmoore/aoc/day03Tests.java
@@ -0,0 +1,15 @@
+package com.pdmoore.aoc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class day03Tests {
+
+
+
+    @Test
+    void day3_part1_exampl() {
+        assertEquals(1, 0);
+    }
+}
