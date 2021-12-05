e8523651e6b8e8041ae5a88591808e62b693121a
Paul Moore <pdmoore@gmail.com>
2021-12-04 22:50:19 -0800

    2021d05 - part 2 solved

--- 2021/src/test/java/com/pdmoore/aoc/day05Tests.java
+++ 2021/src/test/java/com/pdmoore/aoc/day05Tests.java
@@ -33,15 +33,48 @@ public class day05Tests {
         assertEquals(4873, actual);
     }
 
+    @Test
+    void day05_part2_example() {
+        List<String> input = PuzzleInput.asListOfStringsFrom("./data/day05_example");
+        Day05DataStructure dataStructure = new Day05DataStructure(input, true);
+
+        int actual = dataStructure.overlapsOfTwoOrMore();
+
+        assertEquals(12, actual);
+    }
+
+    @Test
+    void day05_part2_solution() {
+        List<String> input = PuzzleInput.asListOfStringsFrom("./data/day05");
+
+        Day05DataStructure dataStructure = new Day05DataStructure(input, true);
+
+        int actual = dataStructure.overlapsOfTwoOrMore();
+
+        assertEquals(19472, actual);
+    }
+
+
+
     private class Day05DataStructure {
         Map<Point, Integer> pointCount = new HashMap<Point, Integer>();
+        boolean handleDiagonals = false;
 
         public Day05DataStructure(List<String> input) {
+            processInput(input);
+        }
+
+        public Day05DataStructure(List<String> input, boolean ignored) {
+            this.handleDiagonals = true;
+            processInput(input);
+        }
+
+        private void processInput(List<String> input) {
             for (String inputLine :
                     input) {
                 String[] tokens = inputLine.split(" -> ");
                 Point startAt = createPointFrom(tokens[0]);
-                Point endAt   = createPointFrom(tokens[1]);
+                Point endAt = createPointFrom(tokens[1]);
 
                 if (startAt.y == endAt.y) {
                     int lower = Math.min(startAt.x, endAt.x);
@@ -69,6 +102,78 @@ public class day05Tests {
                             pointCount.put(thisPoint, currentCount + 1);
                         }
                     }
+                } else if (handleDiagonals) {
+                    System.out.println("Diagonal from " + startAt + " to " + endAt);
+
+                    if (startAt.x < endAt.x) {
+
+                        if (startAt.y < endAt.y) {
+                            // 498, 588 -- 503, 593
+                            System.out.println("1a Diagonal");
+                            int currentY = startAt.y;
+
+                            for (int i = startAt.x; i <= endAt.x; i++) {
+                                Point thisPoint = new Point(i, currentY);
+                                if (!pointCount.containsKey(thisPoint)) {
+                                    pointCount.put(thisPoint, 1);
+                                } else {
+                                    int currentCount = pointCount.get(thisPoint);
+                                    pointCount.put(thisPoint, currentCount + 1);
+                                }
+                                currentY++;
+                            }
+                        } else {
+                            // 627, 659 -- 763, 523
+                            System.out.println("Diagonal 1b");
+                            int currentY = startAt.y;
+                            for (int i = startAt.x; i <= endAt.x; i++) {
+                                Point thisPoint = new Point(i, currentY);
+                                if (!pointCount.containsKey(thisPoint)) {
+                                    pointCount.put(thisPoint, 1);
+                                } else {
+                                    int currentCount = pointCount.get(thisPoint);
+                                    pointCount.put(thisPoint, currentCount + 1);
+                                }
+                                currentY--;
+                            }
+                        }
+                    } else {
+
+                        // guaranteed that start.x > end.x
+                        if (startAt.y < endAt.y) {
+                            // 861, 137 -- 102, 896
+                            System.out.println("Diagonal 2a");
+                            int currentY = startAt.y;
+                            for (int i = startAt.x; i >= endAt.x; i--) {
+                                Point thisPoint = new Point(i, currentY);
+                                if (!pointCount.containsKey(thisPoint)) {
+                                    pointCount.put(thisPoint, 1);
+                                } else {
+                                    int currentCount = pointCount.get(thisPoint);
+                                    pointCount.put(thisPoint, currentCount + 1);
+                                }
+                                currentY++;
+                            }
+                        } else {
+                            //629, 581   to 123, 75
+                            System.out.println("Diagonal 2b");
+
+                            int currentY = startAt.y;
+
+                            for (int i = startAt.x; i >= endAt.x; i--) {
+                                Point thisPoint = new Point(i, currentY);
+                                if (!pointCount.containsKey(thisPoint)) {
+                                    pointCount.put(thisPoint, 1);
+                                } else {
+                                    int currentCount = pointCount.get(thisPoint);
+                                    pointCount.put(thisPoint, currentCount + 1);
+                                }
+                                currentY--;
+                            }
+                        }
+
+                    }
+
                 }
             }
         }
