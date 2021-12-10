054b8e0e7ffb12d74cdfaccd7fae915d703ff389
Paul Moore <pdmoore@gmail.com>
2021-12-10 14:44:34 -0800

    2021d06 - starting point

---
+++ 2021/src/test/java/com/pdmoore/aoc/day06Tests.java
@@ -0,0 +1,14 @@
+package com.pdmoore.aoc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class day06Tests {
+
+
+    @Test
+    void day06_part1_example() {
+        assertEquals(0, 1);
+    }
+}
--- PuzzleInput/src/main/java/com/pdmoore/aoc/PuzzleInput.java
+++ PuzzleInput/src/main/java/com/pdmoore/aoc/PuzzleInput.java
@@ -8,7 +8,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.math.BigInteger;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
