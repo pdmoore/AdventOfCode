5e46a77053620195364651a5c8cd2354239e9e91
Paul Moore <pdmoore@gmail.com>
2021-12-14 14:49:25 -0800

    2021d07 - starting point

--- 2021/src/test/java/com/pdmoore/aoc/day06Tests.java
+++ 2021/src/test/java/com/pdmoore/aoc/day06Tests.java
@@ -4,13 +4,17 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class day06Tests {
 
     String example_input = "3,4,3,1,2";
+
+    //TODO - part 1 can be solved by the part2 logic (School class)
 
     @Test
     void day06_part1_example_populate() {
@@ -26,7 +30,7 @@ public class day06Tests {
         lanternFish = tickMany(lanternFish, 18);
 
         assertEquals(26, lanternFish.size());
-}
+    }
 
     @Test
     void day06_part1_example_tick80() {
@@ -46,14 +50,48 @@ public class day06Tests {
         assertEquals(386755, lanternFish.size());
     }
 
-//    @Test
-//    void day06_part1_tick256() {
-//        List<Integer> lanternFish = populate(PuzzleInput.asStringFrom("data/day06"));
-//
-//        lanternFish = tickMany(lanternFish, 256);
-//
-//        assertEquals(26984457539, lanternFish.size());
-//    }
+    @Test
+    void day06_part2_example_InitialCount() {
+        School lanternFish = new School(example_input);
+
+        Integer expectedInt = 5; // 5 lanternFish total
+        BigInteger expected = BigInteger.valueOf(expectedInt.intValue());
+
+        assertEquals(expected, lanternFish.count());
+    }
+
+    @Test
+    void day06_part2_example_tick18() {
+        School lanternFish = new School(example_input);
+
+        lanternFish.tickMany(18);
+
+        BigInteger expected = new BigInteger("26");
+
+        assertEquals(expected, lanternFish.count());
+    }
+
+    @Test
+    void day06_part2_example_tick256() {
+        School lanternFish = new School(example_input);
+
+        lanternFish.tickMany(256);
+
+        BigInteger expected = new BigInteger("26984457539");
+
+        assertEquals(expected, lanternFish.count());
+    }
+
+    @Test
+    void day06_part2_tick256() {
+        School lanternFish = new School(PuzzleInput.asStringFrom("data/day06"));
+
+        lanternFish.tickMany(256);
+
+        BigInteger expected = new BigInteger("1732731810807");
+
+        assertEquals(expected, lanternFish.count());
+    }
 
 
     private List<Integer> tickMany(List<Integer> lanternFish, int ticks) {
@@ -84,14 +122,70 @@ public class day06Tests {
         return nextFishes;
     }
 
-
     private List<Integer> populate(String input) {
         String[] numbersAsString = input.split(",");
         List<Integer> numbers = new ArrayList<>();
         for (String numberString :
                 numbersAsString) {
-            numbers.add(new Integer(numberString));
+            numbers.add(Integer.parseInt(numberString));
         }
         return numbers;
+    }
+
+    private class School {
+
+        Map<Integer, BigInteger> fishes = new HashMap<>();
+
+        public School(String input) {
+            fishes = createEmptyFishMap();
+
+            String[] numbersAsString = input.split(",");
+            List<Integer> numbers = new ArrayList<>();
+            for (String numberString :
+                    numbersAsString) {
+                int fishValue = Integer.parseInt(numberString);
+                BigInteger currentCount = fishes.get(fishValue);
+                fishes.put(fishValue, currentCount.add(BigInteger.ONE));
+            }
+        }
+
+        private Map<Integer, BigInteger> createEmptyFishMap() {
+            Map<Integer, BigInteger> newSchool = new HashMap<>();
+            for (int i = 0; i <= 8; i++) {
+                newSchool.put(i, BigInteger.ZERO);
+            }
+            return newSchool;
+        }
+
+        public void tickMany(int tickCount) {
+            for (int i = 0; i < tickCount; i++) {
+                tick();
+            }
+        }
+
+        private void tick() {
+            Map<Integer, BigInteger> newSchool = createEmptyFishMap();
+            BigInteger newFishCount = fishes.get(0); // these become the new 8, also added to 6
+            fishes.put(0, fishes.get(1));
+            fishes.put(1, fishes.get(2));
+            fishes.put(2, fishes.get(3));
+            fishes.put(3, fishes.get(4));
+            fishes.put(4, fishes.get(5));
+            fishes.put(5, fishes.get(6));
+            fishes.put(6, fishes.get(7));
+            fishes.put(7, fishes.get(8));
+            fishes.put(8, newFishCount);
+            BigInteger currentAt6 = fishes.get(6);
+            fishes.put(6, currentAt6.add(newFishCount));
+        }
+
+        public BigInteger count() {
+            BigInteger count = BigInteger.ZERO;
+            for (int i = 0; i < fishes.size(); i++) {
+                BigInteger thisFishCount = fishes.get(i);
+                count = count.add(thisFishCount);
+            }
+            return count;
+        }
     }
 }
---
+++ 2021/src/test/java/com/pdmoore/aoc/day07Tests.java
@@ -0,0 +1,13 @@
+package com.pdmoore.aoc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class day07Tests {
+
+    @Test
+    void day06_part1_example() {
+        assertEquals(1, 0);
+    }
+}
