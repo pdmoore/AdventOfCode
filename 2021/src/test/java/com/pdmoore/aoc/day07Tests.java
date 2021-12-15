4482cb0f11bf8a547f5a33fbd787b1d193057db6
Paul Moore <pdmoore@gmail.com>
2021-12-14 16:00:31 -0800

    20201d07 - part 2 solved

--- 2021/src/test/java/com/pdmoore/aoc/day07Tests.java
+++ 2021/src/test/java/com/pdmoore/aoc/day07Tests.java
@@ -10,18 +10,6 @@ public class day07Tests {
 
     final String example_input = "16,1,2,0,4,2,7,1,2,14";
 
-    // Calc difference between mode and position
-    // Sum all differences
-
-    @Test
-    void day06_part1_example_findMode() {
-        int[] horizontalPositions = processInput(example_input);
-
-        int mode = findModeOf(horizontalPositions);
-
-        assertEquals(2, mode);
-    }
-
     @Test
     void day06_part01_example() {
         int[] horizontalPositions = processInput(example_input);
@@ -35,6 +23,44 @@ public class day07Tests {
         int actual = cheapestAlignmentByCheckingAll(horizontalPositions);
 
         assertEquals(364898, actual);
+    }
+
+    @Test
+    void day06_part2_increasingFuelRateCalculation() {
+        assertEquals(1, calculateFuelFor(4, 5));
+        assertEquals(3, calculateFuelFor(3, 5));
+        assertEquals(6, calculateFuelFor(2, 5));
+        assertEquals(10, calculateFuelFor(1, 5));
+        assertEquals(15, calculateFuelFor(0, 5));
+        assertEquals(45, calculateFuelFor(14, 5));
+        assertEquals(66, calculateFuelFor(16, 5));
+    }
+
+    @Test
+    void day06_part02_example() {
+        int[] horizontalPositions = processInput(example_input);
+        int actual = cheapestAlignmentByCheckingAll_part2(horizontalPositions);
+        assertEquals(168, actual);
+    }
+
+    @Test
+    void day06_part02_solution() {
+        int[] horizontalPositions = processInput(PuzzleInput.asStringFrom("data/day07"));
+        int actual = cheapestAlignmentByCheckingAll_part2(horizontalPositions);
+
+        assertEquals(104149091, actual);
+    }
+
+    private int calculateFuelFor(int currentPosition, int basePosition) {
+        int fuelRate = 1;
+        int lower = Math.min(currentPosition, basePosition);
+        int upper = Math.max(currentPosition, basePosition);
+        int consumption = 0;
+        for (int i = lower; i < upper; i++) {
+            consumption += fuelRate;
+            fuelRate++;
+        }
+        return consumption;
     }
 
     private int cheapestAlignmentByCheckingAll(int[] integers) {
@@ -56,10 +82,33 @@ public class day07Tests {
         return cheapest;
     }
 
-    private int cheapestAlignmentViaMode(int[] integers) {
-        int mode = findModeOf(integers);
+    private int cheapestAlignmentByCheckingAll_part2(int[] integers) {
+        int cheapest = Integer.MAX_VALUE;
+        List<Integer> seenIntegers = new ArrayList<>();
 
-        return alignmentComparedTo(integers, mode);
+        int max_position = Arrays.stream(integers).max().getAsInt();
+        for (int position = 0; position < max_position; position++) {
+            if (!seenIntegers.contains(position)) {
+                seenIntegers.add(position);
+
+                int alignmentValue = alignmentWithIncreasingRate(integers, position);
+                if (alignmentValue <= cheapest) {
+                    cheapest = alignmentValue;
+                }
+            }
+        }
+
+        return cheapest;
+    }
+
+    private int alignmentWithIncreasingRate(int[] integers, int basePosition) {
+        int sum = 0;
+        for (int position :
+                integers) {
+            int difference = calculateFuelFor(position, basePosition);
+            sum += difference;
+        }
+        return sum;
     }
 
     private int alignmentComparedTo(int[] integers, int basePosition) {
@@ -72,43 +121,11 @@ public class day07Tests {
         return sum;
     }
 
-    class Mode
-    {
-        public int Value;
-        public int Count;
-    }
-
-    private int findModeOf(int[] integers) {
-        Mode best = new Mode();
-        Map<Integer, Integer> hashtable =
-                new HashMap<>();
-
-        for (int value :
-                integers) {
-            int curCount;
-            int count = 1;
-            if (hashtable.containsKey(value)) {
-                curCount = hashtable.get(value);
-                count = curCount + 1;
-                hashtable.put(value, count);
-            } else {
-                hashtable.put(value, 1);
-            }
-
-            if (count > best.Count)
-            {
-                best.Value = value;
-                best.Count = count;
-            }
-        }
-
-        return best.Value;
-    }
-
+    // TODO
+    // MOVE TO PUZZLIEINPUT
     private int[] processInput(String input) {
         return Arrays.stream(
                 input.split(",")).
                 mapToInt(Integer::parseInt).toArray();
     }
-
 }
