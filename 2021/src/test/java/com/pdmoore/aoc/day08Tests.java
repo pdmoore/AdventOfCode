e5bd79261a361b95ee853adc202528cbb4ae8d6a
Paul Moore <pdmoore@gmail.com>
2021-12-19 14:08:09 -0800

    2021d08 - part 1 starting point

---
+++ 2021/src/test/java/com/pdmoore/aoc/day08Tests.java
@@ -0,0 +1,12 @@
+package com.pdmoore.aoc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class day08Tests {
+
+    @Test
+    void day08_part1_example() {
+        Assertions.assertEquals(1, 0);
+    }
+}
