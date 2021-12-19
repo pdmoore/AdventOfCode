deb43a9391727009c77c34fa240c89d012e3725b
Paul Moore <pdmoore@gmail.com>
2021-12-19 14:19:21 -0800

    2021d08 - part 1 example solved

--- 2021/src/test/java/com/pdmoore/aoc/day08Tests.java
+++ 2021/src/test/java/com/pdmoore/aoc/day08Tests.java
@@ -1,12 +1,39 @@
 package com.pdmoore.aoc;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class day08Tests {
 
     @Test
     void day08_part1_example() {
-        Assertions.assertEquals(1, 0);
+        List<String> input = PuzzleInput.asListOfStringsFrom("data/day08_example");
+
+        int actual = countUniqueSegments(input);
+
+        assertEquals(26, actual);
+    }
+
+    private int countUniqueSegments(List<String> input) {
+        int uniqueSegmentCount = 0;
+
+        for (String line :
+                input) {
+            String rightHandSide = line.split(Pattern.quote("|"))[1];
+            String[] segments = rightHandSide.split(" ");
+            for (String segment :
+                    segments) {
+                int segmentLength = segment.length();
+                if (segmentLength == 2 || segmentLength == 3 || segmentLength == 4 || segmentLength == 7) {
+                    uniqueSegmentCount++;
+                }
+            }
+        }
+
+        return uniqueSegmentCount;
     }
 }
