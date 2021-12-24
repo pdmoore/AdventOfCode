1d2e51890b07b26e04411d5051c1031418e46c4f
Paul Moore <pdmoore@gmail.com>
2021-12-24 15:29:55 -0800

    2021d09 - part 2 solved

--- 2021/src/test/java/com/pdmoore/aoc/day09Tests.java
+++ 2021/src/test/java/com/pdmoore/aoc/day09Tests.java
@@ -2,6 +2,8 @@ package com.pdmoore.aoc;
 
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -24,6 +26,26 @@ public class day09Tests {
         int actual = sumRiskLevel(input);
 
         assertEquals(516, actual);
+    }
+
+    @Test
+    void day09_part2_example() {
+        List<String> input = PuzzleInput.asListOfStringsFrom("data/day09_example");
+
+        LavaTube sut = new LavaTube(input);
+        int actual = sut.threeLargestBasinsMultiplied();
+
+        assertEquals(1134, actual);
+    }
+
+    @Test
+    void day09_part2_solution() {
+        List<String> input = PuzzleInput.asListOfStringsFrom("data/day09");
+
+        LavaTube sut = new LavaTube(input);
+        int actual = sut.threeLargestBasinsMultiplied();
+
+        assertEquals(1023660, actual);
     }
 
     private int sumRiskLevel(List<String> input) {
@@ -84,5 +106,70 @@ public class day09Tests {
             }
         }
         return locations;
+    }
+
+    private class LavaTube {
+        private final int[][] _locations;
+
+        public LavaTube(List<String> input) {
+            _locations = as2dIntArray(input);
+        }
+
+        public int threeLargestBasinsMultiplied() {
+            int firstLargestBasin = 0;
+            int secondLargestBasin = 0;
+            int thirdLargestBasin = 0;
+
+            for (int x = 0; x < _locations.length; x++) {
+                for (int y = 0; y < _locations[0].length; y++) {
+                    if (isLowPoint(_locations, x, y)) {
+
+                        Point starting = new Point(x, y);
+                        List<Point> visited = new ArrayList<>();
+                        int size = calcBasinSize(x, y, 0, visited);
+
+                        if (size >= firstLargestBasin) {
+                            thirdLargestBasin = secondLargestBasin;
+                            secondLargestBasin = firstLargestBasin;
+                            firstLargestBasin = size;
+                        } else if (size >= secondLargestBasin) {
+                            thirdLargestBasin = secondLargestBasin;
+                            secondLargestBasin = size;
+                        } else if (size > thirdLargestBasin) {
+                            thirdLargestBasin = size;
+                        }
+                    }
+                }
+            }
+
+            return firstLargestBasin * secondLargestBasin * thirdLargestBasin;
+        }
+
+        private int calcBasinSize(int x, int y, int size, List<Point> visited) {
+            Point here = new Point(x, y);
+            if (visited.contains(here)) {
+                return 0;
+            }
+            if (_locations[x][y] == 9) {
+                return 0;
+            }
+
+            visited.add(here);
+            size += 1;
+
+            if (y > 0) {
+                size += calcBasinSize(x, y-1, 0, visited);
+            }
+            if (y < _locations[0].length - 1) {
+                size += calcBasinSize(x, y+1, 0, visited);
+            }
+            if (x > 0) {
+                size += calcBasinSize(x - 1, y, 0, visited);
+            }
+            if (x < _locations.length - 1) {
+                size += calcBasinSize(x + 1, y, 0, visited);
+            }
+            return size;
+        }
     }
 }
