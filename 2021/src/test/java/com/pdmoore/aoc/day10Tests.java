5822490d825a0a6bea908028c0efb5c969b0e815
Paul Moore <pdmoore@gmail.com>
2021-12-24 20:43:11 -0800

    2021d10 - starting point

---
+++ 2021/src/test/java/com/pdmoore/aoc/day10Tests.java
@@ -0,0 +1,13 @@
+package com.pdmoore.aoc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class day10Tests {
+
+    @Test
+    void part1_example() {
+
+        Assertions.assertEquals(1, 0);
+    }
+}
