bc76f54075539b12d5ff061e7573c02d182c7d1f
Paul Moore <pdmoore@gmail.com>
2021-12-24 20:44:55 -0800

    2021d10 - first failing test

--- 2021/src/test/java/com/pdmoore/aoc/day10Tests.java
+++ 2021/src/test/java/com/pdmoore/aoc/day10Tests.java
@@ -3,11 +3,20 @@ package com.pdmoore.aoc;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class day10Tests {
 
     @Test
     void part1_example() {
+        List<String> input = PuzzleInput.asStringListFrom("data/day10_example");
 
-        Assertions.assertEquals(1, 0);
+        int actual = calcSyntaxErrorScore(input);
+
+        Assertions.assertEquals(26397, actual);
+    }
+
+    private int calcSyntaxErrorScore(List<String> input) {
+        return 0;
     }
 }
