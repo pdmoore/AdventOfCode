189b3d5ccb069b8f752f6320110bb326ee034532
Paul Moore <pdmoore@gmail.com>
2022-01-03 19:55:31 -0800

    2021d13 - part 1 example, not handling x folds yet

--- 2021/src/test/java/com/pdmoore/aoc/day13Tests.java
+++ 2021/src/test/java/com/pdmoore/aoc/day13Tests.java
@@ -1,9 +1,12 @@
 package com.pdmoore.aoc;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,7 +15,7 @@ public class day13Tests {
     @Test
     void day13_part1_example_FirstFoldOnly() {
         List<String> input = PuzzleInput.asStringListFrom("data/day13_example");
-        List<String> dotLocations = getDotsFrom(input);
+        Set<Point> dotLocations = buildSet(getDotsFrom(input));
         List<String> foldInstructions = getFoldsFrom(input);
 
         int actual = processFold(dotLocations, foldInstructions.get(0));
@@ -20,15 +23,73 @@ public class day13Tests {
         assertEquals(17, actual);
     }
 
-    private int processFold(List<String> dotLocations, String foldInstruction) {
-        return 0;
-    }
-
-    private List<String> getFoldsFrom(List<String> input) {
-        return null;
+    private Set<Point> buildSet(List<String> locationsAsStrings) {
+        Set<Point> dotLocations = new HashSet<>();
+        for (String location :
+                locationsAsStrings) {
+            String[] split = location.split(",");
+            Point p = new Point(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
+            dotLocations.add(p);
+        }
+        
+        return dotLocations;
     }
 
     private List<String> getDotsFrom(List<String> input) {
-        return null;
+        List<String> dotLocations = new ArrayList<>();
+        for (String inputLine :
+                input) {
+            if (inputLine.isEmpty()) {
+                return dotLocations;
+            }
+            dotLocations.add(inputLine);
+        }
+        return dotLocations;
     }
+
+    private List<String> getFoldsFrom(List<String> input) {
+        List<String> foldInstructions = new ArrayList<>();
+        for (String inputLine :
+                input) {
+            if (inputLine.startsWith("fold along")) {
+                foldInstructions.add(inputLine);
+                return foldInstructions;
+            }
+        }
+        return foldInstructions;
+    }
+
+    private int processFold(Set<Point> dotLocations, String foldInstruction) {
+
+        Character axis = foldInstruction.charAt(11);
+        int foldValue = Integer.parseInt(foldInstruction.split("=")[1]);
+
+        Set<Point> newLocations = new HashSet<>();
+
+        if (axis.equals('x')) {
+            throw new UnsupportedOperationException("TODO - handle x axis");
+        } else {
+            for (Point p:
+                 dotLocations) {
+                if (p.y < foldValue) {
+                    newLocations.add(p);
+                } else {
+                    int newY = p.y - foldValue + 1;
+                    Point folded = new Point(p.x, newY);
+                    newLocations.add(folded);
+                }
+            }
+
+        }
+
+        // determine x or y
+
+        // process each dotLocation per the fold
+
+        // count the number of dots in the resulting set
+
+
+        return newLocations.size();
+    }
+
 }
