6a03e7e35fdb878a6212c218ed8f544ea41462c9
Paul Moore <pdmoore@gmail.com>
2022-01-07 13:40:14 -0800

    2021-d14 - refactor to handle big polymers

--- 2021/src/test/java/com/pdmoore/aoc/day14Tests.java
+++ 2021/src/test/java/com/pdmoore/aoc/day14Tests.java
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,7 +15,7 @@ public class day14Tests {
     void day14_part1_example() {
         List<String> input = PuzzleInput.asStringListFrom("data/day14_example");
 
-        String transformed = solvePart1(input);
+        List<Character> transformed = solve(input);
         Map<Character, BigInteger> occurrences = countOccurrences(transformed);
         BigInteger actual = countOfMostCommon(occurrences).subtract(countOfLeastCommmon(occurrences));
 
@@ -29,7 +27,7 @@ public class day14Tests {
     void day14_part1() {
         List<String> input = PuzzleInput.asStringListFrom("data/day14");
 
-        String transformed = solvePart1(input);
+        List<Character> transformed = solve(input);
         Map<Character, BigInteger> occurrences = countOccurrences(transformed);
         BigInteger actual = countOfMostCommon(occurrences).subtract(countOfLeastCommmon(occurrences));
 
@@ -42,15 +40,8 @@ public class day14Tests {
     void day14_part2_example() {
         List<String> input = PuzzleInput.asStringListFrom("data/day14_example");
 
-        // Need to deal with character array and BigInteger
-        // LinkedList<Character> maybe?
-        // max string length is 2,147,483,647 characters
-        // May also need to spread the polymer across multiple "large" strings
-        // and then work across those one-by-one
-        // Probably solve part 1 with the new logic first, then expand to 40 iterations
-
-        String transformed = solvePart1(input);
-        Map<Character, BigInteger> occurrences = countOccurrences(transformed);
+        List<Character> part2_solution = solve(input);
+        Map<Character, BigInteger> occurrences = countOccurrences(part2_solution);
         BigInteger actual = countOfMostCommon(occurrences).subtract(countOfLeastCommmon(occurrences));
 
         BigInteger expected = new BigInteger("2188189693529");
@@ -58,42 +49,45 @@ public class day14Tests {
         assertEquals(expected, actual);
     }
 
-    // TODO - convert this to use List<Character> instead of string...
-    // probably add as third param, switch over, then remove existing 'String polymer' param
-    // Will eventually hit countOccurrences and CountOf
-    private String applyTransformations(Map<String, Character> transforms, String polymer) {
-        StringBuilder result = new StringBuilder();
-        result.append(polymer.charAt(0));
-        for (int i = 0; i < polymer.length() - 1; i++) {
-            String key = polymer.substring(i, i+2);
-            result.append(transforms.get(key));
-            result.append(polymer.charAt(i+1));
+    private List<Character> applyTransformations(Map<String, Character> transforms, List<Character> polymer) {
+        List<Character> result = new LinkedList<>();
+        result.add(polymer.get(0));
+
+        for (int i = 0; i < polymer.size() - 1; i++) {
+            String key = "" + polymer.get(i) + polymer.get(i+1);
+            result.add(transforms.get(key));
+            result.add(polymer.get(i+1));
         }
 
-        return result.toString();
-    }
-
-    private Map<Character, BigInteger> countOccurrences(String transformed) {
-        Map<Character, BigInteger> result = new HashMap<>();
-        for (int i = 0; i < transformed.length(); i++) {
-            Character key = transformed.charAt(i);
-            if (!result.containsKey(key)) {
-                result.put(key, countOf(key, transformed));
-            }
-        }
         return result;
     }
 
-    private BigInteger countOf(Character key, String polymer) {
+    private Map<Character, BigInteger> countOccurrences(List<Character> part2_solution) {
+        Map<Character, BigInteger> result = new HashMap<>();
+
+        for (Character c:
+             part2_solution) {
+            if (!result.containsKey(c)) {
+                result.put(c, countOf(c, part2_solution));
+            }
+        }
+
+        return result;
+    }
+
+    private BigInteger countOf(Character key, List<Character> polymer) {
         BigInteger count = BigInteger.ZERO;
-        for (int i = 0; i < polymer.length(); i++) {
-            if (key.equals(polymer.charAt(i)))
+
+        for (Character c :
+                polymer) {
+            if (key.equals(c))
                 count = count.add(BigInteger.ONE);
         }
+
         return count;
     }
 
-    private String solvePart1(List<String> input) {
+    private List<Character> solve(List<String> input) {
         String polymerTemplate = input.get(0);
 
         Map<String, Character> pairInsertions = new HashMap<>();
@@ -104,7 +98,7 @@ public class day14Tests {
             pairInsertions.put(key, value);
         }
 
-        String polymer = polymerTemplate;
+        List<Character> polymer = polymerTemplate.chars().mapToObj(c -> (char)c).collect(Collectors.toList());
         for (int i = 1; i <= 10; i++) {
             polymer = applyTransformations(pairInsertions, polymer);
         }
