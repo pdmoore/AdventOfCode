d40a1b5b1525793e30a196f1416cafe2c20a056e
Paul Moore <pdmoore@gmail.com>
2022-01-15 10:37:00 -0800

    2021d15 - part 2 example working with expanded cavern map

--- 2021/src/test/java/com/pdmoore/aoc/day15Tests.java
+++ 2021/src/test/java/com/pdmoore/aoc/day15Tests.java
@@ -3,9 +3,9 @@ package com.pdmoore.aoc;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
-import java.awt.*;
-import java.util.*;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -33,6 +33,29 @@ public class day15Tests {
         assertEquals(441, actual);
     }
 
+    @Test
+    void day15_part2_example() {
+        int[][] input = PuzzleInput.as2dIntArray("data/day15_example");
+        int[][] fiveTimes = expandInput(input);
+
+        CavernMap sut = new CavernMap(fiveTimes);
+        int actual = sut.findLowestRiskPath();
+
+        assertEquals(315, actual);
+    }
+
+    @Test
+    void day15_part2() {
+        int[][] input = PuzzleInput.as2dIntArray("data/day15");
+        int[][] fiveTimes = expandInput(input);
+
+        CavernMap sut = new CavernMap(fiveTimes);
+        int actual = sut.findLowestRiskPath();
+
+        // gives 2045 - too low
+        assertEquals(-99, actual);
+    }
+
     private class CavernMap {
         private final int[][] cavernMap;
 
@@ -55,10 +78,12 @@ public class day15Tests {
                     cavernMap[popped.x][popped.y] = 0;
 
                     List<Node> neighbors = gatherUnvisitedNeighbors(popped);
+                    // Works for part 1, well under a second
                     for (Node n :
                             neighbors) {
                         if (!pq.contains(n)) pq.add(n);
                     }
+                    // Following works, takes around 8 seconds for part1
 //                    pq.addAll(neighbors);
                 }
             }
@@ -71,25 +96,25 @@ public class day15Tests {
 
             int newX = current.x - 1;
             int newY = current.y;
-            if((newX > 0 && newX < cavernMap.length) && cavernMap[newX][newY] != 0) {
+            if ((newX > 0 && newX < cavernMap.length) && cavernMap[newX][newY] != 0) {
                 neighbors.add(new Node(newX, newY, current.totalRiskFromStart + cavernMap[newX][newY]));
             }
 
             newX = current.x + 1;
             newY = current.y;
-            if((newX > 0 && newX < cavernMap.length) && cavernMap[newX][newY] != 0) {
+            if ((newX > 0 && newX < cavernMap.length) && cavernMap[newX][newY] != 0) {
                 neighbors.add(new Node(newX, newY, current.totalRiskFromStart + cavernMap[newX][newY]));
             }
 
             newX = current.x;
             newY = current.y - 1;
-            if((newY > 0 && newY < cavernMap.length) && cavernMap[newX][newY] != 0) {
+            if ((newY > 0 && newY < cavernMap.length) && cavernMap[newX][newY] != 0) {
                 neighbors.add(new Node(newX, newY, current.totalRiskFromStart + cavernMap[newX][newY]));
             }
 
             newX = current.x;
             newY = current.y + 1;
-            if((newY > 0 && newY < cavernMap.length) && cavernMap[newX][newY] != 0) {
+            if ((newY > 0 && newY < cavernMap.length) && cavernMap[newX][newY] != 0) {
                 neighbors.add(new Node(newX, newY, current.totalRiskFromStart + cavernMap[newX][newY]));
             }
 
@@ -124,5 +149,55 @@ public class day15Tests {
 
             }
         }
+    }
+
+    private int[][] expandInput(int[][] input) {
+        int[][] expanded = new int[input.length * 5][input.length * 5];
+
+        // 0..input.length
+        // 1-4 across
+        // 2-5 down
+
+        // Copy the original array into the upper left
+        for (int x = 0; x < input.length; x++) {
+            for (int y = 0; y < input.length; y++) {
+                expanded[x][y] = input[x][y];
+            }
+        }
+
+        // across the first "row" of arrays, copy 2 from 1, 3 from 2, 4 from 3, 5 from 4
+        for (int across = 0; across <= 3; across++) {
+            for (int x = 0; x < input.length; x++) {
+                for (int y = 0; y < input.length; y++) {
+                    int newX = x;
+                    int newY = y + input.length + (across * 10);
+
+                    expanded[newX][newY] = Math.max(1, ((expanded[newX][newY - input.length]) + 1) % 10);
+                }
+            }
+        }
+
+
+        // down the columns, and across the rows, copy from the one immediately above
+        // do this 4 times
+        int downOffset = input.length;
+        for (int down = 1; down < 5; down++) {
+
+            downOffset = down * input.length;
+
+            for (int across = 0; across <= 4; across++) {
+                for (int x = 0; x < input.length; x++) {
+                    for (int y = 0; y < input.length; y++) {
+                        int newX = x + downOffset;
+                        int newY = y + (across * input.length);
+
+                        expanded[newX][newY] = Math.max(1, ((expanded[newX - input.length][newY]) + 1) % 10);
+                    }
+                }
+            }
+        }
+
+
+        return expanded;
     }
 }
