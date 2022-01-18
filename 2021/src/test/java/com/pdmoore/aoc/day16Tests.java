cc152603e598214f22b9898081eccf4d6f948bd3
Paul Moore <pdmoore@gmail.com>
2022-01-17 20:16:40 -0800

    2021d16 - part 1 literal value in progress

--- 2021/src/test/java/com/pdmoore/aoc/day16Tests.java
+++ 2021/src/test/java/com/pdmoore/aoc/day16Tests.java
@@ -47,7 +47,24 @@ VVVTTTAAAAABBBBBCCCCC
             version = Integer.parseInt(binaryString.substring(0, 3), 2);
             typeID = Integer.parseInt(binaryString.substring(3, 6), 2);
 
-            
+            if (typeID == 4) {
+//                110 100 10111 11110 00101 000
+//                VVV TTT AAAAA BBBBB CCCCC
+
+                StringBuilder literalString = new StringBuilder();
+                // start grabbing 5 characters at a time from position
+                String aaa = binaryString.substring(6,  11);
+                literalString.append(aaa.substring(1, 5));
+                String bbb = binaryString.substring(11, 16);
+                literalString.append(bbb.substring(1, 5));
+                String ccc = binaryString.substring(16, 21);
+                literalString.append(ccc.substring(1, 5));
+
+                literalValue = Integer.parseInt(literalString.toString(), 2);
+
+            }
+
+
         }
     }
 }
