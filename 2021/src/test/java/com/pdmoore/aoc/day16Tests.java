85b71eb723996c61ca1f45f4d4d1d8f3c8ee9c85
Paul Moore <pdmoore@gmail.com>
2022-01-18 21:54:45 -0800

    2021d16 - part 1, towards Message impl

--- 2021/src/test/java/com/pdmoore/aoc/day16Tests.java
+++ 2021/src/test/java/com/pdmoore/aoc/day16Tests.java
@@ -40,17 +40,6 @@ VVVTTTAAAAABBBBBCCCCC
     // repeat until end of string
 
 
-// Switching over to Message as container of Packets, not directly instantiating packets
-//    @Test
-//    void part1_decode_literal() {
-//        String input = "D2FE28";
-//
-//        Packet actual = new Packet(input);
-//
-//        assertEquals(6, actual.version);
-//        assertEquals(4, actual.typeID);
-//        assertEquals(2021, actual.literalValue);
-//    }
 
     @Test
     void part1_decode_literal_via_Message() {
@@ -84,29 +73,42 @@ VVVTTTAAAAABBBBBCCCCC
         assertEquals(9, actual.sumOfVersions());
     }
 
-
-    //TODO - parse some of the operator examples
     @Test
-    @Disabled
     void part1_decode_operator_LengthType0() {
         String input = "38006F45291200";
 
-        Packet actual = new Packet(input);
+        Message message = new Message(input);
+        Packet actual = message.packets.get(0);
 
         assertEquals(1, actual.version);
         assertEquals(6, actual.typeID);
         assertEquals(0, actual.lengthTypeId);
-        assertEquals(27, actual.subPacketLength);
-
-        //TODO - expect a List of 2 sub-packets
     }
+
+    @Test
+    void part1_decode_operator_LengthType1() {
+        String input = "EE00D40C823060";
+
+        Message message = new Message(input);
+        Packet actual = message.packets.get(0);
+
+        assertEquals(7, actual.version);
+        assertEquals(3, actual.typeID);
+        assertEquals(1, actual.lengthTypeId);
+
+        //TODO Left off here, need to start thinking about how to handle Length Type 1
+        // and the possibility of packets within packets and unknown length...
+        assertEquals(4, message.packets.size());
+    }
+
+
 
     @Test
     @Disabled
     void part1_versionSums_examples() {
         assertEquals(16, new Message("8A004A801A8002F478").sumOfVersions());
-        assertEquals(23, new Message("C0015000016115A2E0802F182340").sumOfVersions());
-        assertEquals(31, new Message("A0016C880162017C3686B18A3D4780").sumOfVersions());
+//        assertEquals(23, new Message("C0015000016115A2E0802F182340").sumOfVersions());
+//        assertEquals(31, new Message("A0016C880162017C3686B18A3D4780").sumOfVersions());
     }
 
     private class Packet {
@@ -115,71 +117,6 @@ VVVTTTAAAAABBBBBCCCCC
         public int literalValue;
         public int lengthTypeId;
         public int subPacketLength;
-
-        public Packet(String input) {
-
-/*
-            String binaryString = new BigInteger(input, 16).toString(2);
-
-            // Didn't see this explicitly called out, but was part of the second example
-            // pre-pend zeros until there are an evenly % 4 number of bits
-            int fourBits = binaryString.length() % 4;
-            for (int i = 0; i < fourBits; i++) {
-                binaryString = "0" + binaryString;
-            }
-
-            version = Integer.parseInt(binaryString.substring(0, 3), 2);
-            typeID = Integer.parseInt(binaryString.substring(3, 6), 2);
-
-
-            //0011 1000 0000 0000 0110 1111 0100 0101 0010 1001 0001 0010 0000 0000
-            //  11 1000 0000 0000 0110 1111 0100 0101 0010 1001 0001 0010 0000 0000
-
-            if (typeID == 4) {
-                //Literal value
-                int pos = 6;
-
-                StringBuilder literalString = new StringBuilder();
-                boolean leadCharacterIsOne;
-                do {
-                    String group = binaryString.substring(pos,  pos + 5);
-                    literalString.append(group.substring(1, 5));
-                    leadCharacterIsOne = group.charAt(0) == '1';
-                    pos += 5;
-                } while (leadCharacterIsOne);
-
-                literalValue = Integer.parseInt(literalString.toString(), 2);
-            } else {
-                // Operator packet
-                // contains hierarchy of sub-packets
-                //TODO - replace hard-coded offsets with index,
-                // increment index after each piece (not all at once at bottom)
-                String lengthTypeId = binaryString.substring(6, 7);   // really want a single 0 or 1
-
-                if (lengthTypeId.equals("0")) {
-                    // next 15 bits represent the total length in bits of the sub-packets contained by this packet
-                    // sub-packets appear after the 15 bits
-                    String substring = binaryString.substring(7, 22);
-                    int length = Integer.parseInt(substring, 2);
-
-                    String nextPackets = binaryString.substring(22, 22 + length);
-                    // assuming it's always 2 packets - create a method that takes this as input and returns 2 packets
-                    // except it could be another operator packet
-                    // 110 100 01010
-                    // 010 100 10001 00100
-
-                } else if (lengthTypeId.equals("1")) {
-                    // next 11 bits represent the number of sub-packets immediately contained in this packet
-                    // sub-packets appear after the 11 bits
-                    throw new UnsupportedOperationException("Haven't handled the length type ID = 1 case");
-                } else {
-                    throw new UnsupportedOperationException("unknown length type ID " + lengthTypeId);
-                }
-
-            }
-
- */
-        }
 
         public Packet(int version, int typeID, int literalValue) {
             this.version = version;
@@ -245,16 +182,17 @@ VVVTTTAAAAABBBBBCCCCC
                     packets.add(p);
                 } else {
 
-                    //TODO bring down the Operator logic and then should be set up to already loop through
-                    // the next operands
-                    // TODO add the operand, maybe new ctor to specify type 0 or 1
-                    Packet p = new Packet(version, typeID, 99);
-                    packets.add(p);
-
                     // Operator packet
                     // contains hierarchy of sub-packets
                     String lengthTypeId = binaryString.substring(index, index + 1);   // really want a single 0 or 1
                     index += 1;
+
+                    //TODO bring down the Operator logic and then should be set up to already loop through
+                    // the next operands
+                    // TODO add the operand, maybe new ctor to specify type 0 or 1
+                    Packet p = new Packet(version, typeID, 99);
+                    p.lengthTypeId = Integer.parseInt(lengthTypeId);
+                    packets.add(p);
 
                     if (lengthTypeId.equals("0")) {
                         // next 15 bits represent the total length in bits of the sub-packets contained by this packet
@@ -272,7 +210,14 @@ VVVTTTAAAAABBBBBCCCCC
                     } else if (lengthTypeId.equals("1")) {
                         // next 11 bits represent the number of sub-packets immediately contained in this packet
                         // sub-packets appear after the 11 bits
-                        throw new UnsupportedOperationException("Haven't handled the length type ID = 1 case");
+                        String substring = binaryString.substring(index, index + 11);
+                        int numPackets = Integer.parseInt(substring, 2);
+                        index += 11;
+
+                        //TODO - Crap - how to know how many bits to jump ahead???
+                        // well, if they are literal, it's 11 times the # of packets
+                        // but you know the real data will be packets inside packets....
+                        index = binaryString.length();
                     } else {
                         throw new UnsupportedOperationException("unknown length type ID " + lengthTypeId);
                     }
