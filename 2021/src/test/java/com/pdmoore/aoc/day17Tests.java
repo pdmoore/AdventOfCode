8ece89143a037532bc9e7df10b632c58b86ddf77
Paul Moore <pdmoore@gmail.com>
2022-01-27 15:29:43 -0800

    2021d17 - part 1 simple check for hitting the target

--- 2021/src/test/java/com/pdmoore/aoc/day17Tests.java
+++ 2021/src/test/java/com/pdmoore/aoc/day17Tests.java
@@ -1,12 +1,75 @@
 package com.pdmoore.aoc;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class day17Tests {
 
     @Test
-    void tarting() {
-        Assertions.assertEquals(0, 1);
+    void part1_fallsWithinTarget() {
+        int x = 7;
+        int y = 2;
+
+        TargetArea t = new TargetArea(20, 30, -10, -5);
+
+        Probe p = new Probe(x, y, t);
+
+        assertTrue(p.eventuallyHitsTargetArea());
+    }
+
+    private class TargetArea {
+        private final int x1;
+        private final int x2;
+        private final int y1;
+        private final int y2;
+
+        public TargetArea(int x1, int x2, int y1, int y2) {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
+
+        public boolean contains(int x, int y) {
+            return ((x >= x1 && x <= x2) && (y >= y1 && y <= y2));
+        }
+    }
+
+    private class Probe {
+        private final int initialX;
+        private final int initialY;
+        private int currentX;
+        private int currentY;
+        private int velocityX;
+        private int velocityY;
+        private final TargetArea targetArea;
+
+        public Probe(int x, int y, TargetArea t) {
+            this.initialX = x;
+            this.initialY = y;
+
+            this.velocityX = x;
+            this.velocityY = y;
+
+            this.targetArea = t;
+
+            this.currentX = 0;
+            this.currentY = 0;
+        }
+
+        public boolean eventuallyHitsTargetArea() {
+            while (!targetArea.contains(currentX, currentY)) {
+                if (velocityX > 0) velocityX -= 1;
+                else if (velocityX <0) velocityX += 1;
+                velocityY -= 1;
+
+                currentX += velocityX;
+                currentY += velocityY;
+            }
+
+
+            return true;
+        }
     }
 }
