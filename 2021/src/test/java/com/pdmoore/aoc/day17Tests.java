227ec97adb24976c0fce9034032e75fde27c1030
Paul Moore <pdmoore@gmail.com>
2022-01-27 15:47:29 -0800

    2021d17 - part 1 examples within target

--- 2021/src/test/java/com/pdmoore/aoc/day17Tests.java
+++ 2021/src/test/java/com/pdmoore/aoc/day17Tests.java
@@ -1,15 +1,19 @@
 package com.pdmoore.aoc;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class day17Tests {
 
-    @Test
-    void part1_fallsWithinTarget() {
-        int x = 7;
-        int y = 2;
+    @ParameterizedTest
+    @ValueSource(strings = {"7,2", "6,3", "9,0"})
+    void part1_fallsWithinTarget_2ndExample(String startingVelocityPair) {
+        String[] split = startingVelocityPair.split(",");
+        int x = Integer.parseInt(split[0]);
+        int y = Integer.parseInt(split[1]);
 
         TargetArea t = new TargetArea(20, 30, -10, -5);
 
@@ -17,6 +21,8 @@ public class day17Tests {
 
         assertTrue(p.eventuallyHitsTargetArea());
     }
+
+
 
     private class TargetArea {
         private final int x1;
@@ -59,15 +65,20 @@ public class day17Tests {
         }
 
         public boolean eventuallyHitsTargetArea() {
-            while (!targetArea.contains(currentX, currentY)) {
+            int count = 0;
+            currentX += velocityX;
+            currentY += velocityY;
+
+            while (!targetArea.contains(currentX, currentY) && (count <=1000)) {
                 if (velocityX > 0) velocityX -= 1;
                 else if (velocityX <0) velocityX += 1;
                 velocityY -= 1;
 
                 currentX += velocityX;
                 currentY += velocityY;
+                count += 1;
+//                System.out.println(currentX + ", " + currentY);
             }
-
 
             return true;
         }
