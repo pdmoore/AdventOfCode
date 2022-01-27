f573a5451bf3c27a448a2cd0a7c825316452177e
Paul Moore <pdmoore@gmail.com>
2022-01-27 15:16:01 -0800

    2021d17 - starting point

---
+++ 2021/src/test/java/com/pdmoore/aoc/day17Tests.java
@@ -0,0 +1,12 @@
+package com.pdmoore.aoc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class day17Tests {
+
+    @Test
+    void tarting() {
+        Assertions.assertEquals(0, 1);
+    }
+}
