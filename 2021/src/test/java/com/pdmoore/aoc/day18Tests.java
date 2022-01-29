0c8f1e81103bcba5b1e9048425a33a3c38d36afa
Paul Moore <pdmoore@gmail.com>
2022-01-28 21:21:16 -0800

    2021d18 - part 1 magnitude & splitting

--- 2021/src/test/java/com/pdmoore/aoc/day18Tests.java
+++ 2021/src/test/java/com/pdmoore/aoc/day18Tests.java
@@ -8,23 +8,59 @@ public class day18Tests {
 
     @Test
     void part1_magnitude_examples() {
-        //[[1,2],[[3,4],5]] becomes 143
         assertEquals(29, magnitudeOf("[9,1]"));
         assertEquals(129, magnitudeOf("[[9,1],[1,9]]"));
-//        assertEquals(143, magnitudeOf("[[1,2],[[3,4],5]]"));
+        assertEquals(143, magnitudeOf("[[1,2],[[3,4],5]]"));
+        assertEquals(1384, magnitudeOf("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"));
+        assertEquals(445, magnitudeOf("[[[[1,1],[2,2]],[3,3]],[4,4]]"));
+        assertEquals(791, magnitudeOf("[[[[3,0],[5,3]],[4,4]],[5,5]]"));
+        assertEquals(1137, magnitudeOf("[[[[5,0],[7,4]],[5,5]],[6,6]]"));
+        assertEquals(3488, magnitudeOf("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"));
+        assertEquals(4140, magnitudeOf("[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]"));
     }
 
-    private int magnitudeOf(String input) {
-        String removeOuterBrackets = input.substring(1, input.length() - 1);
-        if (removeOuterBrackets.charAt(0) != '[') {
-            String[] split = removeOuterBrackets.split(",");
-            int magnitude = (3 * Integer.parseInt(split[0])) + (2 * Integer.parseInt(split[1]));
-            return magnitude;
-        } else {
-            int splitIx = removeOuterBrackets.indexOf("],[");
-            String lhs = removeOuterBrackets.substring(0, splitIx + 1);
-            String rhs = removeOuterBrackets.substring(splitIx + 2);
-            return (3 * magnitudeOf(lhs)) + (2 * magnitudeOf(rhs));
+    @Test
+    void splitting() {
+        String[] expected = {"[1,2]", "[[3,4],5]"};
+        String[] actual = split("[[1,2],[[3,4],5]]");
+        assertEquals(expected[0], actual[0]);
+        assertEquals(expected[1], actual[1]);
+
+        String[] expected2 = {"[3,4]", "5"};
+        actual = split("[[3,4],5]");
+        assertEquals(expected2[0], actual[0]);
+        assertEquals(expected2[1], actual[1]);
+    }
+
+    private String[] split(String input) {
+        String[] result = new String[2];
+
+        int index = 1;
+        int bracketBalanceCount = 0;
+        while (true) {
+            char c = input.charAt(index);
+            if (bracketBalanceCount == 0 && c == ',') {
+                result[0] = input.substring(1, index);
+                result[1] = input.substring(index + 1, input.length() - 1);
+                return result;
+            }
+
+            if (c == '[') {
+                bracketBalanceCount++;
+            } else if (c == ']') {
+                bracketBalanceCount--;
+            }
+
+            index++;
         }
+    }
+    
+    private int magnitudeOf(String input) {
+        if (input.length() == 1) {
+            return Integer.parseInt(input);
+        }
+
+        String[] split = split(input);
+        return (3 * magnitudeOf(split[0])) + (2 * magnitudeOf(split[1]));
     }
 }
