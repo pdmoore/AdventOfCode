ba874736f4420988eb0c15aa741a88eeba2626aa
Paul Moore <pdmoore@gmail.com>
2022-01-28 16:53:08 -0800

    2021d18 - part 1 magnitude wip

--- 2021/src/test/java/com/pdmoore/aoc/day18Tests.java
+++ 2021/src/test/java/com/pdmoore/aoc/day18Tests.java
@@ -7,7 +7,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class day18Tests {
 
     @Test
-    void day1_example() {
-        assertEquals(0, 1);
+    void part1_magnitude_examples() {
+        //[[1,2],[[3,4],5]] becomes 143
+        assertEquals(29, magnitudeOf("[9,1]"));
+        assertEquals(129, magnitudeOf("[[9,1],[1,9]]"));
+//        assertEquals(143, magnitudeOf("[[1,2],[[3,4],5]]"));
+    }
+
+    private int magnitudeOf(String input) {
+        String removeOuterBrackets = input.substring(1, input.length() - 1);
+        if (removeOuterBrackets.charAt(0) != '[') {
+            String[] split = removeOuterBrackets.split(",");
+            int magnitude = (3 * Integer.parseInt(split[0])) + (2 * Integer.parseInt(split[1]));
+            return magnitude;
+        } else {
+            int splitIx = removeOuterBrackets.indexOf("],[");
+            String lhs = removeOuterBrackets.substring(0, splitIx + 1);
+            String rhs = removeOuterBrackets.substring(splitIx + 2);
+            return (3 * magnitudeOf(lhs)) + (2 * magnitudeOf(rhs));
+        }
     }
 }
