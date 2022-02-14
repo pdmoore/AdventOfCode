8214aef3c578548e5840eb80f74c7822cb154fcc
Paul Moore <pdmoore@gmail.com>
2022-02-13 16:49:39 -0800

    2021d19 - refactor

--- 2021/src/test/java/com/pdmoore/aoc/day19Tests.java
+++ 2021/src/test/java/com/pdmoore/aoc/day19Tests.java
@@ -9,7 +9,6 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static java.lang.Integer.parseInt;
-import static java.util.Collections.copy;
 import static java.util.function.UnaryOperator.identity;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
@@ -48,7 +47,7 @@ public class day19Tests {
 
     static class ThreeDRegion {
         public static final int NUM_OVERLAPPING_BEACONS = 12;
-        
+
         List<Scanner> scanners = new ArrayList<>();
         private Set<Point3D> beaconPositions = new HashSet<>();
 
@@ -102,7 +101,7 @@ public class day19Tests {
 
         private Optional<Point3D> overlapsWithMap(Scanner scanner) {
             return beaconPositions.stream()
-                    .flatMap(mapPoint -> scanner.getPoints().stream().map(scannerPoint -> mapPoint.subtractPoint(scannerPoint)))
+                    .flatMap(mapPoint -> scanner.getBeaconLocations().stream().map(scannerPoint -> mapPoint.subtractPoint(scannerPoint)))
                     .collect(groupingBy(identity(), counting()))
                     .entrySet()
                     .stream()
@@ -113,7 +112,7 @@ public class day19Tests {
 
         private void placeScannerOnMap(Scanner scanner, Point3D offset) {
             scanner.setPosition(offset);
-            beaconPositions.addAll(scanner.getPoints()
+            beaconPositions.addAll(scanner.getBeaconLocations()
                     .stream()
                     .map(point -> point.addPoint(offset))
                     .collect(Collectors.toList()));
@@ -128,14 +127,6 @@ public class day19Tests {
         private int y;
         private int z;
 
-        public static Point3D fromInput(String input) {
-            var inputs = input.split(",");
-            return Point3D.builder()
-                    .x(parseInt(inputs[0]))
-                    .y(parseInt(inputs[1]))
-                    .z(parseInt(inputs[2]))
-                    .build();
-        }
 //        public Point3D(String x, String y, String z) {
 //            this.x = parseInt(x);
 //            this.y = parseInt(y);
@@ -182,21 +173,21 @@ public class day19Tests {
     @Data
     static class Scanner {
         private String id;
-        private final List<Point3D> points;
+        private final List<Point3D> beaconLocations;
         private Point3D position;
 
         public Scanner(String id) {
             this.id = id;
-            points = new ArrayList<>();
+            beaconLocations = new ArrayList<>();
             position = Point3D.builder().x(0).y(0).z(0).build();
         }
 
         public void add(Point3D p) {
-            points.add(p);
+            beaconLocations.add(p);
         }
 
-        public List<Point3D> getPoints() {
-            return points;
+        public List<Point3D> getBeaconLocations() {
+            return beaconLocations;
         }
 
         public Stream<Scanner> allOrientations() {
@@ -206,17 +197,17 @@ public class day19Tests {
         }
 
         public Scanner roll() {
-            this.points.forEach(Point3D::rotateAroundX);
+            this.beaconLocations.forEach(Point3D::rotateAroundX);
             return this;
         }
 
         public Scanner turn() {
-            this.points.forEach(Point3D::rotateAroundZ);
+            this.beaconLocations.forEach(Point3D::rotateAroundZ);
             return this;
         }
 
         public Scanner reverseTurn() {
-            this.points.forEach(Point3D::reverseRotateAroundZ);
+            this.beaconLocations.forEach(Point3D::reverseRotateAroundZ);
             return this;
         }
     }
