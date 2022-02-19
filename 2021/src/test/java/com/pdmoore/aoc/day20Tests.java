d37b67a21c53754204203fc322c5d65c64fac627
Paul Moore <pdmoore@gmail.com>
2022-02-18 21:26:20 -0800

    2021d20 - part 1 wip

--- 2021/src/test/java/com/pdmoore/aoc/day20Tests.java
+++ 2021/src/test/java/com/pdmoore/aoc/day20Tests.java
@@ -1,11 +1,17 @@
 package com.pdmoore.aoc;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class day20Tests {
 
     @Test
+    @Disabled
     void part1_example() {
         String imageEnhancementAlgorithm = "..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..##\n" +
                 "#..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###\n" +
@@ -15,11 +21,12 @@ public class day20Tests {
                 "...####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.....\n" +
                 "..##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#";
 
-        String image = "#..#.\n" +
-                "#....\n" +
-                "##..#\n" +
-                "..#..\n" +
-                "..###";
+        List<String> image = Arrays.asList(
+                "#..#.\n",
+                "#....\n",
+                "##..#\n",
+                "..#..\n",
+                "..###" );
 
         ImageEnhancer sut = new ImageEnhancer(imageEnhancementAlgorithm, image);
 
@@ -28,12 +35,48 @@ public class day20Tests {
         int actual = sut.countOfLitPixels();
 
         Assertions.assertEquals(35, actual);
+    }
 
+    @Test
+    void part1_litPixelCount() {
+        String imageEnhancementAlgorithm = "ignored";
 
+        List<String> image = Arrays.asList(
+                "#..#.",
+                "#....",
+                "##..#",
+                "..#..",
+                "..###" );
+
+        ImageEnhancer sut = new ImageEnhancer(imageEnhancementAlgorithm, image);
+
+        int actual = sut.countOfLitPixels();
+
+        Assertions.assertEquals(10, actual);
     }
 
     private class ImageEnhancer {
-        public ImageEnhancer(String imageEnhancementAlgorithm, String image) {
+        private final String imageEnhancementAlgorithm;
+        private List<Point> litPixels;
+
+        public ImageEnhancer(String imageEnhancementAlgorithm, List<String> image) {
+            litPixels = new ArrayList();
+            this.imageEnhancementAlgorithm = imageEnhancementAlgorithm;
+            trackLitPixels(image);
+        }
+
+        private void trackLitPixels(List<String> input) {
+            int x = 0;
+            for (String line :
+                    input) {
+                for (int y = 0; y < line.length(); y++) {
+                    if (line.charAt(y) == '#') {
+                        Point p = new Point(x, y);
+                        litPixels.add(p);
+                    }
+                }
+            }
+
         }
 
         public void enhance() {
@@ -41,7 +84,17 @@ public class day20Tests {
         }
 
         public int countOfLitPixels() {
-            return 0;
+            return litPixels.size();
+        }
+    }
+
+    class Point {
+        private final int x;
+        private final int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 }
