a5bc88e7360db94974f1ce8e79638aae9d6f9589
Paul Moore <pdmoore@gmail.com>
2022-03-05 15:19:26 -0800

    2021d21 - starting point

--- 2021/src/test/java/com/pdmoore/aoc/day21Tests.java
+++ 2021/src/test/java/com/pdmoore/aoc/day21Tests.java
@@ -17,7 +17,63 @@ public class day21Tests {
         assertEquals(expected, actual);
     }
 
+
+
     private int playGame(int trackLength, int player1StartAt, int player2StartAt) {
-        return 0;
+        DeterministicDie die = new DeterministicDie();
+
+        int player1Position = player1StartAt;
+        int player2Position = player2StartAt;
+
+        int player1Score = 0;
+        int player2Score = 0;
+
+        while (player1Score < 1000 && player2Score < 1000) {
+
+            int roll1 = die.roll();
+            int roll2 = die.roll();
+            int roll3 = die.roll();
+            int player1Rolls = roll1 + roll2 + roll3;
+            player1Position = (player1Position + player1Rolls);
+            while (player1Position > 10) {
+                player1Position -= 10;
+            }
+            player1Score += player1Position;
+            System.out.println("Player 1 rolls " + roll1 + "+" + roll2 + "+" + roll3 + " and moves to space "+ player1Position+" for a total score of " + player1Score+ ".");
+            if (player1Score >= 1000) {
+                return player2Score * die.rollCount;
+            }
+
+            roll1 = die.roll();
+            roll2 = die.roll();
+            roll3 = die.roll();
+            int player2Rolls = roll1 + roll2 + roll3;
+            player2Position = (player2Position + player2Rolls) % 10;
+            while (player2Position > 10) {
+                player2Position -= 10;
+            }
+            player2Score += player2Position;
+            System.out.println("Player 2 rolls " + roll1 + "+" + roll2 + "+" + roll3 + " and moves to space "+ player2Position+" for a total score of " + player2Score+ ".");
+            if (player2Score >= 1000) {
+                return player1Score * die.rollCount;
+            }
+        }
+
+        return -1;
+    }
+
+    private class DeterministicDie {
+        int returnValue = 1;
+        int rollCount = 0;
+
+        int roll() {
+            rollCount++;
+            int dieResult = returnValue;
+            if (returnValue++ >= 100) {
+                returnValue = 1;
+            }
+            return dieResult;
+        }
+
     }
 }
