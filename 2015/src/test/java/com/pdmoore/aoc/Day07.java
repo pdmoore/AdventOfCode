9665f8d27783594c87f02062f2017bee9ab20746
Paul Moore <pdmoore@gmail.com>
2022-04-20 08:23:23 -0700

    2015d07 - starting point

---
+++ 2015/src/test/java/com/pdmoore/aoc/Day07.java
@@ -0,0 +1,12 @@
+package com.pdmoore.aoc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Day07 {
+
+    @Test
+    void startingPoint() {
+        Assertions.assertEquals(0, 1);
+    }
+}
