60784cf59809266b46b51a1e38bbf1a5a736e436
Paul Moore <pdmoore@gmail.com>
2022-04-20 08:45:00 -0700

    2015d07 - part1 NOT wip

--- 2015/src/test/java/com/pdmoore/aoc/Day07.java
+++ 2015/src/test/java/com/pdmoore/aoc/Day07.java
@@ -1,6 +1,7 @@
 package com.pdmoore.aoc;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -27,23 +28,72 @@ public class Day07 {
 
     @Test
     void signalToWire() {
-        String input = "123 -> x";
-        
-        Day7Thing sut = new Day7Thing();
-        sut.processInputLine(input);
-        
+        List<String> input = Arrays.asList("123 -> x");
+        Day7Thing sut = new Day7Thing(input);
+
         int actual = sut.valueOf("x");
         
         assertEquals(123, actual);
     }
 
+    @Test
+    void not() {
+        List<String> input = Arrays.asList("123 -> x", "NOT x -> h");
+        Day7Thing sut = new Day7Thing(input);
+
+        int actual = sut.valueOf("h");
+
+        assertEquals(65412, actual);
+    }
+
+    // LSHIFT
+    // RSHIFT
+    // NOT
+    // AND (2 values)
+    // OR (2 values)
+    // multiple passes to reduce unsolved to solved
+
+
     private class Day7Thing {
         Map<String, Integer> solved = new HashMap<>();
+        Map<String, String> unsolved = new HashMap<>();
+
+        public Day7Thing(List<String> input) {
+            for (String inputLine :
+                    input) {
+                processInputLine(inputLine);
+            }
+
+            solveUnsolved();
+        }
 
         public void processInputLine(String input) {
             String[] tokens = input.split(" -> ");
-            int value = Integer.parseInt(tokens[0]);
-            solved.put(tokens[1], value);
+
+            if (!tokens[0].contains(" ")) {
+                int value = Integer.parseInt(tokens[0]);
+                solved.put(tokens[1], value);
+            } else {
+                unsolved.put(tokens[1], tokens[0]);
+            }
+        }
+
+        private void solveUnsolved() {
+
+            if (unsolved.isEmpty()) return;
+
+            String expression = unsolved.get("h");
+            String[] tokens = expression.split(" ");
+
+            //check expression type
+            // assuming NOT here
+            int value = solved.get(tokens[1]);
+
+            int newValue = 65535 - value;
+            solved.put("h", newValue);
+
+
+            unsolved.remove("h");
         }
 
         public int valueOf(String key) {
