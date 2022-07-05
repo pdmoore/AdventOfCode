3ec81d53829b2e2dd81ea745df4a1d92f4e9236e
Paul Moore <pdmoore@gmail.com>
2022-07-05 16:00:38 -0700

    2015d08 part 1 - refactor to explicitly handle quote as only other escape sequence

--- 2015/src/test/java/com/pdmoore/aoc/Day08Tests.java
+++ 2015/src/test/java/com/pdmoore/aoc/Day08Tests.java
@@ -7,20 +7,6 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day08Tests {
-
-
-
-    /*
-"" is 2 characters of code (the two double quotes), but the string contains zero characters.
-"abc" is 5 characters of code, but 3 characters in the string data.
-"aaa\"aaa" is 10 characters of code, but the string itself contains six "a" characters and a single, escaped quote character, for a total of 7 characters in the string data.
-"\x27" is 6 characters of code, but the string itself contains just one - an apostrophe ('), escaped using hexadecimal notation.
-
-For example, given the four strings above,
-the total number of characters of string code (2 + 5 + 10 + 6 = 23)
-minus the total number of characters in memory for string values (0 + 3 + 7 + 1 = 11) is 23 - 11 = 12.
-     */
-
     @Test
     void DifferenceOf_EmptyString() {
         String input = "\"\"";
@@ -88,6 +74,9 @@ minus the total number of characters in memory for string values (0 + 3 + 7 + 1 
                     i += 3;
                 } else if (nextChar == '\\') {
                     sb.append("\\");
+                    i += 1;
+                } else {
+                    sb.append("\"");
                     i += 1;
                 }
             }
