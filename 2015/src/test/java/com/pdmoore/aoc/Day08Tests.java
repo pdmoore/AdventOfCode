a78f7c590a2546bb44eddae9b8bdef8ac5dcc16f
Paul Moore <pdmoore@gmail.com>
2022-07-04 17:20:21 -0700

    2015d08 part 1 - starting point

---
+++ 2015/src/test/java/com/pdmoore/aoc/Day08Tests.java
@@ -0,0 +1,19 @@
+package com.pdmoore.aoc;
+
+public class Day08Tests {
+
+
+
+    /*
+"" is 2 characters of code (the two double quotes), but the string contains zero characters.
+"abc" is 5 characters of code, but 3 characters in the string data.
+"aaa\"aaa" is 10 characters of code, but the string itself contains six "a" characters and a single, escaped quote character, for a total of 7 characters in the string data.
+"\x27" is 6 characters of code, but the string itself contains just one - an apostrophe ('), escaped using hexadecimal notation.
+
+For example, given the four strings above,
+the total number of characters of string code (2 + 5 + 10 + 6 = 23)
+minus the total number of characters in memory for string values (0 + 3 + 7 + 1 = 11) is 23 - 11 = 12.
+
+
+     */
+}
