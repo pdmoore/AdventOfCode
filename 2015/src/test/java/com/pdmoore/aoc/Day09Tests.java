dd7725d11fe968977a6c6f8015b7a6633ec6cfa9
Paul Moore <pdmoore@gmail.com>
2022-07-13 13:11:59 -0700

    2015d09 - starting point

---
+++ 2015/src/test/java/com/pdmoore/aoc/Day09Tests.java
@@ -0,0 +1,17 @@
+package com.pdmoore.aoc;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Day09Tests {
+
+    List<String> exampleInput =
+            Arrays.asList("London to Dublin = 464",
+                          "London to Belfast = 518",
+                          "Dublin to Belfast = 141");
+
+
+
+
+
+}
