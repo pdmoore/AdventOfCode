2de893cc1b4c620928b7bed850f5d9c4c7cb6471
Paul Moore <pdmoore@gmail.com>
2022-07-14 19:58:30 -0700

    2015d10 - part 1 starting point

---
+++ 2015/src/test/java/com/pdmoore/aoc/Day10Tests.java
@@ -0,0 +1,15 @@
+package com.pdmoore.aoc;
+
+public class Day10Tests {
+
+    /*
+    1 becomes 11 (1 copy of digit 1).
+11 becomes 21 (2 copies of digit 1).
+21 becomes 1211 (one 2 followed by one 1).
+1211 becomes 111221 (one 1, one 2, and two 1s).
+111221 becomes 312211 (three 1s, two 2s, and one 1).
+     */
+
+    static final String input = "1321131112";
+
+}
