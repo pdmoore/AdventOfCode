0c44b5ca11c3ca6926f9e924d1a72745294fdde1
Paul Moore <pdmoore@gmail.com>
2022-07-26 21:03:45 -0700

    2015d12 - defining examples

--- 2015/src/test/java/com/pdmoore/aoc/Day12Tests.java
+++ 2015/src/test/java/com/pdmoore/aoc/Day12Tests.java
@@ -1,5 +1,6 @@
 package com.pdmoore.aoc;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import org.json.simple.JSONArray;
@@ -13,12 +14,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Day12Tests {
 
     @Test
+    void emptyElements_SumToZero() {
+        assertEquals(0, sumAllNumbersIn("[]"));
+        assertEquals(0, sumAllNumbersIn("{}"));
+    }
+    
+    @Test
     void sumArray() {
         String input = "[1,2,3]";
 
         int actual = sumAllNumbersIn(input);
 
         assertEquals(6, actual);
+    }
+
+    @Test
+    @Disabled("How to handle array in an array in an array")
+    void sumArray_EmbeddedArrays() {
+        String input = "[[[3]]]";
+
+        int actual = sumAllNumbersIn(input);
+
+        assertEquals(3, actual);
     }
 
     @Test
@@ -30,6 +47,16 @@ public class Day12Tests {
         assertEquals(6, actual);
     }
 
+    @Test
+    @Disabled("Object embedded in an object")
+    void sumObjects_NegativeValue() {
+        String input = "{\"a\":{\"b\":4},\"c\":-1}";
+
+        int actual = sumAllNumbersIn(input);
+
+        assertEquals(3, actual);
+    }
+
     private int sumAllNumbersIn(String json) {
         Object parsed = null;
         try {
@@ -38,7 +65,7 @@ public class Day12Tests {
             throw new RuntimeException(e);
         }
 
-            int sum = 0;
+        int sum = 0;
         if (parsed instanceof JSONArray) {
             JSONArray jo = (JSONArray) parsed;
             for (Iterator it = jo.iterator(); it.hasNext(); ) {
