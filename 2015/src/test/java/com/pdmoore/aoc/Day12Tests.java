e4a13426226c49dcc8284b46253ce7c0eeb91024
Paul Moore <pdmoore@gmail.com>
2022-07-26 20:31:23 -0700

    2015d12 - part 1 hard coded for JSON array

--- 2015/src/test/java/com/pdmoore/aoc/Day12Tests.java
+++ 2015/src/test/java/com/pdmoore/aoc/Day12Tests.java
@@ -2,6 +2,12 @@ package com.pdmoore.aoc;
 
 import org.junit.jupiter.api.Test;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
+import java.util.Iterator;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day12Tests {
@@ -15,7 +21,24 @@ public class Day12Tests {
         assertEquals(6, actual);
     }
 
-    private int sumAllNumbersIn(String input) {
-        return 0;
+    private int sumAllNumbersIn(String json) {
+        Object parsed = null;
+        try {
+            parsed = new JSONParser().parse(json);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        JSONArray jo = (JSONArray) parsed;
+
+        int sum = 0;
+
+        for (Iterator it = jo.iterator(); it.hasNext(); ) {
+            Long l = (Long) it.next();
+            sum += l;
+        }
+
+
+
+        return sum;
     }
 }
