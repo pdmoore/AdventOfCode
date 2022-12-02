3182b8a46c852074ce7b1be53b823472eee70925
Paul Moore <pdmoore@gmail.com>
2022-12-01 20:42:16 -0800

    2022 day 02 - prep

---
+++ 2022/src/test/com/pdmoore/aoc/day02Tests.java
@@ -0,0 +1,12 @@
+package com.pdmoore.aoc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class day02Tests {
+
+    @Test
+    void part1_example() {
+        Assertions.assertEquals(0, 1);
+    }
+}
