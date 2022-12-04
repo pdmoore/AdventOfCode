248c584fdecb5b76dbb95b609d1f6b0b90fb4fa8
Paul Moore <pdmoore@gmail.com>
2022-12-03 21:16:51 -0800

    2022 - day04 part 2 solution

--- 2022/src/test/com/pdmoore/aoc/Day04.java
+++ 2022/src/test/com/pdmoore/aoc/Day04.java
@@ -16,13 +16,65 @@ public class Day04 {
 
         assertEquals(2, actual);
     }
+
     @Test
-    public void part1_slution() {
+    public void part1_solution() {
         List<String> input = PuzzleInput.asStringListFrom("./data/day04");
 
         int actual = part1(input);
 
         assertEquals(462, actual);
+    }
+    @Test
+    public void part2_solution() {
+        List<String> input = PuzzleInput.asStringListFrom("./data/day04");
+
+        int actual = part2(input);
+
+        assertEquals(835, actual);
+    }
+
+    @Test
+    public void part2_example() {
+        List<String> input = PuzzleInput.asStringListFrom("./data/day04_example");
+
+        int actual = part2(input);
+
+        assertEquals(4, actual);
+    }
+
+    private int part2(List<String> input) {
+        int result = 0;
+
+        for (String inputLine :
+                input) {
+
+            if (overlapAtAll(inputLine)) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    private boolean overlapAtAll(String inputLine) {
+        String[] split = inputLine.split(",");
+        String lhs = split[0];
+        String rhs = split[1];
+
+        String[] lhsplit = lhs.split("-");
+        String[] rhsplit = rhs.split("-");
+
+        int l_lower = Integer.parseInt(lhsplit[0]);
+        int l_upper = Integer.parseInt(lhsplit[1]);
+
+        int r_lower = Integer.parseInt(rhsplit[0]);
+        int r_upper = Integer.parseInt(rhsplit[1]);
+
+        if (l_lower <= r_lower && l_upper >= r_lower) return true;
+        if (r_lower <= l_lower && r_upper >= l_lower) return true;
+
+        return false;
     }
 
     private int part1(List<String> input) {
