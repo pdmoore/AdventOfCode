b798519a09074a7a24654621b6cffa80679d323d
Paul Moore <pdmoore@gmail.com>
2022-12-04 19:12:17 -0800

    2022 - day05 prep

---
+++ 2022/src/test/com/pdmoore/aoc/Day05.java
@@ -0,0 +1,20 @@
+package com.pdmoore.aoc;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day05 {
+
+//    @Test
+//    public void part1_example() {
+//        List<String> input = PuzzleInput.asStringListFrom("./data/day05_example");
+//
+//        int actual = part1(input);
+//
+//        assertEquals(99, actual);
+//    }
+
+}
