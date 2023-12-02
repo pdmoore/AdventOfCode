de0ffb2bd3dd168046430a90fba339abe3547f21
Paul Moore <pdmoore@gmail.com>
2023-12-02 09:23:07 -0800

    day02 - part 2 wip

--- 2023/src/test/Day02.java
+++ 2023/src/test/Day02.java
@@ -38,6 +38,9 @@ public class Day02 {
         List<Integer> expected = Arrays.asList(4, 2, 6);  // R G B
         assertEquals(expected, part2singlething(input));
         assertEquals(Arrays.asList(1, 3, 4), part2singlething("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"));
+        assertEquals(Arrays.asList(20, 13, 6), part2singlething("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"));
+        assertEquals(Arrays.asList(14, 3, 15), part2singlething("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"));
+        assertEquals(Arrays.asList(6, 3, 2), part2singlething("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"));
     }
 
     //-------------------------
