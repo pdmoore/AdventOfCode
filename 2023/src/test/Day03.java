33b1ea31db3edb5ac522239dc304fe46d9b62d20
Paul Moore <pdmoore@gmail.com>
2023-12-02 22:53:23 -0800

    day03 - part 1 wip

--- 2023/src/test/Day03.java
+++ 2023/src/test/Day03.java
@@ -34,7 +34,7 @@ public class Day03 {
     }
 
     @Test
-    void something() {
+    void get_all_the_numbers_in_the_grid() {
         List<Integer> expected = Arrays.asList(467, 114, 35, 633, 617, 58, 592, 755, 664, 598);
         Collections.sort(expected);
 
@@ -46,6 +46,69 @@ public class Day03 {
         Collections.sort(xxx);
 
         assertEquals(expected, xxx);
+    }
+
+    @Test
+    void get_only_part_numbers_in_grid() {
+        // number is a part number if adjacent to symbol
+        // where symbol is not a number or period
+        List<Integer> expected = Arrays.asList(467, 35, 633, 617, 592, 755, 664, 598);
+        Collections.sort(expected);
+
+        List<Integer> actual = getAllPartNumbers("./data/day03_part1_example");
+        Collections.sort(actual);
+
+        assertEquals(expected, actual);
+    }
+
+    private List<Integer> getAllPartNumbers(String filename) {
+        Map<Point, Integer> allTheNumbers = getAllTheNumbers(filename);
+
+        List<Integer> result = new ArrayList<>();
+
+        char[][] grid = as2dCharArray(filename);
+
+        for (Point point : allTheNumbers.keySet()) {
+
+            if (isPartNumber(grid, point, allTheNumbers.get(point))) {
+                result.add(allTheNumbers.get(point));
+            }
+        }
+
+        return result;
+    }
+
+    private boolean isPartNumber(char[][] grid, Point point, Integer integer) {
+        int numDigits = String.valueOf(integer).length();
+
+        // missing 467, 617, 755
+        for (int curY = point.y - 1; curY <= point.y + numDigits; curY++) {
+            int curX = point.x;
+            if (symbolAt(grid, curX - 1, curY)) return true;
+            if (symbolAt(grid, curX - 1, curY - 1)) return true;
+            if (symbolAt(grid, curX - 1, curY + 1)) return true;
+
+            if (symbolAt(grid, curX, curY)) return true;
+            if (symbolAt(grid, curX, curY)) return true;
+            if (symbolAt(grid, curX, curY)) return true;
+
+            if (symbolAt(grid, curX + 1, curY)) return true;
+            if (symbolAt(grid, curX + 1, curY)) return true;
+            if (symbolAt(grid, curX + 1, curY)) return true;
+        }
+
+        return false;
+    }
+
+    private boolean symbolAt(char[][] grid, int x, int y) {
+        if (x < 0 || x >= grid.length) return false;
+        if (y < 0 || y >= grid[0].length) return false;
+
+        char c = grid[x][y];
+        if (c == '.') return false;
+        if (c >= '0' && c <= '9') return false;
+
+        return true;
     }
 
     private Map<Point, Integer> getAllTheNumbers(String filename) {
