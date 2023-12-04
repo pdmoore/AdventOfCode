1b746528842d750a9d8542b40123b17e49eac7c4
Paul Moore <pdmoore@gmail.com>
2023-12-03 21:12:30 -0800

    day04 - part 1 wip

--- 2023/src/test/Day04.java
+++ 2023/src/test/Day04.java
@@ -2,12 +2,44 @@ package com.pdmoore.aoc;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day04 {
 
     @Test
-    void something() {
-        assertEquals(0, 1);
+    void part1_score_single_scratch_card() {
+        String input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
+
+        int actual = scoreScratchCard(input);
+
+        assertEquals(8, actual);
+    }
+
+    private int scoreScratchCard(String input) {
+//        "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
+
+        String[] firstSplit = input.split(":");
+        String[] secondSplit = firstSplit[1].split("\\|");
+
+        List<String> winningNumbers = Arrays.asList(secondSplit[0].split(" "));
+        List<String> numbersYouHave = Arrays.asList(secondSplit[1].split(" "));
+
+        int count = 0;
+        for (String numberYouHave :
+                numbersYouHave) {
+            if (!numberYouHave.isEmpty() && winningNumbers.contains(numberYouHave)) {
+                count++;
+            }
+        }
+
+       // split on colon, only retain second half
+        // split second half on '|'
+        // [0] is the winning numbers - convert to list
+        // [1] is numbers you have - for each instance in winning numbers, score is 2^(num matches - 1)
+
+        return (int) Math.pow(2, count - 1);
     }
 }
