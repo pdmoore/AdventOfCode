d91299052e0b93cfb5af3aeaf555d2ffdd960b23
Paul Moore <pdmoore@gmail.com>
2023-12-03 20:56:04 -0800

    day03 - part 2 solved

--- 2023/src/test/Day03.java
+++ 2023/src/test/Day03.java
@@ -216,8 +216,6 @@ public class Day03 {
             if (symbolAt(grid, curX - 1, curY)) return true;
 
             if (symbolAt(grid, curX, curY)) return true;
-            if (symbolAt(grid, curX, curY)) return true;
-            if (symbolAt(grid, curX, curY)) return true;
 
             if (symbolAt(grid, curX + 1, curY)) return true;
             if (symbolAt(grid, curX + 1, curY)) return true;
