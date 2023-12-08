e4836a42dd325e4526c447afbd18673c3fc3813a
Paul Moore <pdmoore@gmail.com>
2023-12-07 21:59:54 -0800

    day08 - part 1 wip

--- 2023/src/test/Day08.java
+++ 2023/src/test/Day08.java
@@ -2,12 +2,16 @@ package com.pdmoore.aoc;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day08 {
 
     @Test
-    void something() {
+    void nod_data_structure_from_input_line() {
 
         String inputLine = "AAA = (BBB, CCC)";
         Node n = new Node(inputLine);
@@ -15,6 +19,44 @@ public class Day08 {
         assertEquals("BBB", n.left);
         assertEquals("CCC", n.right);
     }
+
+    @Test
+    void part1_example() {
+        List<String> input = PuzzleInput.asStringListFrom("./data/day08_part1_example");
+        int actual = stepCountToFind(input, "ZZZ");
+
+        assertEquals(2, actual);
+    }
+
+    private int stepCountToFind(List<String> input, String target) {
+        int stepCount = 0;
+        String instructions = input.get(0);
+
+        Map<String, Node> nodeMap = new HashMap<>();
+        for (int i = 2; i < input.size(); i++) {
+            Node node = new Node(input.get(i));
+            nodeMap.put(node.key, node);
+        }
+
+        int instructionIndex = 0;
+        String currentKey = "AAA";
+        while (!currentKey.equals("ZZZ")) {
+            stepCount++;
+            if (instructions.charAt(instructionIndex) == 'L') {
+                currentKey = nodeMap.get(currentKey).left;
+            } else {
+                currentKey = nodeMap.get(currentKey).right;
+            }
+            instructionIndex++;
+            if (instructionIndex >= instructions.length()) {
+                instructionIndex = 0;
+            }
+        }
+
+
+        return stepCount;
+    }
+
 
     class Node {
 
