0cb4c484a08d7e28c70a310b366eac66b2cbd08d
Paul Moore <pdmoore@gmail.com>
2023-12-09 10:28:20 -0800

    day09 - part 2 wip

--- 2023/src/test/Day09.java
+++ 2023/src/test/Day09.java
@@ -21,6 +21,18 @@ class Day09 {
     }
 
     @Test
+    void part2_example() {
+        List<String> input = Arrays.asList("0 3 6 9 12 15",
+                "1 3 6 10 15 21",
+                "10 13 16 21 30 45");
+
+        int actual = actuallySolvePart2(input);
+
+
+        assertEquals(2, actual);
+    }
+
+    @Test
     void part1_solution() {
         List<String> input = PuzzleInput.asStringListFrom("./data/day09");
         int actual = actuallySolvePart1(input);
@@ -35,6 +47,17 @@ class Day09 {
         }
         return sum;
     }
+
+    private int actuallySolvePart2(List<String> input) {
+        int sum = 0;
+        for (String inputLine :
+                input) {
+            sum += part2_solveSingleLine(inputLine);
+        }
+        return sum;
+    }
+
+
 
     @Test
     void part1_example_line_1() {
@@ -63,6 +86,50 @@ class Day09 {
         assertEquals(68, actual);
     }
 
+    @Test
+    void part2_example_line_1() {
+        String input = "0 3 6 9 12 15";
+
+        int actual = part2_solveSingleLine(input);
+
+        assertEquals(-3, actual);
+    }
+
+    @Test
+    void part2_example_line_2() {
+        String input = "1   3   6  10  15  21";
+
+        int actual = part2_solveSingleLine(input);
+
+        assertEquals(0, actual);
+    }
+
+    @Test
+    void part2_example_line_3() {
+        String input = "10 13 16 21 30 45";
+
+        int actual = part2_solveSingleLine(input);
+
+        assertEquals(5, actual);
+    }
+
+    private int part2_solveSingleLine(String input) {
+        String[] s = input.split(" ");
+
+        List<Integer> firstRow = new ArrayList<>();
+        for (String number :
+                s) {
+            if (!number.isEmpty()) {
+                firstRow.add(Integer.valueOf(number));
+            }
+        }
+
+        Collections.reverse(firstRow);
+
+        return solvePart1(firstRow);
+    }
+
+
     private int part1_solveSingleLine(String input) {
         String[] s = input.split(" ");
 
@@ -74,6 +141,10 @@ class Day09 {
             }
         }
 
+        return solvePart1(firstRow);
+    }
+
+    private static Integer solvePart1(List<Integer> firstRow) {
         List<List<Integer>> histories = new ArrayList<>();
         histories.add(firstRow);
 
