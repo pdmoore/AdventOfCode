f9e0ea2f53a3dcf456bb100c624efa5ee84e02f7
Paul Moore <pdmoore@gmail.com>
2023-12-11 19:05:05 -0800

    day11 - refactor

--- 2023/src/test/Day11.java
+++ 2023/src/test/Day11.java
@@ -9,7 +9,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class Day11 {
+class Day11 {
 
     @Test
     void expand_space() {
