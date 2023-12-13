9217b83369b2c09a6985127ee50c2955fa5f1c4f
Paul Moore <pdmoore@gmail.com>
2023-12-12 20:57:38 -0800

    day13 - part 1 wip

---
+++ 2023/src/test/Day13.java
@@ -0,0 +1,13 @@
+package com.pdmoore.aoc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day13 {
+
+    @Test
+    void something() {
+        assertEquals(0, 1);
+    }
+}
