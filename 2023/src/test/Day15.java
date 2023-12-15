2e57a8806691398fdd7cde3a182a8c58ca8f3cc5
Paul Moore <pdmoore@gmail.com>
2023-12-15 14:05:28 -0800

    day 15 - part 1 wip

--- 2023/src/test/Day15.java
+++ 2023/src/test/Day15.java
@@ -2,6 +2,9 @@ package com.pdmoore.aoc;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day15 {
@@ -15,6 +18,24 @@ class Day15 {
         assertEquals(52, actual);
     }
 
+    // comma separated list of steps - IGNORE new lines!
+    // get currentValue of each step
+    // sum those results
+    @Test
+    void part1_example_convert_input_to_steps() {
+        String input = PuzzleInput.asStringFrom("./data/day15_part1_example");
+
+        List<String> inputLines = getStepsFrom(input);
+
+        assertEquals(11, inputLines.size());
+    }
+
+    private List<String> getStepsFrom(String input) {
+        String[] split = input.split(",");
+        return Arrays.asList(split);
+    }
+
+
     private int currentValueOf(String input) {
         int currentValue = 0;
 
@@ -23,18 +44,7 @@ class Day15 {
             currentValue += ascii;
             currentValue *= 17;
             currentValue %= 256;
-                    /*
-        Determine the ASCII code for the current character of the string.
-Increase the current value by the ASCII code you just determined.
-Set the current value to itself multiplied by 17.
-Set the current value to the remainder of dividing itself by 256.
-         */
-
-
-
-
         }
-
 
         return currentValue;
     }
