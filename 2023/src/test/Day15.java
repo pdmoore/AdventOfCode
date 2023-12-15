5e0ca3f4d069550d944562d5e9304288d6f2992b
Paul Moore <pdmoore@gmail.com>
2023-12-15 14:00:10 -0800

    day 15 - part 1 wip

--- 2023/src/test/Day15.java
+++ 2023/src/test/Day15.java
@@ -7,7 +7,35 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class Day15 {
 
     @Test
-    void something() {
-        assertEquals(0, 1);
+    void example_current_value_HASH() {
+        String input = "HASH";
+
+        int actual = currentValueOf(input);
+
+        assertEquals(52, actual);
+    }
+
+    private int currentValueOf(String input) {
+        int currentValue = 0;
+
+        for (char c : input.toCharArray()) {
+            int ascii = (int)c;
+            currentValue += ascii;
+            currentValue *= 17;
+            currentValue %= 256;
+                    /*
+        Determine the ASCII code for the current character of the string.
+Increase the current value by the ASCII code you just determined.
+Set the current value to itself multiplied by 17.
+Set the current value to the remainder of dividing itself by 256.
+         */
+
+
+
+
+        }
+
+
+        return currentValue;
     }
 }
