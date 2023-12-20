a3c9e2a5d55f99472838f5b9a91683ad7b4db0c2
Paul Moore <pdmoore@gmail.com>
2023-12-20 15:54:13 -0800

    day20 - part 1 wip

--- 2023/src/test/Day20.java
+++ 2023/src/test/Day20.java
@@ -2,12 +2,61 @@ package com.pdmoore.aoc;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day20 {
 
+    // read input and create data structures
+    // simulate pressing button and processing pulses
+    // loop 1000 times
+    // running count of low and high pulses across 1000 button presses
+    // (don't look for cycles, assume that's part 2)
+
     @Test
-    void something() {
-        assertEquals(0, 1);
+    void module_configuration_example1() {
+        List<String> input = PuzzleInput.asStringListFrom("./data/day20_part1_example1");
+
+        ModuleConfiguration sut = new ModuleConfiguration(input);
+
+        List<String> broadcasterModules = Arrays.asList("a", "b", "c");
+        assertEquals(broadcasterModules, sut.getBroadcasterModules());
+    }
+
+    private class ModuleConfiguration {
+        List<String> _broadcasterModules = new ArrayList<>();
+
+        public ModuleConfiguration(List<String> input) {
+            for (String inputLine: input) {
+                String[] split = inputLine.split(" -> ");
+                if (split[0].startsWith("broadcaster")) {
+                    String[] modules = split[1].split(", ");
+                    for (String module: modules) {
+                        _broadcasterModules.add(module.trim());
+                    }
+                } else if (split[0].charAt(0) == '%') {
+                    // handle FLipFLop
+                } else if (split[0].charAt(0) == '&') {
+                    // handle Conjunction
+                } else {
+                    throw new IllegalArgumentException("unknown module "  + inputLine);
+                }
+                /*
+                broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> inv
+&inv -> a
+                 */
+            }
+        }
+
+        public List<String> getBroadcasterModules() {
+            return _broadcasterModules;
+        }
     }
 }
