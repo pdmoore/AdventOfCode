fee68bfaf5e88d3e37490de6bf6c18507c7647f3
Paul Moore <pdmoore@gmail.com>
2023-12-22 15:59:57 -0800

    day22 - part 1 wip

--- 2023/src/test/Day22.java
+++ 2023/src/test/Day22.java
@@ -6,14 +6,53 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class Day22 {
+
+    // FALLING STUFF
+    // brick span, brick supports
+    // anything below brick
+    @Test
+    void falling_brick() {
+        List<String> allBricksInput = PuzzleInput.asStringListFrom("./data/day22_part1_example");
+        List<String> justABC = allBricksInput.subList(0, 3);
+
+        List<Brick> bricks = createBricksFrom(justABC);
+        assertEquals(3, bricks.size());
+
+        settleAllBricks(bricks);
+
+        assertEquals(1, bricks.get(0).start.z, "A is on the ground");
+        assertEquals(2, bricks.get(1).start.z, "B is atop A, does not settle");
+        assertEquals(2, bricks.get(2).start.z, "C was falling, settles on A");
+    }
+
+    private void settleAllBricks(List<Brick> bricks) {
+
+        for (Brick maybeFalling: bricks) {
+            // is it already on the ground?
+            if (maybeFalling.start.z == 1 || maybeFalling.end.z == 1) {
+                continue;
+            }
+
+            // check if it is atop any other brick
+            if (maybeFalling.StillInAir(bricks)) {
+                maybeFalling.fall();
+            }
+        }
+    }
+
 
     @Test
     void part1_example() {
         List<String> input = PuzzleInput.asStringListFrom("./data/day22_part1_example");
 
-        List<Position> bricks = createBricksFrom(input);
-        Assertions.assertEquals(7, bricks.size());
+        List<Brick> bricks = createBricksFrom(input);
+        assertEquals(7, bricks.size());
+
+        //maybe a map by Z position with a list of those bricks at that z?
+
         // parse each line, create a 3D object
         // from bottom up, fall all the bricks until no bricks fall
         // process each brick, is it the only support for a brick above it
@@ -27,33 +66,67 @@ class Day22 {
 //        assertEquals(5, actual);
     }
 
-    private List<Position> createBricksFrom(List<String> input) {
-        List<Position> result = new ArrayList<>();
+    private List<Brick> createBricksFrom(List<String> input) {
+        List<Brick> result = new ArrayList<>();
         for (String inputLine: input) {
             String[] split = inputLine.split("~");
-            result.add(new Position(split[0], split[1]));
+            result.add(new Brick(split[0], split[1]));
         }
         return result;
     }
 
-    class Position {
-        int x_start;
-        int y_start;
-        int z_start;
+    class Brick {
+        Point3D start;
+        Point3D end;
 
-        int x_end;
-        int y_end;
-        int z_end;
+        public Brick(String startString, String endString) {
+            String[] startSplit = startString.split(",");
+            start = new Point3D(Integer.parseInt(startSplit[0]), Integer.parseInt(startSplit[1]), Integer.parseInt(startSplit[2]));
+            String[] endSplit = endString.split(",");
+            end = new Point3D(Integer.parseInt(endSplit[0]), Integer.parseInt(endSplit[1]), Integer.parseInt(endSplit[2]));
+        }
 
-        public Position(String start, String end) {
-            String[] startSplit = start.split(",");
-            x_start = Integer.parseInt(startSplit[0]);
-            y_start = Integer.parseInt(startSplit[1]);
-            z_start = Integer.parseInt(startSplit[2]);
-            String[] endSplit = end.split(",");
-            x_end = Integer.parseInt(endSplit[0]);
-            y_end = Integer.parseInt(endSplit[1]);
-            z_end = Integer.parseInt(endSplit[2]);
+
+        public void fall() {
+            this.start.z -= 1;
+            this.end.z -= 1;
+        }
+
+        public boolean StillInAir(List<Brick> bricks) {
+            for (int otherIndex = 0; otherIndex < bricks.size(); otherIndex++) {
+                Brick other = bricks.get(otherIndex);
+                if (this == other) {
+                    continue;
+                }
+
+                if (this.isAtop(other)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private boolean isAtop(Brick other) {
+            // TODO -
+            // If this.z != other.z - 1, return false
+            // then check the x/y spans are somehow aligned
+            if (this.start.z != 3)             return true;
+
+
+            return false;
+        }
+    }
+
+    class Point3D {
+
+        private final int x;
+        private final int y;
+        private int z;
+
+        public Point3D(int x, int y, int z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
         }
     }
 }
