cb5ed4f8599f3e9aafe8d1443895506c251b7256
Paul Moore <pdmoore@gmail.com>
2023-12-23 17:03:07 -0800

    day23 - part 2 wip

--- 2023/src/test/Day23.java
+++ 2023/src/test/Day23.java
@@ -13,13 +13,26 @@ class Day23 {
     final char FOREST = '#';
     final String slopes = "^>v<";
 
+    boolean _slippery = false;
+
     @Test
     void part1_example() {
         char[][] input = PuzzleInput.as2dCharArray("./data/day23_part1_example");
 
+        _slippery = true;
         int actual = findLongestHike(input);
 
         assertEquals(94, actual);
+    }
+
+    @Test
+    void part2_example() {
+        char[][] input = PuzzleInput.as2dCharArray("./data/day23_part1_example");
+
+        _slippery = false;
+        int actual = findLongestHike(input);
+
+        assertEquals(154, actual);
     }
 
     @Test
@@ -29,6 +42,16 @@ class Day23 {
         int actual = findLongestHike(input);
 
         assertEquals(2162, actual);
+    }
+
+    @Test
+    void part2_solution() {
+        char[][] input = PuzzleInput.as2dCharArray("./data/day23");
+
+        _slippery = false;
+        int actual = findLongestHike(input);
+
+        assertEquals(99, actual);
     }
 
     private int findLongestHike(char[][] map) {
@@ -62,7 +85,7 @@ class Day23 {
 
         List<Point> stepsFromHere = new ArrayList<>();
         char tryLeft = map[thisStep.x][thisStep.y - 1];
-        if (tryLeft == PATH || tryLeft == '<') {
+        if (tryLeft == PATH || tryLeft == '<' || (!_slippery && tryLeft == '>')) {
             Point nextStep = new Point(thisStep.x, thisStep.y - 1);
             if (!currentHike.contains(nextStep)) {
                 stepsFromHere.add(nextStep);
@@ -70,7 +93,7 @@ class Day23 {
         }
         if (thisStep.x > 0) {
             char tryUp = map[thisStep.x - 1][thisStep.y];
-            if (tryUp == PATH || tryUp == '^') {
+            if (tryUp == PATH || tryUp == '^' || (!_slippery && tryUp == 'v')) {
                 Point nextStep = new Point(thisStep.x - 1, thisStep.y);
                 if (!currentHike.contains(nextStep)) {
                     stepsFromHere.add(nextStep);
@@ -78,26 +101,19 @@ class Day23 {
             }
         }
         char tryRight = map[thisStep.x][thisStep.y + 1];
-        if (tryRight == PATH || tryRight == '>') {
+        if (tryRight == PATH || tryRight == '>' || (!_slippery && tryRight == '<')) {
             Point nextStep = new Point(thisStep.x, thisStep.y + 1);
             if (!currentHike.contains(nextStep)) {
                 stepsFromHere.add(nextStep);
             }
         }
-        if (thisStep.x + 1 < map[0].length) {
-
-            char tryDown = map[thisStep.x + 1][thisStep.y];
-            if (tryDown == PATH || tryDown == 'v') {
-                Point nextStep = new Point(thisStep.x + 1, thisStep.y);
-                if (!currentHike.contains(nextStep)) {
-                    stepsFromHere.add(nextStep);
-                }
+        char tryDown = map[thisStep.x + 1][thisStep.y];
+        if (tryDown == PATH || tryDown == 'v' || (!_slippery && tryDown == '^')) {
+            Point nextStep = new Point(thisStep.x + 1, thisStep.y);
+            if (!currentHike.contains(nextStep)) {
+                stepsFromHere.add(nextStep);
             }
-        } else {
-            int breakpoint = 1;
-            breakpoint--;
         }
-
 
         if (stepsFromHere.size() == 1) {
             takeAHike(map, currentHike, stepsFromHere.get(0), allCompleteHikes, end);
