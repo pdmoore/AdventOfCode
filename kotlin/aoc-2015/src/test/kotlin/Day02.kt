960e342422fcc2df5745578864614ae039048ec3
Paul Moore <pdmoore@gmail.com>
2024-01-02 10:21:54 -0800

    2015d02 - part 1 wip

--- kotlin/aoc-2015/src/test/kotlin/Day02.kt
+++ kotlin/aoc-2015/src/test/kotlin/Day02.kt
@@ -1,4 +1,5 @@
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
 
 class Day02 : FunSpec( {
@@ -13,7 +14,30 @@ class Day02 : FunSpec( {
         AreaOfSmallestSide(inputLine) shouldBe 6
     }
 
+    data class SideLengths(val inputLine: String, val expected: Int)
+    context("Wrapping paper needed is surface area plus area of smallest side") {
+        withData(
+            SideLengths("2x3x4", 58),
+            SideLengths("1x1x10", 43)
+        ) { (inputLine, expected) ->
+            solvePart21(inputLine) shouldBe expected
+        }
+    }
+
+    test("Solve part 1") {
+        val input = PuzzleInput.asListOfStrings("./data/day02")
+        solvePart21(input) shouldBe 1586300
+    }
+
 })
+
+fun solvePart21(inputLine: List<String>): Int {
+    var result = 0
+    for (i in inputLine.indices) {
+        result += solvePart21(inputLine[i])
+    }
+    return result
+}
 
 fun AreaOfSmallestSide(inputLine: String): Int {
     val split = inputLine.split("x")
@@ -32,4 +56,10 @@ fun surfaceAreaOf(inputLine: String): Int {
     val w = split[1].toInt()
     val h = split[2].toInt()
     return (2*l*w) + (2*w*h) + (2*h*l)
+}
+
+//TODO calling this solvePart1 collides with Day01 - need to figure out
+// whether to dump this in a class or have better naming
+fun solvePart21(inputLine: String): Int {
+    return surfaceAreaOf(inputLine) + AreaOfSmallestSide(inputLine)
 }
--- kotlin/aoc-2015/src/test/kotlin/PuzzleInput.kt
+++ kotlin/aoc-2015/src/test/kotlin/PuzzleInput.kt
@@ -3,5 +3,6 @@ import java.io.File
 class PuzzleInput {
     companion object {
         fun asSingleLine(filename: String): String  =  File(filename).readLines()[0]
+        fun asListOfStrings(filename: String): List<String> = File(filename).readLines()
     }
 }
