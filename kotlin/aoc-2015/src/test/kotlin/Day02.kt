512e5e8a0cdd734bb679c52e425a8860ef7228a3
Paul Moore <pdmoore@gmail.com>
2024-01-02 10:11:48 -0800

    2015d02 - part 1 wip

--- kotlin/aoc-2015/src/test/kotlin/Day02.kt
+++ kotlin/aoc-2015/src/test/kotlin/Day02.kt
@@ -8,8 +8,23 @@ class Day02 : FunSpec( {
         surfaceAreaOf(inputLine) shouldBe 52
     }
 
+    test("Smallest side of l*w*h rectangle") {
+        val inputLine = "2x3x4"
+        AreaOfSmallestSide(inputLine) shouldBe 6
+    }
 
 })
+
+fun AreaOfSmallestSide(inputLine: String): Int {
+    val split = inputLine.split("x")
+    val l = split[0].toInt()
+    val w = split[1].toInt()
+    val h = split[2].toInt()
+    val lByw = l*w
+    val wByh = w*h
+    val lByh = l*h
+    return Math.min(lByw, Math.min(wByh, lByh))
+}
 
 fun surfaceAreaOf(inputLine: String): Int {
     val split = inputLine.split("x")
