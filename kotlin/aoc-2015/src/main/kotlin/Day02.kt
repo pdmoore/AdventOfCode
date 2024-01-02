347af647e818494ac861ecf0fa32f7d784783578
Paul Moore <pdmoore@gmail.com>
2024-01-02 15:55:35 -0800

    2015d02 - refactor

---
+++ kotlin/aoc-2015/src/main/kotlin/Day02.kt
@@ -0,0 +1,43 @@
+import kotlin.math.min
+
+class Day02 {
+    companion object {
+        fun solvePart21(inputLine: String): Int {
+            val (l, w, h) = extractLengthWidthHeight(inputLine)
+            return surfaceAreaOf(l, w, h) + areaOfSmallestSide(l, w, h)
+        }
+
+        fun solvePart21(inputLine: List<String>): Int {
+            return inputLine.sumOf { i -> solvePart21(i) }
+        }
+
+        fun areaOfSmallestSide(inputLine: String): Int {
+            val (l, w, h) = extractLengthWidthHeight(inputLine)
+            return areaOfSmallestSide(l, w, h)
+        }
+
+        fun surfaceAreaOf(inputLine: String): Int {
+            val (l, w, h) = extractLengthWidthHeight(inputLine)
+            return surfaceAreaOf(l, w, h)
+        }
+
+        private fun extractLengthWidthHeight(inputLine: String): Triple<Int, Int, Int> {
+            val split = inputLine.split("x")
+            val l = split[0].toInt()
+            val w = split[1].toInt()
+            val h = split[2].toInt()
+            return Triple(l, w, h)
+        }
+
+        fun areaOfSmallestSide(l: Int, w: Int, h: Int): Int {
+            val lByw = l * w
+            val wByh = w * h
+            val lByh = l * h
+            return min(lByw, min(wByh, lByh))
+        }
+
+        fun surfaceAreaOf(l: Int, w: Int, h: Int): Int {
+            return (2 * l * w) + (2 * w * h) + (2 * h * l)
+        }
+    }
+}
---
+++ kotlin/aoc-2015/src/test/kotlin/Day02Tests.kt
@@ -0,0 +1,32 @@
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.withData
+import io.kotest.matchers.shouldBe
+
+class Day02Tests : FunSpec( {
+
+    test("Surface area is 2*l*w + 2*w*h + 2*h*l") {
+        val inputLine = "2x3x4"
+        Day02.surfaceAreaOf(inputLine) shouldBe 52
+    }
+
+    test("Smallest side of l*w*h rectangle") {
+        val inputLine = "2x3x4"
+        Day02.areaOfSmallestSide(inputLine) shouldBe 6
+    }
+
+    data class SideLengths(val inputLine: String, val expected: Int)
+    context("Wrapping paper needed is surface area plus area of smallest side") {
+        withData(
+            SideLengths("2x3x4", 58),
+            SideLengths("1x1x10", 43)
+        ) { (inputLine, expected) ->
+            Day02.solvePart21(inputLine) shouldBe expected
+        }
+    }
+
+    test("Solve part 1") {
+        val input = PuzzleInput.asListOfStrings("./data/day02")
+        Day02.solvePart21(input) shouldBe 1586300
+    }
+})
+
