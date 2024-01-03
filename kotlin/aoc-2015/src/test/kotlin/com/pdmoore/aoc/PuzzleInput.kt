7ce269c75d6b0c07e1cbf417993867cb14db4c4f
Paul Moore <pdmoore@gmail.com>
2024-01-02 16:00:07 -0800

    2015 - puzzle input refactor

--- kotlin/aoc-2015/src/test/kotlin/com/pdmoore/aoc/PuzzleInput.kt
+++ kotlin/aoc-2015/src/test/kotlin/com/pdmoore/aoc/PuzzleInput.kt
@@ -4,7 +4,8 @@ import java.io.File
 
 class PuzzleInput {
     companion object {
-        fun asSingleLine(filename: String): String  =  File(filename).readLines()[0]
         fun asListOfStrings(filename: String): List<String> = File(filename).readLines()
+
+        fun asSingleLine(filename: String): String  =  File(filename).readLines()[0]
     }
 }
