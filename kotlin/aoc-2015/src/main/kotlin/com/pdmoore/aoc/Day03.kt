64cb95f9d52fde3b618183270464b69f21c976b2
Paul Moore <pdmoore@gmail.com>
2024-01-05 08:47:17 -0800

    2015d03 - part 2 solved

--- kotlin/aoc-2015/src/main/kotlin/com/pdmoore/aoc/Day03.kt
+++ kotlin/aoc-2015/src/main/kotlin/com/pdmoore/aoc/Day03.kt
@@ -2,7 +2,7 @@ package com.pdmoore.aoc
 
 class Day03 {
     companion object {
-        fun housesThatReceiveAtLeastOnePresent(input: String): Int {
+        fun houseCountSantaDeliversTo(input: String): Int {
             var current = Point2D(0, 0)
 
             val housesVisited = mutableSetOf(current)
@@ -12,12 +12,55 @@ class Day03 {
                     '<' -> current.left()
                     '^' -> current.up()
                     'v' -> current.down()
-                    else -> { throw IllegalArgumentException("unknown character $c")}
+                    else -> {
+                        throw IllegalArgumentException("unknown character $c")
+                    }
                 }
                 housesVisited.add(current)
             }
 
             return housesVisited.size
+        }
+
+        fun houseCountWithRoboSantaDeliveries(input: String): Int {
+            var currentSanta = Point2D(0, 0)
+            var currentRoboSanta = Point2D(0, 0)
+
+            var santaMovesOnEven = 0
+
+            val housesVisited = mutableSetOf(currentSanta, currentRoboSanta)
+            for (c in input) {
+                if (santaMovesOnEven++ % 2 == 0) {
+
+                    currentSanta = when (c) {
+                        '>' -> currentSanta.right()
+                        '<' -> currentSanta.left()
+                        '^' -> currentSanta.up()
+                        'v' -> currentSanta.down()
+                        else -> {
+                            throw IllegalArgumentException("unknown character $c")
+                        }
+                    }
+                    housesVisited.add(currentSanta)
+                } else {
+
+                    currentRoboSanta = when (c) {
+                        '>' -> currentRoboSanta.right()
+                        '<' -> currentRoboSanta.left()
+                        '^' -> currentRoboSanta.up()
+                        'v' -> currentRoboSanta.down()
+                        else -> {
+                            throw IllegalArgumentException("unknown character $c")
+                        }
+                    }
+                    housesVisited.add(currentRoboSanta)
+                }
+
+
+            }
+
+            return housesVisited.size
+
         }
     }
 }
--- kotlin/aoc-2015/src/test/kotlin/com/pdmoore/aoc/Day03Tests.kt
+++ kotlin/aoc-2015/src/test/kotlin/com/pdmoore/aoc/Day03Tests.kt
@@ -1,28 +1,50 @@
 package com.pdmoore.aoc
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
 
 class Day03Tests : FunSpec( {
 
     test("Single move delivers to two unique locations") {
         val input = ">"
-        Day03.housesThatReceiveAtLeastOnePresent(input) shouldBe 2
+        Day03.houseCountSantaDeliversTo(input) shouldBe 2
     }
 
     test("Non-repeating sequence delivers to each location") {
         val input = "^>v<"
-        Day03.housesThatReceiveAtLeastOnePresent(input) shouldBe 4
+        Day03.houseCountSantaDeliversTo(input) shouldBe 4
     }
 
     test("Repeating sequence only counts unique houses") {
         val input = "^v^v^v^v^v"
-        Day03.housesThatReceiveAtLeastOnePresent(input) shouldBe 2
+        Day03.houseCountSantaDeliversTo(input) shouldBe 2
     }
 
     test("part 1 solution") {
         val input = PuzzleInput.asSingleLine("./data/day03")
-        Day03.housesThatReceiveAtLeastOnePresent(input) shouldBe 2572
+        Day03.houseCountSantaDeliversTo(input) shouldBe 2572
+    }
+
+    test("Robo-Santa alternates moves with real Santa - simple example") {
+        val input = "^v"
+        Day03.houseCountWithRoboSantaDeliveries(input) shouldBe 3
+    }
+
+    data class InputAndExpected(val inputLine: String, val expected: Int)
+    context("Santa and Robo-Santa alternate executing moves") {
+        withData(
+            InputAndExpected("^v", 3),
+            InputAndExpected("^>v<", 3),
+            InputAndExpected("^v^v^v^v^v", 11)
+        ) { (input, expected) ->
+            Day03.houseCountWithRoboSantaDeliveries(input) shouldBe expected
+        }
+    }
+
+    test("part 2 solution") {
+        val input = PuzzleInput.asSingleLine("./data/day03")
+        Day03.houseCountWithRoboSantaDeliveries(input) shouldBe 2631
     }
 })
 
