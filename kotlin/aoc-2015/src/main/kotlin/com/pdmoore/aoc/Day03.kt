2bdbf103851f64dfe2a3bf41dbafd6b16aeb64c1
Paul Moore <pdmoore@gmail.com>
2024-01-06 09:37:53 -0800

    2015d03 - refactor

--- kotlin/aoc-2015/src/main/kotlin/com/pdmoore/aoc/Day03.kt
+++ kotlin/aoc-2015/src/main/kotlin/com/pdmoore/aoc/Day03.kt
@@ -2,32 +2,31 @@ package com.pdmoore.aoc
 
 class Day03 {
     companion object {
+        const val DO_NOT_USE_ROBO_SANTA = false
+        const val USE_ROBO_SANTA = true
+
         fun houseCountSantaDeliversTo(input: String): Int {
-            var current = Point2D(0, 0)
-
-            val housesVisited = mutableSetOf(current)
-            for (c in input) {
-                current = moveTo(c, current)
-                housesVisited.add(current)
-            }
-
-            return housesVisited.size
+            return countHousesVisited(input, DO_NOT_USE_ROBO_SANTA)
         }
 
         fun houseCountWithRoboSantaDeliveries(input: String): Int {
-            var currentSanta = Point2D(0, 0)
-            var currentRoboSanta = Point2D(0, 0)
+            return countHousesVisited(input, USE_ROBO_SANTA)
+        }
+
+        private fun countHousesVisited(input: String, useRoboSanta: Boolean): Int {
+            var santaPosition = Point2D(0, 0)
+            var roboSantaPosition = Point2D(0, 0)
 
             var santaMovesOnEven = 0
 
-            val housesVisited = mutableSetOf(currentSanta, currentRoboSanta)
+            val housesVisited = mutableSetOf(santaPosition, roboSantaPosition)
             for (c in input) {
-                if (santaMovesOnEven++ % 2 == 0) {
-                    currentSanta = moveTo(c, currentSanta)
-                    housesVisited.add(currentSanta)
+                if (!useRoboSanta || santaMovesOnEven++ % 2 == 0) {
+                    santaPosition = moveTo(c, santaPosition)
+                    housesVisited.add(santaPosition)
                 } else {
-                    currentRoboSanta = moveTo(c, currentRoboSanta)
-                    housesVisited.add(currentRoboSanta)
+                    roboSantaPosition = moveTo(c, roboSantaPosition)
+                    housesVisited.add(roboSantaPosition)
                 }
             }
 
