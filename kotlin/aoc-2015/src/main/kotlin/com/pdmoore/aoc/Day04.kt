466d600125accd110a5b9f1ee0a5cc7321d1d704
Paul Moore <pdmoore@gmail.com>
2024-01-09 14:23:31 -0800

    2015d04 - part 1 wip

---
+++ kotlin/aoc-2015/src/main/kotlin/com/pdmoore/aoc/Day04.kt
@@ -0,0 +1,18 @@
+package com.pdmoore.aoc
+
+import java.math.BigInteger
+import java.security.MessageDigest
+import kotlin.text.Charsets.UTF_8
+
+class Day04 {
+    companion object {
+        fun generateMD5(input: String): String {
+            val md = MessageDigest.getInstance("MD5")
+            val toByteArray = input.toByteArray(UTF_8)
+            val hash = md.digest(toByteArray)
+            val bi = BigInteger(1, hash)
+            return bi.toString(16).padStart(32, '0')
+        }
+    }
+
+}
--- kotlin/aoc-2015/src/test/kotlin/com/pdmoore/aoc/Day04Tests.kt
+++ kotlin/aoc-2015/src/test/kotlin/com/pdmoore/aoc/Day04Tests.kt
@@ -1,10 +1,27 @@
 package com.pdmoore.aoc
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+const val PUZZLE_INPUT = "ckczppom"
 
 class Day04Tests : FunSpec( {
 
-    // MD5 hash
+
+    // MD5 hash, without hex
+    // with hex
+    // find via searching, from 0 on up (or backwards?)
+    // secret key (puzzle input)
+    // hexadecimal hash
+    // result has 5 leading zeros
+    // start with 00000_000000 and see if it matches secret?
+
+    test("Generate MD5 hash given a key and value") {
+        val hashed = Day04.generateMD5("abcdef609043")
+        val firstN = hashed.substring(0, 11)
+        firstN shouldBe "000001dbbfa"
+    }
+
 
 
 
