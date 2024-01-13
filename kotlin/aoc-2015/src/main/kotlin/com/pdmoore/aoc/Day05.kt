d26ee3a161ad48762841e35c1ccd34c2c1983cdb
Paul Moore <pdmoore@gmail.com>
2024-01-13 11:32:06 -0800

    2015d05 - part 1 solved

--- kotlin/aoc-2015/src/main/kotlin/com/pdmoore/aoc/Day05.kt
+++ kotlin/aoc-2015/src/main/kotlin/com/pdmoore/aoc/Day05.kt
@@ -8,7 +8,6 @@ class Day05 {
         }
 
         fun atLeastOneLetterTwiceInARow(s: String): Boolean {
-//            (0..s.length - 2).forEach { i -> if (s[i] == s[i + 1]) return true }
             (0..s.length - 2).forEach { i ->
                 if (s[i] == s[i+1]) return true
             }
@@ -17,15 +16,16 @@ class Day05 {
 
         fun containsUnwantedCharacterPair(s: String): Boolean {
             val unwantedPairs = listOf("ab", "cd", "pq", "xy")
-            for (i in 0..s.length - 2) {
-                val thisPair = "" + s[i] + s[i+1]
-                if (thisPair in unwantedPairs) return true
+            (0..s.length - 2).forEach { i ->
+                if (Character.toString(s[i]).plus(s[i + 1]) in unwantedPairs) return true
             }
             return false
         }
 
         fun isNice(s: String): Boolean {
-            return atLeastThreeVowels(s) && atLeastOneLetterTwiceInARow(s) && !containsUnwantedCharacterPair(s)
+            return atLeastThreeVowels(s) &&
+                    atLeastOneLetterTwiceInARow(s) &&
+                    !containsUnwantedCharacterPair(s)
         }
 
         fun solvePart1(input: List<String>): Int {
