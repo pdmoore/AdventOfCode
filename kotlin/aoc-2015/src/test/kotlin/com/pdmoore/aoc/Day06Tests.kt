b37966b1c445be31d29b8785b52ca883a6a4b285
Paul Moore <pdmoore@gmail.com>
2024-01-14 10:16:10 -0800

    2015d06 - part 1 wip

---
+++ kotlin/aoc-2015/src/test/kotlin/com/pdmoore/aoc/Day06Tests.kt
@@ -0,0 +1,17 @@
+package com.pdmoore.aoc
+
+import io.kotest.core.spec.style.FunSpec
+
+class Day06Tests: FunSpec( {
+
+    // how to track lit lights?
+    // grid off all locations or just Coord of lit ones?
+    // turn on command
+    // turn off command
+    // toggle command
+    // Part 1 loop to solve List<String> input
+
+
+
+
+})
