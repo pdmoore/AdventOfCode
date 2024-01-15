7aab266b011ce0040950d60ab577b764bc752495
Paul Moore <pdmoore@gmail.com>
2024-01-15 10:11:15 -0800

    2015d06 - part 1 wip

--- kotlin/aoc-2015/src/test/kotlin/com/pdmoore/aoc/Day06Tests.kt
+++ kotlin/aoc-2015/src/test/kotlin/com/pdmoore/aoc/Day06Tests.kt
@@ -1,6 +1,7 @@
 package com.pdmoore.aoc
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
 
 class Day06Tests: FunSpec( {
 
@@ -10,6 +11,14 @@ class Day06Tests: FunSpec( {
     // turn off command
     // toggle command
     // Part 1 loop to solve List<String> input
+    test("Turn on command will turn on all lights in the rectangle") {
+        val input = "turn on 0,0 through 2,2"
+
+        var sut = Day06(1000, 1000)
+        sut.followInstruction(input)
+        sut.countOfLitLights() shouldBe 9
+
+    }
 
 
 
