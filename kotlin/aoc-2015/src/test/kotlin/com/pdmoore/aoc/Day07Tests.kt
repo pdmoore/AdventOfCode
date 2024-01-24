89c13206e89c8ff80c99a1173a183cc5965f87b4
Paul Moore <pdmoore@gmail.com>
2024-01-24 09:29:26 -0800

    2015d07 - part 1 wip

---
+++ kotlin/aoc-2015/src/test/kotlin/com/pdmoore/aoc/Day07Tests.kt
@@ -0,0 +1,26 @@
+package com.pdmoore.aoc
+
+import io.kotest.core.spec.style.FunSpec
+
+class Day07Tests: FunSpec( {
+
+    /*
+    signal to ID    123 -> x
+    AND gate        x AND y -> d
+    OR gate         x OR y -> e
+    LSHIFT gate     x LSHIFT 2 -> f
+    RSHIFT gate     y RSHIFT 2 -> g
+    NOT gate        NOT x -> h
+
+    example final states
+d: 72
+e: 507
+f: 492
+g: 114
+h: 65412
+i: 65079
+x: 123
+y: 456
+     */
+
+})
