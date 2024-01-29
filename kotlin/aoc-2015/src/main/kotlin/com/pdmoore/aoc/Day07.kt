5f69074cf4e7f7deb4fdba6d532b9970ecdc2f72
Paul Moore <pdmoore@gmail.com>
2024-01-28 19:16:16 -0800

    2015d07 - part 1 wip

--- kotlin/aoc-2015/src/main/kotlin/com/pdmoore/aoc/Day07.kt
+++ kotlin/aoc-2015/src/main/kotlin/com/pdmoore/aoc/Day07.kt
@@ -2,29 +2,25 @@ package com.pdmoore.aoc
 
 class Day07 {
 
-    private var IDtoSignal: MutableMap<String, Int>
-
-    init {
-        IDtoSignal = mutableMapOf<String, Int>()
-    }
+    private var identifierToSignal = mutableMapOf<String, Int>()
 
     fun processInputLine(input: String) {
         val split = input.split(" -> ")
 
         if (split[0].contains("AND")) {
             val splitAnd = split[0].split(" AND ")
-            val lhs = IDtoSignal.get(splitAnd[0])
-            val rhs = IDtoSignal.get(splitAnd[1])
+            val lhs = identifierToSignal.get(splitAnd[0])
+            val rhs = identifierToSignal.get(splitAnd[1])
 
-            IDtoSignal.put(split[1], lhs?.and(rhs!!) ?: -99)
+            identifierToSignal.put(split[1], lhs?.and(rhs!!) ?: -99)
         } else {
-            IDtoSignal.put(split[1], split[0].toInt())
+            identifierToSignal.put(split[1], split[0].toInt())
         }
 
     }
 
     fun valueOf(identifier: String): Int {
-        return IDtoSignal.get(identifier)!!
+        return identifierToSignal.get(identifier)!!
     }
 
     fun processInput(input: List<String>) {
