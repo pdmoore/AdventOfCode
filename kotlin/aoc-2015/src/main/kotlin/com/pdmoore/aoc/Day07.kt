8bea07058f270894df4bc9813289d6edc96d37d8
Paul Moore <pdmoore@gmail.com>
2024-01-29 09:21:09 -0800

    2015d07 - part 1 wip

--- kotlin/aoc-2015/src/main/kotlin/com/pdmoore/aoc/Day07.kt
+++ kotlin/aoc-2015/src/main/kotlin/com/pdmoore/aoc/Day07.kt
@@ -7,36 +7,42 @@ class Day07 {
     fun processInputLine(input: String) {
         val split = input.split(" -> ")
 
-        if (split[0].contains("AND")) {
-            val splitAnd = split[0].split(" AND ")
-            val lhs = identifierToSignal[splitAnd[0]]
-            val rhs = identifierToSignal[splitAnd[1]]
+        when {
+            split[0].contains("AND") -> {
+                val splitAnd = split[0].split(" AND ")
+                val lhs = identifierToSignal[splitAnd[0]]
+                val rhs = identifierToSignal[splitAnd[1]]
 
-            identifierToSignal[split[1]] = lhs?.and(rhs!!) ?: -99
-        } else if (split[0].contains("OR")) {
-            val splitOr = split[0].split(" OR ")
-            val lhs = identifierToSignal[splitOr[0]]
-            val rhs = identifierToSignal[splitOr[1]]
-
-            identifierToSignal[split[1]] = lhs?.or(rhs!!) ?: -88
-        } else if (split[0].contains("LSHIFT")) {
-            val splitLshift = split[0].split(" LSHIFT ")
-            val lhs = identifierToSignal[splitLshift[0]]
-            val rhs = splitLshift[1].toInt()
-
-            if (lhs != null) {
-                identifierToSignal[split[1]] = lhs shl rhs
+                identifierToSignal[split[1]] = lhs?.and(rhs!!) ?: -99
             }
-        } else if (split[0].contains("RSHIFT")) {
-            val splitRshift = split[0].split(" RSHIFT ")
-            val lhs = identifierToSignal[splitRshift[0]]
-            val rhs = splitRshift[1].toInt()
+            split[0].contains("OR") -> {
+                val splitOr = split[0].split(" OR ")
+                val lhs = identifierToSignal[splitOr[0]]
+                val rhs = identifierToSignal[splitOr[1]]
 
-            if (lhs != null) {
-                identifierToSignal[split[1]] = lhs shr rhs
+                identifierToSignal[split[1]] = lhs?.or(rhs!!) ?: -88
             }
-        } else {
-            identifierToSignal[split[1]] = split[0].toInt()
+            split[0].contains("LSHIFT") -> {
+                val splitLshift = split[0].split(" LSHIFT ")
+                val lhs = identifierToSignal[splitLshift[0]]
+                val rhs = splitLshift[1].toInt()
+
+                if (lhs != null) {
+                    identifierToSignal[split[1]] = lhs shl rhs
+                }
+            }
+            split[0].contains("RSHIFT") -> {
+                val splitRshift = split[0].split(" RSHIFT ")
+                val lhs = identifierToSignal[splitRshift[0]]
+                val rhs = splitRshift[1].toInt()
+
+                if (lhs != null) {
+                    identifierToSignal[split[1]] = lhs shr rhs
+                }
+            }
+            else -> {
+                identifierToSignal[split[1]] = split[0].toInt()
+            }
         }
 
     }
