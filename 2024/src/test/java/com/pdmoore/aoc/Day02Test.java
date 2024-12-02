2d5dcdc9dbb8d026e6d1e3c28e4a3c15a504d5dc
Paul Moore <pdmoore@gmail.com>
2024-12-01 16:44:29 -0800

    2024 day02 prep

---
+++ 2024/src/test/java/com/pdmoore/aoc/Day02Test.java
@@ -0,0 +1,14 @@
+package com.pdmoore.aoc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day02Test {
+
+
+    @Test
+    void part1_example() {
+        assertEquals(1, 0);
+    }
+}
