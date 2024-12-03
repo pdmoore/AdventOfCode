31b14700c51b0b9d9f131bc91d8a8ce412e8cdea
Paul Moore <pdmoore@gmail.com>
2024-12-02 21:33:46 -0800

    day03 part 1 solved

--- 2024/src/test/java/com/pdmoore/aoc/Day03Test.java
+++ 2024/src/test/java/com/pdmoore/aoc/Day03Test.java
@@ -3,6 +3,8 @@ package com.pdmoore.aoc;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class Day03Test {
 
     @Test
@@ -14,19 +16,32 @@ public class Day03Test {
         Assertions.assertEquals(161, actual);
     }
 
+    @Test
+    void part1() {
+        List<String> input = PuzzleInput.asStringListFrom("data/day03.txt");
+        int actual = 0;
+        for (String line : input) {
+            actual += solve(line);
+        }
+
+        Assertions.assertEquals(178794710, actual);
+
+    }
+
+
     private int solve(String input) {
         int result = 0;
         // left to right, pattern mul(##,##)
         String[] split = input.split("mul");
         for (int i = 0; i < split.length; i++) {
             String each = split[i];
-            if (each.charAt(0) != '(') continue;
-            // match ##,##)  "^[\\d]+[\\.]$"
+            if (each.length() > 0 && each.charAt(0) != '(') continue;
 
             int firstNumber = 0;
             int secondNumber = 0;
             try {
                 int commaIx = each.indexOf(',');
+                if (commaIx == -1) continue;
                 firstNumber = Integer.parseInt(each.substring(1, commaIx));
                 int closeParenIx = each.indexOf(')', commaIx + 1);
                 if (closeParenIx == -1) continue;
