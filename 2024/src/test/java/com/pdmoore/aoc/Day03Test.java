b20406d2d67852057a81d00a7ed166f9c9d08843
Paul Moore <pdmoore@gmail.com>
2024-12-02 16:39:09 -0800

    2024 day03 prep empty files

---
+++ 2024/src/test/java/com/pdmoore/aoc/Day03Test.java
@@ -0,0 +1,12 @@
+package com.pdmoore.aoc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Day03Test {
+
+    @Test
+    void part1_example() {
+        Assertions.assertEquals(0, 1);
+    }
+}
