d989663706fb683f8c77fbeb0d080e1c111c5d1c
Paul Moore <pdmoore@gmail.com>
2024-12-08 13:03:28 -0800

    2024 doy09 prep

---
+++ 2024/src/test/java/com/pdmoore/aoc/Day09Test.java
@@ -0,0 +1,23 @@
+package com.pdmoore.aoc;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day09Test {
+
+    @Test
+    void part1_example() {
+        List<String> input = PuzzleInput.asStringListFrom("data/day09_example.txt");
+        int actual = solvePart1(input);
+        assertEquals(0, actual);
+    }
+
+    private int solvePart1(List<String> input) {
+        return 0;
+    }
+
+
+}
