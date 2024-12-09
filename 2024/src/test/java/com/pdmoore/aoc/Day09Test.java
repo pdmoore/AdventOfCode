0ccca3e37f187d40064e1a24242d21cad192b414
Paul Moore <pdmoore@gmail.com>
2024-12-08 21:11:41 -0800

    2024 day09 part 1 wip

--- 2024/src/test/java/com/pdmoore/aoc/Day09Test.java
+++ 2024/src/test/java/com/pdmoore/aoc/Day09Test.java
@@ -10,12 +10,53 @@ class Day09Test {
 
     @Test
     void part1_example() {
-        List<String> input = PuzzleInput.asStringListFrom("data/day09_example.txt");
+        String input = "2333133121414131402";
         int actual = solvePart1(input);
         assertEquals(0, actual);
     }
 
-    private int solvePart1(List<String> input) {
+    @Test
+    void testDiskMapToBlock() {
+        String input = "12345";
+        String actual = diskMapToBlock(input);
+        assertEquals("0..111....22222", actual);
+
+        input = "2333133121414131402";
+        actual = diskMapToBlock(input);
+        assertEquals("00...111...2...333.44.5555.6666.777.888899", actual);
+    }
+
+    private String diskMapToBlock(String input) {
+
+        //char by char, expand and track IDNumber
+        int idNumber = 0;
+        boolean fileOrFreeSpace = true;
+        StringBuilder result = new StringBuilder();
+        for (Character c : input.toCharArray()) {
+            int num = Integer.parseInt(String.valueOf(c));
+            if (fileOrFreeSpace) {
+                for (int i = 0; i < num; i++) {
+                    result.append(idNumber);
+                }
+                idNumber++;
+            } else {
+                for (int i = 0; i < num; i++) {
+                    result.append(".");
+                }
+            }
+
+            fileOrFreeSpace = !fileOrFreeSpace;
+        }
+
+        return result.toString();
+    }
+
+
+    private int solvePart1(String input) {
+
+
+
+
         return 0;
     }
 
