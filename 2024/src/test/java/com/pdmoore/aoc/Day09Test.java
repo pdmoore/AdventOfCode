33dce158ac507e0833593a24139ad09537c0ea87
Paul Moore <pdmoore@gmail.com>
2024-12-08 21:24:04 -0800

    2024 day09 part 1 wip

--- 2024/src/test/java/com/pdmoore/aoc/Day09Test.java
+++ 2024/src/test/java/com/pdmoore/aoc/Day09Test.java
@@ -2,8 +2,6 @@ package com.pdmoore.aoc;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day09Test {
@@ -26,9 +24,60 @@ class Day09Test {
         assertEquals("00...111...2...333.44.5555.6666.777.888899", actual);
     }
 
-    private String diskMapToBlock(String input) {
+    @Test
+    void testMoveFileBlocks() {
+        String input = "0..111....22222";
+        String actual = moveFileBlocks(input);
+        assertEquals("022111222......", actual);
 
-        //char by char, expand and track IDNumber
+        input = "00...111...2...333.44.5555.6666.777.888899";
+        actual = moveFileBlocks(input);
+        assertEquals("0099811188827773336446555566..............", actual);
+    }
+
+    private String moveFileBlocks(String input) {
+        // first gap from the left
+        // first gap from the right
+        // if the same, return
+
+        // grab the first non-gap from the right and position it in the gap on the left
+        // repeat
+
+        /*
+0..111....22222
+02.111....2222.
+022111....222..
+0221112...22...
+02211122..2....
+022111222......
+         */
+        String result = input;
+        int rightIndex = result.length() - 1;
+        while (true) {
+            int leftIndex = result.indexOf('.');
+            if (leftIndex >= rightIndex) {
+                return result.toString();
+            }
+
+            char rightChar = result.charAt(rightIndex);
+
+
+
+            char[] charArray = result.toCharArray();
+            charArray[leftIndex] = rightChar;
+            charArray[rightIndex] = '.';
+
+            while (charArray[rightIndex] == '.') {
+                rightIndex--;
+            }
+
+            result = new String(charArray);
+        }
+
+    }
+
+
+    private String diskMapToBlock(String input) {
         int idNumber = 0;
         boolean fileOrFreeSpace = true;
         StringBuilder result = new StringBuilder();
