69022f89516b2a4b98c23b455ff71a974dcf0f1a
Paul Moore <pdmoore@gmail.com>
2024-12-11 09:34:22 -0800

    2024 day11 new impl as BigInteger not strings

--- 2024/src/test/java/com/pdmoore/aoc/Day11Test.java
+++ 2024/src/test/java/com/pdmoore/aoc/Day11Test.java
@@ -3,6 +3,8 @@ package com.pdmoore.aoc;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,8 +14,11 @@ class Day11Test {
     void part1_example() {
         String input = "0 1 10 99 999";
 
-        int actual = solvePart1(input, 1);
+        int actual = solvePart1_viaStrings(input, 1);
 
+        assertEquals(7, actual);
+
+        actual = solvePart1(input, 1);
         assertEquals(7, actual);
     }
 
@@ -42,14 +47,39 @@ class Day11Test {
     }
 
     @Test
-    void part2() {
-        String input = "1750884 193 866395 7 1158 31 35216 0";
-        int actual = solvePart1(input, 75);
-        assertEquals(99, actual);
+    void part2_longerExample() {
+        String input = "125 17";
+        int actual = solvePart2(input, 6);
+        assertEquals(22, actual);
+
+        actual = solvePart2(input, 25);
+        assertEquals(55312, actual);
     }
 
 
-    private int solvePart1(String input, int blinkCount) {
+
+    @Test
+    void part2() {
+        String input = "1750884 193 866395 7 1158 31 35216 0";
+        int actual = solvePart2(input, 75);
+        assertEquals(99, actual);
+    }
+
+    private int solvePart2(String input, int blinkCount) {
+
+        // INSTEAD OF solving the whole thing as one monolith
+        // split it into smaller pieces and sum those small piece counts
+
+        // need to watch out for rule where things are split into two - that seems
+        // require some extra logic to follow those paths to the end
+
+        // approach it as recursion....
+
+
+        return 0;
+    }
+
+        private int solvePart1_viaStrings(String input, int blinkCount) {
         String next = input;
         for (int i = 0; i < blinkCount; i++) {
             System.out.println("blink " + i);
@@ -81,4 +111,39 @@ class Day11Test {
 
         return next.split(" ").length;
     }
+
+    private int solvePart1(String input, int blinkCount) {
+        String[] split = input.split(" ");
+        List<BigInteger> stones = new ArrayList<>();
+        for (int i = 0; i < split.length; i++) {
+            stones.add(new BigInteger(split[i]));
+        }
+
+        for (int i = 0; i < blinkCount; i++) {
+            List<BigInteger> nextStones = new ArrayList<>();
+
+            for (BigInteger stone : stones) {
+                String s = stone.toString();
+
+                if (stone.equals(BigInteger.ZERO)) {
+                    nextStones.add(BigInteger.ONE);
+                } else if (s.length() % 2 == 0) {
+                    int length = s.length();
+                    String substring = s.substring(0, length / 2);
+                    StringBuilder sb = new StringBuilder(substring);
+                    nextStones.add(new BigInteger(substring));
+                    substring = s.substring(length / 2);
+                    nextStones.add(new BigInteger(substring));
+                } else {
+                    BigInteger multiplyBy2024 = stone.multiply(BigInteger.valueOf(2024));
+                    nextStones.add(multiplyBy2024);
+                }
+            }
+
+            stones = nextStones;
+        }
+
+        return stones.size();
+    }
+
 }
