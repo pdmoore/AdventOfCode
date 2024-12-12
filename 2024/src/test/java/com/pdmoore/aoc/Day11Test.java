af6dab89fa21aaff0bf41622a1bfd34934c5b1e7
Paul Moore <pdmoore@gmail.com>
2024-12-11 20:05:09 -0800

    2024 day11 still going

--- 2024/src/test/java/com/pdmoore/aoc/Day11Test.java
+++ 2024/src/test/java/com/pdmoore/aoc/Day11Test.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,11 +16,7 @@ class Day11Test {
     void part1_example() {
         String input = "0 1 10 99 999";
 
-        int actual = solvePart1_viaStrings(input, 1);
-
-        assertEquals(7, actual);
-
-        actual = solvePart1(input, 1);
+        int actual = solvePart1(input, 1);
         assertEquals(7, actual);
     }
 
@@ -30,6 +28,13 @@ class Day11Test {
 
         actual = solvePart1(input, 25);
         assertEquals(55312, actual);
+    }
+
+    @Test
+    void canZeroRun75Blinks() {
+        String input = "0";
+        int actual = solvePart1(input, 40);
+        assertEquals(10174278, actual);
     }
 
     @Test
@@ -52,8 +57,11 @@ class Day11Test {
         int actual = solvePart2(input, 6);
         assertEquals(22, actual);
 
-        actual = solvePart2(input, 25);
-        assertEquals(55312, actual);
+//        actual = solvePart2(input, 25);
+//        assertEquals(55312, actual);
+//
+//        actual = solvePart2(input, 75);
+//        assertEquals(99, actual);
     }
 
 
@@ -61,7 +69,7 @@ class Day11Test {
     @Test
     void part2() {
         String input = "1750884 193 866395 7 1158 31 35216 0";
-        int actual = solvePart2(input, 75);
+        int actual = solvePart1(input, 75);
         assertEquals(99, actual);
     }
 
@@ -75,41 +83,78 @@ class Day11Test {
 
         // approach it as recursion....
 
+        // Store engraving and count
+        //
 
-        return 0;
-    }
+        String[] split = input.split(" ");
+        List<BigInteger> stones = new ArrayList<>();
+        for (int i = 0; i < split.length; i++) {
+            stones.add(new BigInteger(split[i]));
+        }
+        Map<BigInteger, Integer> engravingByCount = new HashMap<>();
+        for (BigInteger stone : stones) {
+//            engravingByCount.put(stone, engravingByCount.getOrDefault(stone, 0) + 1);
+            engravingByCount.put(stone, 1);
+        }
 
-        private int solvePart1_viaStrings(String input, int blinkCount) {
-        String next = input;
-        for (int i = 0; i < blinkCount; i++) {
+        for (int i = 1; i <= blinkCount; i++) {
             System.out.println("blink " + i);
-            String[] split = next.split(" ");
+//            List<BigInteger> nextStones = new ArrayList<>();
 
-            StringBuilder sb = new StringBuilder();
-            for (String s : split) {
-                BigInteger engraving = new BigInteger(s);
 
-                if (engraving.equals(BigInteger.ZERO)) {
-                    sb.append("1");
+            // TODO - switching to map lost the fact there may be duplicates generated during
+            // the processing of all previous.
+            // Instead of just blindly doing a put, need to check if key is there already
+            // and update by amount or put amount if not there
+            Map<BigInteger, Integer> nextStonesByCount = new HashMap<>();
+            for (BigInteger stone : engravingByCount.keySet()) {
+
+                if (i == 4) {
+                    int breakpoint = 55;
+                }
+
+                // Need to track amount and up those counts for the new stones
+                int amount = engravingByCount.get(stone);
+                if (amount == 0) continue;
+                System.out.print(stone + "   ");
+
+                engravingByCount.replace(stone, 0);
+
+                String s = stone.toString();
+                if (stone.equals(BigInteger.ZERO)) {
+//                    nextStones.add(BigInteger.ONE);
+                    nextStonesByCount.put(BigInteger.ONE, amount);
                 } else if (s.length() % 2 == 0) {
                     int length = s.length();
                     String substring = s.substring(0, length / 2);
-                    sb.append(new BigInteger(substring));
-                    sb.append(" ");
+                    StringBuilder sb = new StringBuilder(substring);
+//                    nextStones.add(new BigInteger(substring));
+                    nextStonesByCount.put(new BigInteger(substring), amount);
                     substring = s.substring(length / 2);
-                    sb.append(new BigInteger(substring));
-                }
-                else {
-                    BigInteger multiplyBy2024 = engraving.multiply(BigInteger.valueOf(2024));
-                    sb.append(multiplyBy2024);
+//                    nextStones.add(new BigInteger(substring));
+                    nextStonesByCount.put(new BigInteger(substring), amount);
+                } else {
+                    BigInteger multiplyBy2024 = stone.multiply(BigInteger.valueOf(2024));
+//                    nextStones.add(multiplyBy2024);
+                    nextStonesByCount.put(multiplyBy2024, amount);
+
                 }
 
-                sb.append(" ");
+                System.out.println(" ");
             }
-            next = sb.toString().trim();
+
+//            for (BigInteger nextStone : nextStones) {
+            for (BigInteger nextStone : nextStonesByCount.keySet()) {
+                engravingByCount.put(nextStone, engravingByCount.getOrDefault(nextStone, 0) + nextStonesByCount.get(nextStone));
+            }
         }
 
-        return next.split(" ").length;
+        int result = 0;
+        for (BigInteger stone : engravingByCount.keySet()) {
+            result += engravingByCount.get(stone);
+        }
+
+        return result;
     }
 
     private int solvePart1(String input, int blinkCount) {
@@ -120,6 +165,8 @@ class Day11Test {
         }
 
         for (int i = 0; i < blinkCount; i++) {
+            System.out.println("blink " + i + "   " + stones.size());
+
             List<BigInteger> nextStones = new ArrayList<>();
 
             for (BigInteger stone : stones) {
