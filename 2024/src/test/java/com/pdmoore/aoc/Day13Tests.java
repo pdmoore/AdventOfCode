ea7926cb924e5e6b13b9e075a5fbe133cbfc2ced
Paul Moore <pdmoore@gmail.com>
2024-12-12 22:18:44 -0800

    2024 day13 part 1  wip

--- 2024/src/test/java/com/pdmoore/aoc/Day13Tests.java
+++ 2024/src/test/java/com/pdmoore/aoc/Day13Tests.java
@@ -1,4 +1,44 @@
 package com.pdmoore.aoc;
 
-public class Day13Tests {
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day13Tests {
+
+    @Test
+    void part1_example() {
+        List<String> input = PuzzleInput.asStringListFrom("data/day13_example.txt");
+
+        int actual = solvePart1(input);
+
+        assertEquals(480, actual);
+    }
+
+    private int solvePart1(List<String> input) {
+        // need to split input by blank lines
+
+        // for each set of three,
+        // button a stats
+        // button b stats
+        // prize target
+        //3 tokens for A button, 1 token for B button
+
+        // solving a single set
+        // would it be x mod a plus x mod b equal to 0
+        // and y mod a plus y mod b equal to 0 for some pair of a and b
+
+        // a or b can be pressed up to 100
+        // so it could be 1 to 100 for each and whether both x and y are met
+        // it is possible there is not solution
+
+
+
+        // solve those for each set of inputs
+
+
+        return 0;
+    }
 }
