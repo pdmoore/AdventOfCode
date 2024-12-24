f64b6094ca7f6c77746e5e4782df444f79d153ae
Paul Moore <pdmoore@gmail.com>
2024-12-23 20:37:15 -0800

    2024 day23 part1 solved

--- 2024/src/test/java/com/pdmoore/aoc/Day19Test.java
+++ 2024/src/test/java/com/pdmoore/aoc/Day19Test.java
@@ -3,7 +3,9 @@ package com.pdmoore.aoc;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,13 +15,110 @@ class Day19Test {
     void part1Example() {
         List<String> input = PuzzleInput.asStringListFrom("data/day19_example.txt");
 
-        int actual = solvePart1(input);
+        int actual = solvePart1(input).size();
 
         assertEquals(6, actual);
-
     }
 
-    private int solvePart1(List<String> input) {
+    @Test
+    void part1() {
+        List<String> input = PuzzleInput.asStringListFrom("data/day19.txt");
+
+        int actual = solvePart1(input).size();
+
+        assertEquals(355, actual);
+    }
+
+    @Test
+    void part2Example() {
+        List<String> input = PuzzleInput.asStringListFrom("data/day19_example.txt");
+
+        int actual = solvePart2(input);
+
+        assertEquals(16, actual);
+    }
+
+    @Test
+    void part2_examples_one_at_time() {
+        String towelPatterns = "r, wr, b, g, bwu, rb, gb, br";
+        List<String> input = List.of(towelPatterns, "", "brwrr");
+//        int actual = solvePart2(input);
+//        assertEquals(2, actual);
+
+//        assertEquals(1, solvePart2(List.of(towelPatterns, "", "bggr")));
+        assertEquals(4, solvePart2(List.of(towelPatterns, "", "gbbr")));
+    }
+
+    @Test
+    void part2() {
+        List<String> input = PuzzleInput.asStringListFrom("data/day19.txt");
+
+        int actual = solvePart2(input);
+
+        assertEquals(-1, actual);
+    }
+
+    int allDesignPermutations = 0;
+    Map<String, Integer> cachedPatterns = new HashMap<>();
+    private int solvePart2(List<String> input) {
+        List<String> patterns = grabPatternsFrom(input.get(0));
+
+        List<String> validDesigns = solvePart1(input);
+        allDesignPermutations = 0;
+        cachedPatterns = new HashMap<>();
+        for (String pattern : patterns) {
+            int count = tryPermutations(patterns, pattern);
+            cachedPatterns.put(pattern, count);
+        }
+        for (String design : validDesigns) {
+            allDesignPermutations += tryPermutations(patterns, design);
+        }
+
+        return allDesignPermutations;
+    }
+
+    // TODO - this is not a brute force solution. Think about caching solved sub-problems
+    // along the way.
+    // Could it be building permutations from the patterns list first and then looking for sub-strings?
+    private int tryPermutations(List<String> patterns, String design) {
+
+        // Maybe determine cached patterns by length of pattern:
+        // r, b, g
+        // wr, rb, gb, br,
+        // bwu
+        // then do the same for the designs?
+
+
+        // how to show br should be 'br' and 'b, r'? need to update the cached permutations
+        // once and not each time it's encountered
+        // rather than copy all patterns in once at the beginning, build it before solving designs?
+        if (design.isEmpty()) {
+            return 0;
+        }
+
+//        if (cachedPatterns.containsKey(design)) {
+//            return cachedPatterns.get(design);
+//        }
+//
+//        if (patterns.contains(design)) {
+//            return 1;
+//        }
+//
+//        for (String pattern : patterns) {
+//            if (design.startsWith(pattern)) {
+//                int tryPermutations = tryPermutations(patterns, design.substring(pattern.length()));
+//                if (tryPermutations > 0) {
+//                    cachedPatterns.put(pattern, tryPermutations);
+//                } else {
+//                    cachedPatterns.put(pattern, 1);
+//                }
+//            }
+//        }
+
+        return 0;
+    }
+
+    private List<String> solvePart1(List<String> input) {
         List<String> patterns = grabPatternsFrom(input.get(0));
 
         List<String> validDesigns = new ArrayList<>();
@@ -33,7 +132,7 @@ class Day19Test {
 
         }
 
-        return validDesigns.size();
+        return validDesigns;
     }
 
     private boolean isValidDesign(List<String> patterns, String design) {
---
+++ 2024/src/test/java/com/pdmoore/aoc/Day23Test.java
@@ -0,0 +1,114 @@
+package com.pdmoore.aoc;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class Day23Test {
+
+    @Test
+    void verifySortedSetBehavior() {
+        Set<String> alphabeticalSet = new TreeSet<>();
+        alphabeticalSet.add("ag");
+        alphabeticalSet.add("cg");
+        alphabeticalSet.add("yn");
+
+        Set<String> mixedSet = new TreeSet<>();
+        mixedSet.add("yn");
+        mixedSet.add("ag");
+        mixedSet.add("cg");
+
+        assertEquals(alphabeticalSet, mixedSet);
+    }
+
+    @Test
+    void part1_example() {
+        List<String> input = PuzzleInput.asStringListFrom("data/day23_example.txt");
+
+        List<Set<String>> actual = findSetsOfThree(input);
+        assertEquals(12, actual.size());
+
+        int actualCount = setsOfThreeThatContainInitialT(actual);
+        assertEquals(7, actualCount);
+    }
+
+    @Test
+    void canFindSetsOfThree() {
+        List<String> input = List.of("aq-yn", "yn-cg", "aq-cg", "dd-ee", "ee-ff");
+
+        List<Set<String>> actual = findSetsOfThree(input);
+
+        assertEquals(1, actual.size());
+        assertEquals(3, actual.get(0).size());
+        assertTrue(actual.get(0).contains("aq"));
+        assertTrue(actual.get(0).contains("cg"));
+        assertTrue(actual.get(0).contains("yn"));
+    }
+
+    @Test
+    void part1() {
+        List<String> input = PuzzleInput.asStringListFrom("data/day23.txt");
+
+        List<Set<String>> actual = findSetsOfThree(input);
+        int actualCount = setsOfThreeThatContainInitialT(actual);
+        assertEquals(1151, actualCount);
+    }
+
+    private List<Set<String>> findSetsOfThree(List<String> input) {
+
+        Map<String, List<String>> connections = new HashMap<>();
+        for (String connection : input) {
+            String[] split = connection.split("-");
+            String lhs = split[0];
+            String rhs = split[1];
+            addConnection(connections, lhs, rhs);
+            addConnection(connections, rhs, lhs);
+        }
+
+        List<Set<String>> threeComputerSets = new ArrayList<>();
+        for (String firstComputer: connections.keySet()) {
+            for (String secondComputer : connections.get(firstComputer)) {
+                for (String thirdComputer : connections.get(secondComputer)) {
+                    if (connections.get(thirdComputer).contains(firstComputer)) {
+                        Set<String> computerSet = new TreeSet<>();
+                        computerSet.add(firstComputer);
+                        computerSet.add(secondComputer);
+                        computerSet.add(thirdComputer);
+                        if (!threeComputerSets.contains(computerSet)) {
+                            threeComputerSets.add(computerSet);
+                        }
+                    }
+                }
+            }
+        }
+
+        return threeComputerSets;
+    }
+
+    private static void addConnection(Map<String, List<String>> connections, String lhs, String rhs) {
+        if (!connections.containsKey(lhs)) {
+            List<String> connectedTo = new ArrayList<>();
+            connectedTo.add(rhs);
+            connections.put(lhs, connectedTo);
+        } else {
+            connections.get(lhs).add(rhs);
+        }
+    }
+
+    private int setsOfThreeThatContainInitialT(List<Set<String>> threeComputerSets) {
+        int result = 0;
+        for (Set<String> set : threeComputerSets) {
+            for (String computer : set) {
+                if (computer.startsWith("t")) {
+                    result++;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
