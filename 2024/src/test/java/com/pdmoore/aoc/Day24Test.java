26e8a1f27169d7497ec58d2c2f413d8e2eda821e
Paul Moore <pdmoore@gmail.com>
2024-12-24 09:02:38 -0800

    2024 day24 part1 wip

--- 2024/src/test/java/com/pdmoore/aoc/Day24Test.java
+++ 2024/src/test/java/com/pdmoore/aoc/Day24Test.java
@@ -2,6 +2,7 @@ package com.pdmoore.aoc;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,16 +18,18 @@ class Day24Test {
         int actual = solvePart1(input);
 
         assertEquals(4, actual);
+    }
 
+    @Test
+    void part1_larger_example() {
+        List<String> input = PuzzleInput.asStringListFrom("data/day24_larger_example.txt");
+
+        int actual = solvePart1(input);
+
+        assertEquals(2024, actual);
     }
 
     private int solvePart1(List<String> input) {
-        // read initial gates and states
-        //blank line
-        // read next section
-        // process everything in next section
-        // get result for z### gates
-
         Map<String, Integer> gateStates = new HashMap<>();
         int instructionIndex = 0;
         for (String line : input) {
@@ -38,48 +41,131 @@ class Day24Test {
 
         instructionIndex++;  // skip the blank line
 
+        List<String> instructionsLeftToProcess = new ArrayList<>();
         for (int i = instructionIndex; i < input.size(); i++) {
-            String line = input.get(i);
-            String[] split = line.split(" -> ");
-            String target = split[1].trim();
+            instructionsLeftToProcess.add(input.get(i).trim());
+        }
 
-            if (split[0].contains(" AND ")) {
-                String[] split2 = split[0].split(" AND ");
 
-                String lhs = split2[0].trim();
-                String rhs = split2[1].trim();
+        // TODO - left off here -
+        // I have all of the unprocessed instructions, but some can't be processed
+        // until later ones fire
+        // Each loop through, process what I can in order, and remove those
+        // Currently trying to remove the instruction being processed but that gives concurrent mod problem
+        // Need to preserve ordering
 
-                Integer newValue = gateStates.get(lhs) & gateStates.get(rhs);
-                gateStates.put(target, newValue);
-            } else if (split[0].contains(" XOR ")) {
-                String[] split2 = split[0].split(" XOR ");
+        int maxZ = -1;
+        List<String> processedInstructions = new ArrayList<>();
+        while (processedInstructions.size() < instructionsLeftToProcess.size()) {
 
-                String lhs = split2[0].trim();
-                String rhs = split2[1].trim();
+            for (String instruction : instructionsLeftToProcess) {
+                if (processedInstructions.contains(instruction)) continue;
 
-                Integer newValue = gateStates.get(lhs) ^ gateStates.get(rhs);
-                gateStates.put(target, newValue);
-            } else if (split[0].contains(" OR ")) {
-          //x02 OR y02 -> z02
-                String[] split2 = split[0].split(" OR ");
+                String[] split = instruction.split(" -> ");
+                String target = split[1].trim();
+                if (target.startsWith("z")) {
 
-                String lhs = split2[0].trim();
-                String rhs = split2[1].trim();
+                    int targetValue = Integer.parseInt(target.substring(1));
+                    maxZ = Math.max(maxZ, targetValue);
+                }
 
-                Integer newValue = gateStates.get(lhs) | gateStates.get(rhs);
-                gateStates.put(target, newValue);
-            } else {
-                throw new RuntimeException("screwed up parsing " + line);
+                if (split[0].contains(" AND ")) {
+                    String[] split2 = split[0].split(" AND ");
+
+                    String lhs = split2[0].trim();
+                    String rhs = split2[1].trim();
+                    if (!gateStates.containsKey(lhs) || !gateStates.containsKey(rhs)) {
+                        continue;
+                    }
+
+                    Integer newValue = gateStates.get(lhs) & gateStates.get(rhs);
+                    gateStates.put(target, newValue);
+                } else if (split[0].contains(" XOR ")) {
+                    String[] split2 = split[0].split(" XOR ");
+
+                    String lhs = split2[0].trim();
+                    String rhs = split2[1].trim();
+                    if (!gateStates.containsKey(lhs) || !gateStates.containsKey(rhs)) {
+                        continue;
+                    }
+
+                    Integer newValue = gateStates.get(lhs) ^ gateStates.get(rhs);
+                    gateStates.put(target, newValue);
+                } else if (split[0].contains(" OR ")) {
+                    //x02 OR y02 -> z02
+                    String[] split2 = split[0].split(" OR ");
+
+                    String lhs = split2[0].trim();
+                    String rhs = split2[1].trim();
+                    if (!gateStates.containsKey(lhs) || !gateStates.containsKey(rhs)) {
+                        continue;
+                    }
+
+                    Integer newValue = gateStates.get(lhs) | gateStates.get(rhs);
+                    gateStates.put(target, newValue);
+                } else {
+                    throw new RuntimeException("screwed up parsing " + instruction);
+                }
+
+                processedInstructions.add(instruction);
             }
         }
 
 
-        // grab the Z's in order
-        int maxZ = 2;
-        // TODO - dynamically determine largest Z
+//        for (int i = instructionIndex; i < input.size(); i++) {
+//            String line = input.get(i);
+//            String[] split = line.split(" -> ");
+//            String target = split[1].trim();
+//            if (target.startsWith("z")) {
+//
+//                int targetValue = Integer.parseInt(target.substring(1));
+//                maxZ = Math.max(maxZ, targetValue);
+//            }
+//
+//            if (split[0].contains(" AND ")) {
+//                String[] split2 = split[0].split(" AND ");
+//
+//                String lhs = split2[0].trim();
+//                String rhs = split2[1].trim();
+//
+//                Integer newValue = gateStates.get(lhs) & gateStates.get(rhs);
+//                gateStates.put(target, newValue);
+//            } else if (split[0].contains(" XOR ")) {
+//                String[] split2 = split[0].split(" XOR ");
+//
+//                String lhs = split2[0].trim();
+//                String rhs = split2[1].trim();
+//
+//                Integer newValue = gateStates.get(lhs) ^ gateStates.get(rhs);
+//                gateStates.put(target, newValue);
+//            } else if (split[0].contains(" OR ")) {
+//                //x02 OR y02 -> z02
+//                String[] split2 = split[0].split(" OR ");
+//
+//                String lhs = split2[0].trim();
+//                String rhs = split2[1].trim();
+//
+//                Integer newValue = gateStates.get(lhs) | gateStates.get(rhs);
+//                gateStates.put(target, newValue);
+//            } else {
+//                throw new RuntimeException("screwed up parsing " + line);
+//            }
+//        }
+
         String resultString = "";
-        for (int i = maxZ; i >=0; i--) {
-            resultString += gateStates.get("z0"+i);
+        for (int i = maxZ; i >= 0; i--) {
+
+            String prefix = "z";
+
+            if (i < 10) {
+                prefix +="0";
+            } else {
+            }
+            Integer gateState = gateStates.get(prefix + i);
+            if (gateState != null) {
+
+                resultString += gateState;
+            }
 
             // pass over if z## is not in gate states
 
@@ -88,7 +174,7 @@ class Day24Test {
 
         // convert result string from boolean to int
 
-
+//        return Integer.parseInt("0011111101000", 2);
         return Integer.parseInt(resultString, 2);
     }
 }
