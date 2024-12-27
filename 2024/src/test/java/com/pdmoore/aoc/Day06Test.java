8033e25c7ed4b8383792bbb278dd6813fd75de21
Paul Moore <pdmoore@gmail.com>
2024-12-27 11:35:28 -0800

    2024 day06 notes on part 2 solution

--- 2024/src/test/java/com/pdmoore/aoc/Day06Test.java
+++ 2024/src/test/java/com/pdmoore/aoc/Day06Test.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class Day06Test {
 
@@ -28,27 +28,129 @@ class Day06Test {
         assertEquals(5453, actual);
     }
 
+    // The example input includes the actual guard paths but this doesn't affect
+    // the detection algo as it only looks for '#' obstructions
+    @Test
+    void part2_detect_a_loop_exists() {
+        char[][] map = PuzzleInput.as2dCharArray("data/day06_example_loop1.txt");
+        DirectionalPoint guardPosition = findGuard(map);
+
+        boolean actual = isThereALoop(map, guardPosition);
+        assertTrue(actual, "expect there to be a loop in the loop1 example map");
+
+        map = PuzzleInput.as2dCharArray("data/day06_example_loop2.txt");
+        actual = isThereALoop(map, guardPosition);
+        assertTrue(actual, "expect there to be a loop in the loop1 example map");
+
+        map = PuzzleInput.as2dCharArray("data/day06_example_loop6.txt");
+        actual = isThereALoop(map, guardPosition);
+        assertTrue(actual, "expect there to be a loop in the loop1 example map");
+    }
+
+    @Test
+    void part2_detect_no_loop_exists() {
+        char[][] map = PuzzleInput.as2dCharArray("data/day06_example.txt");
+        DirectionalPoint guardPosition = findGuard(map);
+
+        boolean actual = isThereALoop(map, guardPosition);
+        assertFalse(actual, "initial example has not loop");
+    }
+
     @Test
     @Disabled("Have not started to solve")
     void part2_example() {
         char[][] map = PuzzleInput.as2dCharArray("data/day06_example.txt");
+        DirectionalPoint guardPosition = findGuard(map);
 
-        int actual = solvePart2(map);
+        int actual = solvePart2(map, guardPosition);
 
         assertEquals(6, actual);
     }
 
-    private int solvePart2(char[][] map) {
+    // collect visited positions
+    // if the same position, with direction, is found, there is a loop
+    // if guard leaves mapped area there is no loop
+    private boolean isThereALoop(char[][] map, DirectionalPoint guardPosition) {
+        Set<DirectionalPoint> visited = new HashSet<>();
+        visited.add(guardPosition);
+
+        while (true) {
+            switch (guardPosition.direction) {
+                case UP:
+                    if (guardPosition.x == 0) {
+                        return false;
+                    } else {
+                        if (map[guardPosition.x - 1][guardPosition.y] == '#') {
+                            guardPosition = new DirectionalPoint(guardPosition.x, guardPosition.y, Direction.RIGHT);
+                        } else {
+                            guardPosition = new DirectionalPoint(guardPosition.x - 1, guardPosition.y, Direction.UP);
+                        }
+                    }
+                    break;
+                case RIGHT:
+                    if (guardPosition.y == map.length - 1) {
+                        return false;
+                    } else {
+                        if (map[guardPosition.x][guardPosition.y + 1] == '#') {
+                            guardPosition = new DirectionalPoint(guardPosition.x, guardPosition.y, Direction.DOWN);
+                        } else {
+                            guardPosition = new DirectionalPoint(guardPosition.x, guardPosition.y + 1, Direction.RIGHT);
+                        }
+                    }
+                    break;
+                case DOWN:
+                    if (guardPosition.x == map[0].length - 1) {
+                        return false;
+                    } else {
+                        if (map[guardPosition.x + 1][guardPosition.y] == '#') {
+                            guardPosition = new DirectionalPoint(guardPosition.x, guardPosition.y, Direction.LEFT);
+                        } else {
+                            guardPosition = new DirectionalPoint(guardPosition.x + 1, guardPosition.y, Direction.DOWN);
+                        }
+                    }
+                    break;
+                case LEFT:
+                    if (guardPosition.y == 0) {
+                        return false;
+                    } else {
+                        if (map[guardPosition.x][guardPosition.y - 1] == '#') {
+                            guardPosition = new DirectionalPoint(guardPosition.x, guardPosition.y, Direction.UP);
+                        } else {
+                            guardPosition = new DirectionalPoint(guardPosition.x, guardPosition.y - 1, Direction.LEFT);
+                        }
+                    }
+                    break;
+            }
+            if (visited.contains(guardPosition)) {
+                return true;
+            }
+            visited.add(guardPosition);
+        }
+    }
+
+
+    private int solvePart2(char[][] map, DirectionalPoint guardPosition) {
 
         // Thought:
         // Place a block '#' in front of the current position
-        // and then try to see if the new map can be escaped or not
-        // challenge is to figure out whether it gets stuck in a loop
-        // run simulation for 10_000 rounds and if no escape, assume it's a loop
+        // and then try to see if the new map has a loop or not
+
         // track points visited in order and look for repetition
         // if stuck in a loop, add the point to a list
         // remove the block '#', advance the guard one position, and try again
         // return the size of the list
+
+        // given the guard position and direction
+        // if guard is at edge and moving off board, don't bother
+        // copy the map and place a '#' in the up/down/left/right of guardPosition to block it
+        // call isThereALoop
+        // if returns true, collect the position of the '#' as a point in a set
+        // there can be dupes of the blocking points since they can be approached from 4 sides
+
+
+
+
+
 
 
         return 0;
@@ -60,9 +162,6 @@ class Day06Test {
         DirectionalPoint current = findGuard(map);
         visited.add(current);
 
-        // TODO - can I remove need to have a separate facing variable?
-        // current has the direction baked in, and when turning 90 degrees should result
-        // in a new directional point added to the Set
         boolean done = false;
         while (!done) {
             switch (current.direction) {
@@ -137,7 +236,7 @@ class Day06Test {
 
     enum Direction {UP, RIGHT, DOWN, LEFT}
 
-    class DirectionalPoint extends Point {
+    static class DirectionalPoint extends Point {
 
         private final Direction direction;
 
