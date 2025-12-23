182500e18754f9e78b0668d3fc837fc75078206c
Paul Moore <pdmoore@gmail.com>
2025-12-23 08:52:44 -0800

    kotlin 2025 day 1 - part 1 refactor

--- kotlin/aoc-2025/aoc-2025/src/main/kotlin/com/pdmoore/aoc/Day01.kt
+++ kotlin/aoc-2025/aoc-2025/src/main/kotlin/com/pdmoore/aoc/Day01.kt
@@ -4,7 +4,7 @@ open class Day01(val input: List<String>) {
     fun solve() : Int {
         var zeroCount = 0
         var currentDial = Dial(50)
-        for (rotation in input) {
+        input.forEach { rotation ->
             currentDial = currentDial.rotate(rotation)
             if (currentDial.position == 0) zeroCount++
         }
--- kotlin/aoc-2025/aoc-2025/src/main/kotlin/com/pdmoore/aoc/Dial.kt
+++ kotlin/aoc-2025/aoc-2025/src/main/kotlin/com/pdmoore/aoc/Dial.kt
@@ -6,7 +6,6 @@ open class Dial(position: Int) {
         val leftOrRight = if (rotation.contains("L")) -1 else 1;
         val distance = rotation.substring(1).toInt()
         var newPosition = (position + (leftOrRight * distance)) % 100
-
         if (newPosition < 0) newPosition += 100
         return Dial(newPosition)
     }
