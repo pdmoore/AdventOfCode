53c2af7a364336ac7282a1356f16035089bc950d
Paul Moore <pdmoore@gmail.com>
2025-12-23 08:46:36 -0800

    kotlin 2025 day 1 - part 1 refactor

--- kotlin/aoc-2025/aoc-2025/src/main/kotlin/com/pdmoore/aoc/Dial.kt
+++ kotlin/aoc-2025/aoc-2025/src/main/kotlin/com/pdmoore/aoc/Dial.kt
@@ -3,12 +3,11 @@ package com.pdmoore.aoc
 open class Dial(position: Int) {
     val position = position
     fun rotate(rotation: String): Dial {
-        val distance = rotation.substring(1).toInt() % 100
-
         val leftOrRight = if (rotation.contains("L")) -1 else 1;
+        val distance = rotation.substring(1).toInt() % 100
         var newPosition = position + (leftOrRight * distance)
 
-        if (newPosition >= 100) newPosition -= 100
+        if (newPosition >= 100) newPosition = newPosition % 100
         if (newPosition < 0) newPosition += 100
         return Dial(newPosition)
     }
@@ -16,11 +15,10 @@ open class Dial(position: Int) {
     companion object {
         fun rotate(input: List<String>): Dial {
             var currentDial = Dial(50)
-            for (i in input) {
+            input.forEach { i ->
                 currentDial = currentDial.rotate(i)
             }
             return currentDial
         }
     }
-
 }
