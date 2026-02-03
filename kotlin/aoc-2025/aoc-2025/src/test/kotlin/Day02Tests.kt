e5c807c3c847af581a6640527a53df29656b627c
Paul Moore <pdmoore@gmail.com>
2026-02-03 12:14:34 -0800

    2026 day02 - part 1 solved

--- kotlin/aoc-2025/aoc-2025/src/test/kotlin/Day02Tests.kt
+++ kotlin/aoc-2025/aoc-2025/src/test/kotlin/Day02Tests.kt
@@ -7,6 +7,10 @@ import java.math.BigInteger
 
 class Day02Tests: FunSpec ( {
 
+    // part 1 solved
+    // Refactor part 1
+    // tackle part 2
+
     val sampleInput = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224," +
             "1698522-1698528,446443-446449,38593856-38593862,565653-565659," +
             "824824821-824824827,2121212118-2121212124"
@@ -76,6 +80,10 @@ class Day02Tests: FunSpec ( {
         sut.solve(sampleInput) shouldBe BigInteger("1227775554")
     }
 
+    test("solve part 1") {
+        val sut = Thing()
+        sut.solve(PuzzleInput.asStringFrom("./data/day02")) shouldBe BigInteger("30599400849")
+    }
 })
 
 open class Thing {
