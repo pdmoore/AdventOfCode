7f84fb317ca8685d9706af993fe8ea3c12a0c88f
Paul Moore <pdmoore@gmail.com>
2026-04-28 10:48:32 -0700

    2015 day 12 part 1 - solved

--- 2015/src/test/java/com/pdmoore/aoc/Day12Tests.java
+++ 2015/src/test/java/com/pdmoore/aoc/Day12Tests.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Day12_take2_Tests {
+public class Day12Tests {
 
     @Test
     void emptyElements_SumToZero() {
