d6f48585188078a021e2dfe8f45c8ec83a772a9b
Paul Moore <pdmoore@gmail.com>
2026-07-08 10:03:51 -0700

    2025 day 3 - part 1 wip

--- kotlin/aoc-2025/aoc-2025/src/test/kotlin/Day03Tests.kt
+++ kotlin/aoc-2025/aoc-2025/src/test/kotlin/Day03Tests.kt
@@ -20,13 +20,13 @@ class Day03Tests: FunSpec({
 98 + 89 + 78 + 92 = 357
      */
 
-    test("largest is the first two digits") {
+    test("largest possible joltage from any two digits") {
 
-        val input = "987654321111111"
-        X.findLargestTwoDigitsFromLeft(input) shouldBe 98
+        X.findLargestTwoDigitsFromLeft("987654321111111") shouldBe 98
+        X.findLargestTwoDigitsFromLeft("811111111111119") shouldBe 89
+        X.findLargestTwoDigitsFromLeft("234234234234278") shouldBe 78
+        X.findLargestTwoDigitsFromLeft("818181911112111") shouldBe 92
     }
-
-
 
 
 })
@@ -34,7 +34,19 @@ class Day03Tests: FunSpec({
 open class X(name: String, test: suspend TestScope.() -> Unit) {
     companion object {
         fun findLargestTwoDigitsFromLeft(input: String): Int {
-            return 66
+            //TODO have not even started to solve the number extraction
+            // it's just grab the largest digit in the string, then the largest
+            // digit in the substring to the right of the first digit
+
+            // get the largest digit & position
+            val maxDigitChar = input.substring(0, input.length - 1).filter { it.isDigit() }.max()
+            val indexOfFirstDigit = input.indexOf(maxDigitChar)
+            val remainingString = input.substring(indexOfFirstDigit + 1)
+            val secondDigitChar = remainingString.filter { it.isDigit() }.max()
+
+            val largestDigit = maxDigitChar.digitToInt() * 10 + secondDigitChar.digitToInt()
+
+            return largestDigit
         }
     }
 }
