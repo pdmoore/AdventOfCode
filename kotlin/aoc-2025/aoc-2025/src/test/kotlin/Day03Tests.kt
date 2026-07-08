a21b2a93ee4385642093b5d7e2386aa3a64d07ab
Paul Moore <pdmoore@gmail.com>
2026-07-08 10:10:22 -0700

    2025 day 3 - part 1 wip

--- kotlin/aoc-2025/aoc-2025/src/test/kotlin/Day03Tests.kt
+++ kotlin/aoc-2025/aoc-2025/src/test/kotlin/Day03Tests.kt
@@ -11,34 +11,29 @@ class Day03Tests: FunSpec({
     // sum all two digit numbers found
     // part 2 - not started
 
-    /*
-    sample input
-987654321111111
-811111111111119
-234234234234278
-818181911112111
-98 + 89 + 78 + 92 = 357
-     */
+
+    val sampleInput: List<String> = listOf("987654321111111",
+        "811111111111119",
+        "234234234234278",
+        "818181911112111"
+    )
 
     test("largest possible joltage from any two digits") {
-
         X.findLargestTwoDigitsFromLeft("987654321111111") shouldBe 98
         X.findLargestTwoDigitsFromLeft("811111111111119") shouldBe 89
         X.findLargestTwoDigitsFromLeft("234234234234278") shouldBe 78
         X.findLargestTwoDigitsFromLeft("818181911112111") shouldBe 92
     }
 
+    test("part 1 answer is sum of all joltages") {
+        X.solvePart1(sampleInput) shouldBe 357
+    }
 
 })
 
 open class X(name: String, test: suspend TestScope.() -> Unit) {
     companion object {
         fun findLargestTwoDigitsFromLeft(input: String): Int {
-            //TODO have not even started to solve the number extraction
-            // it's just grab the largest digit in the string, then the largest
-            // digit in the substring to the right of the first digit
-
-            // get the largest digit & position
             val maxDigitChar = input.substring(0, input.length - 1).filter { it.isDigit() }.max()
             val indexOfFirstDigit = input.indexOf(maxDigitChar)
             val remainingString = input.substring(indexOfFirstDigit + 1)
@@ -47,6 +42,16 @@ open class X(name: String, test: suspend TestScope.() -> Unit) {
             val largestDigit = maxDigitChar.digitToInt() * 10 + secondDigitChar.digitToInt()
 
             return largestDigit
+        }
+
+        fun solvePart1(input: List<String>): Int {
+            // for each line in input
+            // calc and sum joltages
+
+            // return total
+            var total = 0
+            input.forEach { input -> total += findLargestTwoDigitsFromLeft(input) }
+            return total
         }
     }
 }
