79ab4ff34a95c9c15e96a59926bb81d070443d0d
Paul Moore <pdmoore@gmail.com>
2026-07-27 18:42:19 -0700

    2015 d12 p2 - wip - parsing object and ignoring red. Not handling Array as value

--- 2015/src/test/java/com/pdmoore/aoc/Day12Tests.java
+++ 2015/src/test/java/com/pdmoore/aoc/Day12Tests.java
@@ -2,10 +2,13 @@ package com.pdmoore.aoc;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -95,6 +98,21 @@ public class Day12Tests {
         assertEquals(6, actual);
     }
 
+    @Test
+    void part2_ignore_object_that_has_red() {
+        var input = "{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}";
+        int actual = part2Thingy(input);
+        assertEquals(0, actual);
+    }
+
+    @Test
+    void part2_sum_object_values() {
+        var input = "{\"d\":4,\"f\":5}";
+        int actual = part2Thingy(input);
+        assertEquals(9, actual);
+    }
+
+
     private int part2Thingy(String input) {
 
         // convert string to JSON
@@ -106,20 +124,38 @@ public class Day12Tests {
         JsonElement rootNode = JsonParser.parseString(input);
         if (rootNode.isJsonObject()) {
 
-            // Maybe try this - flatten the object to an array and sum
-            // unless red is encountered in which case stop
+            // flattens whole structure to one array
+            //            JsonObject jsonObject = rootNode.getAsJsonObject();
 //            JsonArray jsonArray = jsonObject.getAsJsonArray("myArray");
 //            for (JsonElement element : jsonArray) {
 //                System.out.println(element.getAsString());
 //            }
 
-//            JsonObject jsonObject = rootNode.getAsJsonObject();
-//            jsonObject.keySet().forEach(key -> {
-//                key = key.toString();
-//                if (jsonObject.get(key).isJsonArray()) {
-//                    int sam = 0;
-//                }
-//            });
+            // probably need to do this - halt if value is "red", sum values/arrays otherwise
+            JsonObject jsonObject = rootNode.getAsJsonObject();
+            //                if (jsonObject.get(key).isJsonArray()) {
+            //                    int sam = 0;
+            //                }
+//            {"d":"red","e":[1,2,3,4],"f":5}
+            Iterator<String> keys = jsonObject.keySet().iterator();
+            int objectSum = 0;
+            while(keys.hasNext()) {
+                String key = keys.next();
+                JsonElement jsonElement = jsonObject.get(key);
+                if (jsonElement.isJsonPrimitive() &&
+                        jsonElement.getAsJsonPrimitive().isString()) {
+                    String value = jsonElement.getAsString();
+                    if ("red".equals(value)) {
+                        objectSum = 0;
+                        break;
+                    }
+                }
+                if (jsonElement.isJsonPrimitive() &&
+                    jsonElement.getAsJsonPrimitive().isNumber()) {
+                    objectSum += jsonElement.getAsInt();
+                }
+            }
+            sum += objectSum;
         } else if (rootNode.isJsonArray()) {
             JsonArray jsonArray = rootNode.getAsJsonArray();
             for (JsonElement element : jsonArray) {
